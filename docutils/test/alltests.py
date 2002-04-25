6b37b489d7bc388bb3aee8f089e0d958e8a04b57
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-04-25 03:56:07 +0000

    updated
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@50 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/alltests.py
+++ docutils/test/alltests.py
@@ -6,9 +6,15 @@
 :Revision: $Revision$
 :Date: $Date$
 :Copyright: This module has been placed in the public domain.
+
+All modules named 'test_*.py' in the current directory, and recursively in
+subdirectories (packages) called 'test_*', are loaded and test suites within
+are run.
 """
 
 import time
+# Start point for actual elapsed time, including imports
+# and setup outside of unittest.
 start = time.time()
 
 import sys, os
@@ -30,12 +36,30 @@ class Tee:
 # must redirect stderr *before* first import of unittest
 sys.stdout = sys.stderr = Tee('alltests.out')
 
-import UnitTestFolder
+
+def pformat(suite):
+    step = 4
+    suitestr = repr(suite).replace('=[<', '=[\n<').replace(', ', ',\n')
+    indent = 0
+    output = []
+    for line in suitestr.splitlines():
+        output.append(' ' * indent + line)
+        if line[-1:] == '[':
+            indent += step
+        else:
+            if line [-5:] == ']>]>,':
+                indent -= step * 2
+            elif line[-3:] == ']>,':
+                indent -= step
+    return '\n'.join(output)
 
 
 if __name__ == '__main__':
+    import package_unittest
     path, script = os.path.split(sys.argv[0])
-    suite = UnitTestFolder.loadModulesFromFolder(path, 'test_', subfolders=1)
-    UnitTestFolder.main(suite)
+    suite = package_unittest.loadTestModules(path, 'test_', packages=1)
+    package_unittest.main(suite)
+    #if package_unittest.verbosity > 1:
+    #    print >>sys.stderr, pformat(suite) # check the test suite
     finish = time.time()
     print 'Elapsed time: %.3f seconds' % (finish - start)
