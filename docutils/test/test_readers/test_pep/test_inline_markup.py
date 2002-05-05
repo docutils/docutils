47424a65d202d7c458f348ee5e09b76a1e1a9803
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-05 15:44:00 +0000

    PEP testing.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@89 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_readers/test_pep/test_inline_markup.py
@@ -0,0 +1,120 @@
+#! /usr/bin/env python
+
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision$
+:Date: $Date$
+:Copyright: This module has been placed in the public domain.
+
+Tests for inline markup in PEPs (readers/pep.py).
+"""
+
+from __init__ import DocutilsTestSupport
+
+
+def suite():
+    s = DocutilsTestSupport.PEPParserTestSuite()
+    s.generateTests(totest)
+    return s
+
+
+totest = {}
+
+totest['standalone_references'] = [
+["""\
+See PEP 287 (pep-0287.txt),
+and RFC 2822 (which obsoletes RFC822 and RFC-733).
+""",
+"""\
+<document>
+    <paragraph>
+        See \n\
+        <reference refuri="pep-0287.html">
+            PEP 287
+         (
+        <reference refuri="pep-0287.html">
+            pep-0287.txt
+        ),
+        and \n\
+        <reference refuri="http://www.faqs.org/rfcs/rfc2822.html">
+            RFC 2822
+         (which obsoletes \n\
+        <reference refuri="http://www.faqs.org/rfcs/rfc822.html">
+            RFC822
+         and \n\
+        <reference refuri="http://www.faqs.org/rfcs/rfc733.html">
+            RFC-733
+        ).
+"""],
+["""\
+References split across lines:
+
+PEP
+287
+
+RFC
+2822
+""",
+"""\
+<document>
+    <paragraph>
+        References split across lines:
+    <paragraph>
+        <reference refuri="pep-0287.html">
+            PEP
+            287
+    <paragraph>
+        <reference refuri="http://www.faqs.org/rfcs/rfc2822.html">
+            RFC
+            2822
+"""],
+]
+
+totest['miscellaneous'] = [
+["""\
+For *completeness*, _`let's` ``test`` **other** forms_
+|of| `inline markup` [*]_.
+
+.. [*] See http://docutils.sf.net/spec/rst/reStructuredText.html.
+""",
+"""\
+<document>
+    <paragraph>
+        For \n\
+        <emphasis>
+            completeness
+        , \n\
+        <target id="let-s" name="let's">
+            let's
+         \n\
+        <literal>
+            test
+         \n\
+        <strong>
+            other
+         \n\
+        <reference refname="forms">
+            forms
+        \n\
+        <substitution_reference refname="of">
+            of
+         \n\
+        <interpreted>
+            inline markup
+         \n\
+        <footnote_reference auto="*" id="id1">
+        .
+    <footnote auto="*" id="id2">
+        <paragraph>
+            See \n\
+            <reference refuri="http://docutils.sf.net/spec/rst/reStructuredText.html">
+                http://docutils.sf.net/spec/rst/reStructuredText.html
+            .
+"""],
+]
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
