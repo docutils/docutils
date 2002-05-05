71fc15a3b5ee0fde60fc29ab34017fa753aab7c6
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-05 15:55:33 +0000

    *** empty log message ***
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@93 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_transforms/test_filter.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision$
+:Date: $Date$
+:Copyright: This module has been placed in the public domain.
+
+Tests for docutils.transforms.components.Filter.
+"""
+
+from __init__ import DocutilsTestSupport
+from docutils.transforms.universal import FirstWriterPending
+from docutils.parsers.rst import Parser
+
+
+def suite():
+    parser = Parser()
+    s = DocutilsTestSupport.TransformTestSuite(parser)
+    s.generateTests(totest)
+    return s
+
+totest = {}
+
+totest['meta'] = ((FirstWriterPending,), [
+["""\
+.. meta::
+   :description: The reStructuredText plaintext markup language
+   :keywords: plaintext,markup language
+""",
+"""\
+<document>
+    <meta content="The reStructuredText plaintext markup language" name="description">
+    <meta content="plaintext,markup language" name="keywords">
+"""],
+])
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
