4c44351516935d1b95f8b5ca710bea91811d0cef
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-05 15:26:09 +0000

    Docutils component-related transforms.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@77 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/docutils/transforms/components.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+"""
+:Author: David Goodger
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision$
+:Date: $Date$
+:Copyright: This module has been placed in the public domain.
+
+Docutils component-related transforms.
+"""
+
+__docformat__ = 'reStructuredText'
+
+import sys, os, re, time
+from docutils import nodes, utils, ApplicationError, DataError
+from docutils.transforms import Transform, TransformError
+
+
+class Filter(Transform):
+
+    """
+    Include or exclude elements which depend on a specific Docutils component.
+
+    For use with `nodes.pending` elements.  A "pending" element's dictionary
+    attribute ``details`` must contain a key matching the dependency component
+    type (e.g. ``details['writer']`` for a "pending" element whose ``stage``
+    attribute is 'last writer').  The value is the name of a specific format
+    or context of that component (e.g. ``details['writer'] = 'html'``).  If
+    the Docutils component which called this transform supports that format or
+    context, the "pending" element is replaced by the nodes in
+    ``details['nodes']``; otherwise, the "pending" element is removed.
+
+    For example, the reStructuredText "meta" directive creates a "pending"
+    element containing a "meta" element.  Only writers supporting the "html"
+    format will include the "meta" element; it will be deleted from the output
+    of all other writers.
+    """
+
+    def transform(self):
+        pending = self.startnode
+        component_name = pending.details[pending.stage.split()[-1]]
+        if self.component.supports(component_name):
+            pending.parent.replace(pending, pending.details['nodes'])
+        else:
+            pending.parent.remove(pending)
