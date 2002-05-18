f50a65925cf02643d19330578d80e1e5ac731f75
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-18 02:55:04 +0000

    - Filled in some descriptions.  - Added "shttp" scheme.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@131 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/urischemes.py
+++ docutils/docutils/urischemes.py
@@ -1,13 +1,14 @@
 """
 `schemes` is a dictionary with lowercase URI addressing schemes as
 keys and descriptions as values. It was compiled from the index at
-http://www.w3.org/Addressing/schemes.html, revised 2001-08-20. Many
-values are blank and should be filled in with useful descriptions.
+http://www.w3.org/Addressing/schemes.html (revised 2001-08-20).
 """
+
+# Many values are blank and should be filled in with useful descriptions.
 
 schemes = {
       'about': 'provides information on Navigator',
-      'acap': 'application configuration access protocol',
+      'acap': 'Application Configuration Access Protocol',
       'addbook': "To add vCard entries to Communicator's Address Book",
       'afp': 'Apple Filing Protocol',
       'afs': 'Andrew File System global file names',
@@ -16,33 +17,38 @@ schemes = {
       'castanet': 'Castanet Tuner URLs for Netcaster',
       'chttp': 'cached HTTP supported by RealPlayer',
       'cid': 'content identifier',
-      'data': 'allows inclusion of small data items as "immediate" data; RFC-2397',
+      'data': ('allows inclusion of small data items as "immediate" data; '
+               'RFC 2397'),
       'dav': 'Distributed Authoring and Versioning Protocol; RFC 2518',
       'dns': 'Domain Name System resources',
       'eid': ('External ID; non-URL data; general escape mechanism to allow '
               'access to information for applications that are too '
               'specialized to justify their own schemes'),
-      'fax': '',
+      'fax': ('a connection to a terminal that can handle telefaxes '
+              '(facsimiles); RFC 2806'),
       'file': 'Host-specific file names',
       'finger': '',
       'freenet': '',
       'ftp': 'File Transfer Protocol',
       'gopher': 'The Gopher Protocol',
-      'gsm-sms': '',
-      'h323': '',
-      'h324': '',
-      'hdl': '',
-      'hnews': '',
+      'gsm-sms': ('Global System for Mobile Communications Short Message '
+                  'Service'),
+      'h323': 'video (audiovisual) communication on local area networks',
+      'h324': ('video and audio communications over low bitrate connections '
+               'such as POTS modem connections'),
+      'hdl': 'CNRI handle system',
+      'hnews': 'an HTTP-tunneling variant of the NNTP news protocol',
       'http': 'Hypertext Transfer Protocol',
-      'https': '',
-      'iioploc': '',
-      'ilu': '',
-      'imap': 'internet message access protocol',
-      'ior': '',
-      'ipp': '',
+      'https': 'HTTP over SSL',
+      'iioploc': 'Internet Inter-ORB Protocol Location?',
+      'ilu': 'Inter-Language Unification',
+      'imap': 'Internet Message Access Protocol',
+      'ior': 'CORBA interoperable object reference',
+      'ipp': 'Internet Printing Protocol',
       'irc': 'Internet Relay Chat',
-      'jar': '',
-      'javascript': 'JavaScript code; evaluates the expression after the colon',
+      'jar': 'Java archive',
+      'javascript': ('JavaScript code; evaluates the expression after the '
+                     'colon'),
       'jdbc': '',
       'ldap': 'Lightweight Directory Access Protocol',
       'lifn': '',
@@ -54,9 +60,10 @@ schemes = {
       'md5': '',
       'mid': 'message identifier',
       'mocha': '',
-      'modem': '',
+      'modem': ('a connection to a terminal that can handle incoming data '
+                'calls; RFC 2806'),
       'news': 'USENET news',
-      'nfs': 'network file system protocol',
+      'nfs': 'Network File System protocol',
       'nntp': 'USENET news using NNTP access',
       'opaquelocktoken': '',
       'phone': '',
@@ -71,12 +78,16 @@ schemes = {
       'rx': 'Remote Execution',
       'sdp': '',
       'service': 'service location',
-      'sip': 'session initiation protocol',
+      'shttp': 'secure hypertext transfer protocol',
+      'sip': 'Session Initiation Protocol',
       'smb': '',
       'snews': 'For NNTP postings via SSL',
-      't120': '',
+      't120': 'real time data conferencing (audiographics)',
       'tcp': '',
-      'tel': 'telephone',
+      'tel': ('a connection to a terminal that handles normal voice '
+              'telephone calls, a voice mailbox or another voice messaging '
+              'system or a service that can be operated using DTMF tones; '
+              'RFC 2806.'),
       'telephone': 'telephone',
       'telnet': 'Reference to interactive sessions',
       'tip': 'Transaction Internet Protocol',
