4f29c57ecd8d796528afb7c0b31a4f2b2e2265e6
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-30 02:24:20 +0000

    Updated front-ends to use the new command-line processing facilities of ``docutils.frontend`` (exposed in ``docutils.core.Publisher``), reducing each to just a few lines of code.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@157 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/grubert/tools/pdf.py
+++ sandbox/grubert/tools/pdf.py
@@ -7,31 +7,16 @@
 :Date: $Date$
 :Copyright: This module has been placed in the public domain.
 
-A minimal front-end to the Docutils Publisher.
-
-This module takes advantage of the default values defined in `publish()`.
+A minimal front-end to the Docutils Publisher, producing PDF via ReportLabs.
 """
 
-import sys
-print sys.path
 from docutils.core import publish
 try:
     from docutils.writers.rlpdf import Writer
 except ImportError:
     from rlpdf import Writer
 
-from docutils import utils
 
-reporter = utils.Reporter(2, 4)
-#reporter.setconditions('nodes.Node.walkabout', 2, 4, debug=1)
+usage = 'usage:\n  %prog [options] [source [destination]]'
 
-if len(sys.argv) == 2:
-    publish(writer_name=Writer(), source=sys.argv[1], reporter=reporter)
-elif len(sys.argv) == 3:
-    publish(writer_name=Writer(), source=sys.argv[1], destination=sys.argv[2],
-            reporter=reporter)
-elif len(sys.argv) > 3:
-    print >>sys.stderr, 'Maximum 2 arguments allowed.'
-    sys.exit(1)
-else:
-    publish()
+publish(writer=Writer(), usage=usage)
