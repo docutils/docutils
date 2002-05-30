8640d4f87b40096d834d65db238dc6542dd337d7
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-05-30 02:19:18 +0000

    Updated front-ends to use the new command-line processing facilities of ``docutils.frontend`` (exposed in ``docutils.core.Publisher``), reducing each to just a few lines of code.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@156 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/tools/html.py
+++ docutils/tools/html.py
@@ -7,30 +7,12 @@
 :Date: $Date$
 :Copyright: This module has been placed in the public domain.
 
-A minimal front-end to the Docutils Publisher.
-
-This module takes advantage of the default values defined in `publish()`.
+A minimal front-end to the Docutils Publisher, producing HTML.
 """
 
-import sys
 from docutils.core import publish
-from docutils import utils
 
 
-reporter = utils.Reporter(1, 4)
-#reporter.setconditions('nodes.Node.walkabout', 2, 4, debug=1)
+usage = 'usage:\n  %prog [options] [source [destination]]'
 
-if len(sys.argv) == 2:
-    source = sys.argv[1]
-    destination = None
-elif len(sys.argv) == 3:
-    source = sys.argv[1]
-    destination = sys.argv[2]
-elif len(sys.argv) > 3:
-    print >>sys.stderr, 'Maximum 2 arguments allowed.'
-    sys.exit(1)
-else:
-    source = None
-    destination = None
-publish(source=source, destination=destination, reporter=reporter,
-        writer_name='html')
+publish(writer_name='html', usage=usage)
