44dca2a88509958e6d43e40c098a0654cec8c7f7
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-07-28 18:36:34 +0000

    Relative URLs for stylesheet links.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@405 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/pep_html.py
+++ docutils/docutils/writers/pep_html.py
@@ -53,6 +53,7 @@ class Writer(html4css1.Writer):
         stylesheet = options.pep_stylesheet
         if stylesheet is None:
             stylesheet = options.stylesheet
+        stylesheet = utils.relative_uri(options.destination, stylesheet)
         pyhome = options.python_home
         pephome = options.pep_home
         if pyhome == '..':
