813d21ae5e935c527c8bbc429ee8eb8fd43b7d63
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-08-14 02:39:34 +0000

    Added to project. Contains the "target-notes" directive.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@518 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/docutils/parsers/rst/directives/references.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+
+"""
+:Author: David Goodger, Dmitry Jemerov
+:Contact: goodger@users.sourceforge.net
+:Revision: $Revision$
+:Date: $Date$
+:Copyright: This module has been placed in the public domain.
+
+Directives for references and targets.
+"""
+
+__docformat__ = 'reStructuredText'
+
+from docutils import nodes
+from docutils.transforms import references
+
+
+def target_notes(match, type_name, data, state, state_machine, attributes):
+    """Target footnote generation."""
+    pending = nodes.pending(references.TargetNotes, 'first reader', {})
+    state_machine.document.note_pending(pending)
+    nodelist = [pending]
+    if data:
+        warning = state_machine.reporter.warning(
+              'The "%s" directive takes no data; "%s" ignored (at line %s).'
+              % (match.group(1), data, state_machine.abs_line_number()))
+        nodelist.append(warning)
+    return nodelist, state_machine.is_next_line_blank()
