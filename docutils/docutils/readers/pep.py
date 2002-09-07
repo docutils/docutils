560220475c8f8ae54f12aa3657e445482ba8f6ed
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-09-07 02:33:25 +0000

    Removed PEP & RFC reference detection code; moved to parsers/rst/states.py as options (enabled here by default).
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@645 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/readers/pep.py
+++ docutils/docutils/readers/pep.py
@@ -27,12 +27,20 @@ class Reader(standalone.Reader):
     supported = ('pep',)
     """Contexts this reader supports."""
 
+    cmdline_options = (
+        'PEP Reader Option Defaults',
+        'The --pep-references and --rfc-references options (for the '
+        'reStructuredText parser) are on by default.',
+        ())
+
     transforms = (references.Substitutions,
                   peps.Headers,
                   peps.Contents,
                   peps.TargetNotes,
                   references.Footnotes,
                   references.Hyperlinks,)
+
+    option_default_overrides = {'pep_references': 1, 'rfc_references': 1}
 
     def __init__(self, parser, parser_name):
         """`parser` should be ``None``."""
@@ -44,37 +52,7 @@ class Reader(standalone.Reader):
 class Inliner(rst.states.Inliner):
 
     """
-    Extend `rst.Inliner` to also parse standalone PEP & RFC references.
+    Extend `rst.Inliner` to for local PEP references.
     """
 
-    rfc_url = 'http://www.faqs.org/rfcs/rfc%d.html'
-    pep_url = 'pep-%04d.html'
-    pep_ref_pattern = re.compile(r"""
-          (
-            (pep-\d+(.txt)?)
-          |
-            (PEP\s+(?P<pepnum>\d+))
-          |
-            (RFC(-|\s+)?(?P<rfcnum>\d+))
-          )
-          """, re.VERBOSE)
-
-    def standalone_refs(self, match, lineno):
-        text = match.group(0)
-        if text.startswith('pep-'):
-            ref = os.path.splitext(text)[0] + ".html"
-        elif text.startswith('PEP'):
-            pepnum = int(match.group('pepnum'))
-            ref = self.pep_url % pepnum
-        elif text.startswith('RFC'):
-            rfcnum = int(match.group('rfcnum'))
-            ref = self.rfc_url % rfcnum
-        else:
-            raise rst.states.MarkupMismatch
-        unescaped = rst.states.unescape(text, 0)
-        return [nodes.reference(rst.states.unescape(text, 1), unescaped,
-                                refuri=ref)]
-
-    implicit = (rst.states.Inliner.implicit
-                + ((pep_ref_pattern, standalone_refs),))
-    """PEP-specific list of (pattern, dispatch method) pairs."""
+    pep_url = rst.states.Inliner.pep_url_local
