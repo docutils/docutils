39d507a188a53cca41eeb0867f1a42d84be7b5f1
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-09-20 02:49:01 +0000

    Made the ``locale.setlocale()`` calls in front ends fault-tolerant.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@697 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/tools/pep.py
+++ docutils/tools/pep.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-"""
-:Author: David Goodger
-:Contact: goodger@users.sourceforge.net
-:Revision: $Revision$
-:Date: $Date$
-:Copyright: This module has been placed in the public domain.
+# Author: David Goodger
+# Contact: goodger@users.sourceforge.net
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
 
+"""
 A minimal front end to the Docutils Publisher, producing HTML from PEP
 (Python Enhancement Proposal) documents.
 """
 
 import locale
-locale.setlocale(locale.LC_ALL, '')
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
 
 from docutils.core import publish, default_description
 
