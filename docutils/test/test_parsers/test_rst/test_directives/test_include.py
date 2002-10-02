210c2ae22bd9936543078b541b2c794189ce11e4
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-02 03:09:55 +0000

    added to project
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@740 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_parsers/test_rst/test_directives/test_include.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python
+
+# Author: David Goodger
+# Contact: goodger@users.sourceforge.net
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+Tests for misc.py "include" directive.
+"""
+
+import os.path
+from __init__ import DocutilsTestSupport
+
+
+def suite():
+    s = DocutilsTestSupport.ParserTestSuite()
+    s.generateTests(totest)
+    return s
+
+mydir = os.path.dirname(suite.func_code.co_filename)
+include1 = os.path.join(mydir, 'include1.txt')
+include2 = os.path.join(mydir, 'include2.txt')
+
+totest = {}
+
+totest['include'] = [
+["""\
+Include Test
+============
+
+.. include:: %s
+
+A paragraph.
+""" % include1,
+"""\
+<document source="test data">
+    <section id="include-test" name="include test">
+        <title>
+            Include Test
+        <section id="inclusion-1" name="inclusion 1">
+            <title>
+                Inclusion 1
+            <paragraph>
+                This file is used by \n\
+                <literal>
+                    test_include.py
+                .
+        <paragraph>
+            A paragraph.
+"""],
+["""\
+Include Test
+============
+
+.. include:: %s
+   :literal:
+
+A paragraph.
+""" % include1,
+"""\
+<document source="test data">
+    <section id="include-test" name="include test">
+        <title>
+            Include Test
+        <literal_block source="%s" xml:space="preserve">
+            Inclusion 1
+            -----------
+            \n\
+            This file is used by ``test_include.py``.
+        <paragraph>
+            A paragraph.
+""" % include1],
+["""\
+Let's test the parse context.
+
+    This paragraph is in a block quote.
+
+    .. include:: %s
+
+The included paragraphs should also be in the block quote.
+""" % include2,
+"""\
+<document source="test data">
+    <paragraph>
+        Let's test the parse context.
+    <block_quote>
+        <paragraph>
+            This paragraph is in a block quote.
+        <paragraph>
+            Here are some paragraphs
+            that can appear at any level.
+        <paragraph>
+            This file is used by \n\
+            <literal>
+                test_include.py
+            .
+    <paragraph>
+        The included paragraphs should also be in the block quote.
+"""],
+["""\
+Include Test
+============
+
+.. include:: nonexistent.txt
+
+A paragraph.
+""",
+"""\
+<document source="test data">
+    <section id="include-test" name="include test">
+        <title>
+            Include Test
+        <system_message level="4" line="4" source="test data" type="SEVERE">
+            <paragraph>
+                Problems with "include" directive path:
+                [Errno 2] No such file or directory: 'nonexistent.txt'.
+            <literal_block xml:space="preserve">
+                .. include:: nonexistent.txt
+        <paragraph>
+            A paragraph.
+"""],
+]
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
