eab8c1bbb5876bd49f801cacbc767acb405b435f
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-17 01:45:03 +0000

    updated
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@811 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_parsers/test_rst/test_directives/test_unknown.py
+++ docutils/test/test_parsers/test_rst/test_directives/test_unknown.py
@@ -30,16 +30,28 @@ totest['unknown'] = [
 """,
 """\
 <document source="test data">
+    <system_message level="2" line="1" source="test data" type="WARNING">
+        <paragraph>
+            No directive entry for "reStructuredText-unknown-directive" in module "docutils.parsers.rst.languages.en".
+            Trying "reStructuredText-unknown-directive" as canonical directive name.
     <system_message level="3" line="1" source="test data" type="ERROR">
         <paragraph>
             Unknown directive type "reStructuredText-unknown-directive".
         <literal_block xml:space="preserve">
             .. reStructuredText-unknown-directive::
+    <system_message level="2" line="3" source="test data" type="WARNING">
+        <paragraph>
+            No directive entry for "reStructuredText-unknown-directive" in module "docutils.parsers.rst.languages.en".
+            Trying "reStructuredText-unknown-directive" as canonical directive name.
     <system_message level="3" line="3" source="test data" type="ERROR">
         <paragraph>
             Unknown directive type "reStructuredText-unknown-directive".
         <literal_block xml:space="preserve">
             .. reStructuredText-unknown-directive:: argument
+    <system_message level="2" line="5" source="test data" type="WARNING">
+        <paragraph>
+            No directive entry for "reStructuredText-unknown-directive" in module "docutils.parsers.rst.languages.en".
+            Trying "reStructuredText-unknown-directive" as canonical directive name.
     <system_message level="3" line="5" source="test data" type="ERROR">
         <paragraph>
             Unknown directive type "reStructuredText-unknown-directive".
