e846031df3a492e4ceb1a4ecb399e6872a4f9d53
aahz <aahz@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-17 14:35:52 +0000

    Hacked up OpenOffice.org writer
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@820 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/aahz/OO/open_office.py
@@ -0,0 +1,33 @@
+import sys
+import zipfile
+from cStringIO import StringIO
+
+from docutils import core, io
+
+import OOdirectives
+import OOtext
+import OOwriter
+
+pub = core.Publisher(writer=OOwriter.Writer())
+pub.set_reader('standalone', None, 'restructuredtext')
+options = pub.set_options()
+pub.source = io.FileInput(options, source_path=sys.argv[1])
+pub.destination = io.StringOutput(options)
+content = pub.publish()
+
+manifest_list = [
+    ('content.xml', content),
+    ('styles.xml', OOtext.styles)
+    ]
+
+manifest_entries = []
+for docname, _ in manifest_list:
+    manifest_entries.append(OOtext.manifest_format % docname)
+manifest = OOtext.manifest % '\n '.join(manifest_entries)
+manifest_list.append( ('META-INF/manifest.xml', manifest) )
+
+zip = zipfile.ZipFile(sys.argv[2], "w")
+for docname, contents in manifest_list:
+    zinfo = zipfile.ZipInfo(docname)
+    zip.writestr(zinfo, contents)
+zip.close()
