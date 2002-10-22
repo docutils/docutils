14320cd7310ed49cc0c77a1376e0140a13f46722
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-22 01:02:55 +0000

    Fixed attribute typo in <colspec>.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@837 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/html4css1.py
+++ docutils/docutils/writers/html4css1.py
@@ -380,7 +380,7 @@ class HTMLTranslator(nodes.NodeVisitor):
         for node in self.colspecs:
             colwidth = int(node['colwidth'] * 100.0 / width + 0.5)
             self.body.append(self.emptytag(node, 'col',
-                                           colwidth='%i%%' % colwidth))
+                                           width='%i%%' % colwidth))
         self.colspecs = []
 
     def visit_comment(self, node,
