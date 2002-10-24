a1e6a3c02bb2b3b318aae13ff9ff69c85f3efb1d
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-24 00:40:03 +0000

    Added ``Parser.finish_parse()`` method; updated.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@846 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/parsers/__init__.py
+++ docutils/docutils/parsers/__init__.py
@@ -15,14 +15,22 @@ from docutils import Component
 
 class Parser(Component):
 
+    component_type = 'parser'
+
     def parse(self, inputstring, document):
         """Override to parse `inputstring` into document tree `document`."""
         raise NotImplementedError('subclass must override this method')
 
     def setup_parse(self, inputstring, document):
-        """Initial setup, used by `parse()`."""
+        """Initial parse setup.  Call at start of `self.parse()`."""
         self.inputstring = inputstring
         self.document = document
+        document.reporter.attach_observer(document.note_parse_message)
+
+    def finish_parse(self):
+        """Finalize parse details.  Call at end of `self.parse()`."""
+        self.document.reporter.detach_observer(
+            self.document.note_parse_message)
 
 
 _parser_aliases = {
