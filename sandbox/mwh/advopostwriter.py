342bc89f7fc5e39a3085aaeedfb1c017ed0f54c4
mwh <mwh@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-10-25 12:47:21 +0000

    Slightly more robust now, I think.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@862 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/mwh/advopostwriter.py
+++ sandbox/mwh/advopostwriter.py
@@ -7,6 +7,19 @@ class Writer(html4css1.Writer):
         self.W_init()
         self.translator_class = MyHTMLTranslator
 
+def find_visible_child(node, starting_index, direction):
+    if direction == "forwards":
+        step = 1
+        end = len(node)
+    else:
+        step = -1
+        end = -1
+    for i in range(starting_index, end, step):
+        if not isinstance(node[i], nodes.Invisible):
+            return node[i]
+    else:
+        return None
+
 class MyHTMLTranslator(html4css1.HTMLTranslator):
     def visit_title(self, node):
         self.body.append(self.starttag(node, 'b', '',
@@ -17,27 +30,29 @@ class MyHTMLTranslator(html4css1.HTMLTranslator):
     def depart_document(self, node):
         pass
     def visit_paragraph(self, node):
-        index = node.parent.index(node) - 1
-        while index >= 0:
-            if not isinstance(node.parent[index], nodes.Invisible):
-                if isinstance(node.parent[index], nodes.paragraph):
-                    self.body.append('\n<p>\n')
-                else:
-                    self.body.append(self.starttag(node, 'blockquote', '\n'))
-                break
-            index -= 1
+        if isinstance(node.parent, nodes.section):
+            index = node.parent.index(node)
+            prev = find_visible_child(node.parent, index - 1, "backwards")
+            if isinstance(prev, nodes.title):
+                self.body.append(self.starttag(node, 'blockquote', '\n'))
+            else:
+                self.body.append('<p>\n')
         else:
-            self.body.append(self.starttag(node, 'blockquote', '\n'))
+            html4css1.HTMLTranslator.visit_paragraph(self, node)
+
     def depart_paragraph(self, node):
-        index = node.parent.index(node) + 1
-        while index < len(node.parent):
-            if not isinstance(node.parent[index], nodes.Invisible):
-                if not isinstance(node.parent[index], nodes.paragraph):
-                    self.body.append('\n</blockquote>\n')
-                break
-            index += 1
+        if isinstance(node.parent, nodes.section):
+            index = node.parent.index(node)
+            next = find_visible_child(node.parent, index + 1, "forwards")
+            if not next:
+                self.body.append('\n</blockquote>\n')
+            else:
+                self.body.append("\n");
         else:
-            self.body.append('\n</blockquote>\n')
+            # this tends to include "</p>"s which advogato then
+            # discards.  I don't care that much...
+            html4css1.HTMLTranslator.depart_paragraph(self, node)
+        
     def visit_section(self, node):
         pass
     def depart_section(self, node):
