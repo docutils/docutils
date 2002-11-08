778397dc5afa049aadbaa97bb04a0a20a783a81b
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-11-08 01:28:01 +0000

    Unicode escape patch from Miroslav Vasko.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@910 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/languages/sk.py
+++ docutils/docutils/languages/sk.py
@@ -1,10 +1,10 @@
-"""
-:Author: Miroslav Va¹ko
-:Contact: zemiak@zoznam.sk
-:Revision: $Revision$
-:Date: $Date$
-:Copyright: This module has been placed in the public domain.
+# :Author: Miroslav Vasko
+# :Contact: zemiak@zoznam.sk
+# :Revision: $Revision$
+# :Date: $Date$
+# :Copyright: This module has been placed in the public domain.
 
+"""
 Slovak-language mappings for language-dependent features of Docutils.
 """
 
@@ -15,43 +15,43 @@ from docutils import nodes
 
 
 labels = {
-      'author': 'Autor',
-      'authors': 'Autori',
-      'organization': 'Organizácia',
-      'address': 'Adresa',
-      'contact': 'Kontakt',
-      'version': 'Verzia',
-      'revision': 'Revízia',
-      'status': 'Stav',
-      'date': 'Dátum',
-      'copyright': 'Copyright',
-      'dedication': 'Venovanie',
-      'abstract': 'Abstraktne',
-      'attention': 'Pozor!',
-      'caution': 'Opatrne!',
-      'danger': '!NEBEZPEÈENSTVO!',
-      'error': 'Chyba',
-      'hint': 'Rada',
-      'important': 'Dôle¾ité',
-      'note': 'Poznámka',
-      'tip': 'Tip',
-      'warning': 'Varovanie',
-      'contents': 'Obsah'}
+      'author': u'Autor',
+      'authors': u'Autori',
+      'organization': u'Organiz\u00E1cia',
+      'address': u'Adresa',
+      'contact': u'Kontakt',
+      'version': u'Verzia',
+      'revision': u'Rev\u00EDzia',
+      'status': u'Stav',
+      'date': u'D\u00E1tum',
+      'copyright': u'Copyright',
+      'dedication': u'Venovanie',
+      'abstract': u'Abstraktne',
+      'attention': u'Pozor!',
+      'caution': u'Opatrne!',
+      'danger': u'!NEBEZPE\u010cENSTVO!',
+      'error': u'Chyba',
+      'hint': u'Rada',
+      'important': u'D\u00F4le\u017Eit\u00E9',
+      'note': u'Pozn\u00E1mka',
+      'tip': u'Tip',
+      'warning': u'Varovanie',
+      'contents': u'Obsah'}
 """Mapping of node class name to label text."""
 
 bibliographic_fields = {
-      'author': nodes.author,
-      'authors': nodes.authors,
-      'organization': nodes.organization,
-      'address': nodes.address,
-      'contact': nodes.contact,
-      'version': nodes.version,
-      'revision': nodes.revision,
-      'status': nodes.status,
-      'date': nodes.date,
-      'copyright': nodes.copyright,
-      'dedication': nodes.topic,
-      'abstract': nodes.topic}
+      u'autor': nodes.author,
+      u'autori': nodes.authors,
+      u'organiz\u00E1cia': nodes.organization,
+      u'adresa': nodes.address,
+      u'kontakt': nodes.contact,
+      u'verzia': nodes.version,
+      u'rev\u00EDzia': nodes.revision,
+      u'stav': nodes.status,
+      u'D\u00E1tum': nodes.date,
+      u'copyright': nodes.copyright,
+      u'venovanie': nodes.topic,
+      u'abstraktne': nodes.topic}
 """Field name (lowcased) to node class name mapping for bibliographic fields
 (field_list)."""
 
