234ae0eb68ebf54a142cb92ab274f307be8ae54b
gschwant <gschwant@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2002-11-15 22:04:58 +0000

    Some minor adjustments in order to work with docutils 0.2.5 and higher.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@953 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/gschwant/docfactory/factory/docutilsadapter.py
+++ sandbox/gschwant/docfactory/factory/docutilsadapter.py
@@ -7,9 +7,12 @@
 from   docutils.languages.en             import bibliographic_fields
 from   docutils.parsers.rst.languages.en import directives
 from   docutils.core                     import publish_cmdline
+from   docutils                          import __version__
 from   re                                import findall
 import os, sys
 from   wxPython.wx                       import wxLogMessage
+
+docutilsversion = __version__
 
 def get_rest_bibl_fields():
     # suitable for autocompletion in wxStyledTextCtrl
@@ -30,39 +33,45 @@ def get_rest_directives():
     return drvs[1:]
 
 def rest2html(file, htmlfile, dir):
-    # publish reST as HTML and return
-    # linenumbers of warnings and errors
-    warning_lines = []
-    error_lines = []
+    # publish reST as HTML
+    current_dir = os.path.abspath(os.curdir)
     sys.stderr = StdCatcher()
     os.chdir(dir)    
     publish_cmdline(writer_name='html', argv = [file, htmlfile])
-    warning_lines = sys.stderr.GetWarnings()
-    error_lines = sys.stderr.GetErrors()
+    os.chdir(current_dir)
     sys.stderr = sys.__stderr__
-    return warning_lines, error_lines
-    
+    return 1
+
 class StdCatcher:
     # suitable for catching docutils stderr-output
     def __init__(self):
-        self.warnings = []
-        self.errors = []
         self.text = ''
         
     def write(self,s):
-        if s.startswith('WARNING'):
-            for i in findall('line \d+', s):
-                self.warnings.append(int(i.split(' ')[1])-1)
-        if s.startswith('ERROR') or s.startswith('SEVERE'):
-            for i in findall('line \d+', s):
-                self.errors.append(int(i.split(' ')[1])-1)
         wxLogMessage(s)
 
-    def GetWarnings(self):
-        self.warnings.sort()
-        return self.warnings
-
-    def GetErrors(self):
-        self.errors.sort()
-        return self.errors
-
+def get_errors(str):
+    errors = []
+    warnings = []
+    lines = str.splitlines()
+    for s in lines:
+        if docutilsversion < '0.2.5':
+            if s.startswith('WARNING'):
+                for i in findall('line \d+', s):
+                    warnings.append(int(i.split(' ')[1])-1)
+            if s.startswith('ERROR') or s.startswith('SEVERE'):
+                for i in findall('line \d+', s):
+                    errors.append(int(i.split(' ')[1])-1)
+        else:
+            parts = s.split(': (')
+            if len(parts) > 1:
+                s2 = parts[1]
+                if s2.startswith('WARNING'):
+                    for i in findall(':\d+:', s):
+                        warnings.append(int(i[1:-1])-1)
+                if s2.startswith('ERROR') or s2.startswith('SEVERE'):
+                    for i in findall(':\d+:', s):
+                        errors.append(int(i[1:-1])-1)
+    errors.sort()
+    warnings.sort()
+    return warnings, errors
