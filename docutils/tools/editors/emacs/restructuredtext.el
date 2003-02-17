da2542b4ec49c8bbeeac771ffbdb8bc3a22e6ab2
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-02-17 21:35:25 +0000

    added function from Martin Blais
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1180 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/tools/editors/emacs/restructuredtext.el
+++ docutils/tools/editors/emacs/restructuredtext.el
@@ -3,10 +3,11 @@
 ;; Copyright: This module has been placed in the public domain.
 ;;
 ;; Support code for editing reStructuredText with Emacs indented-text mode.
-;; The goal is to integrate it into a reStructuredText editing mode.
+;; The goal is to create an integrated reStructuredText editing mode.
 
 (defun replace-lines (fromchar tochar)
-  "Replace flush-left lines, consisting of multiple FROMCHAR characters, \
+  ;; by David Goodger
+  "Replace flush-left lines, consisting of multiple FROMCHAR characters,
 with equal-length lines of TOCHAR."
   (interactive "\
 cSearch for flush-left lines of char: 
@@ -27,3 +28,44 @@ cand replace with char: ")
 	    (insert-char tochar l))
 	(search-failed
 	 (message (format "%d lines replaced." found)))))))
+
+(defun repeat-last-character ()
+  ;; by Martin Blais
+  "Fills the current line up to the length of the preceding line (if not
+empty), using the last character on the current line.  If the preceding line
+is empty, or if a prefix argument is provided, fill up to the fill-column.
+
+If the current line is longer than the desired length, shave the characters
+off the current line to fit the desired length.
+
+As an added convenience, if the command is repeated immediately, the
+alternative behaviour is performed."
+  (interactive)
+  (let* ((curcol (current-column))
+	 (lbp (line-beginning-position -1))
+	 (prevcol (if (= lbp 1) 
+		      fill-column
+		    (save-excursion
+		      (forward-line -1)
+		      (end-of-line)
+		      (skip-chars-backward " \t" lbp)
+		      (let ((cc (current-column)))
+			(if (= cc 0) fill-column cc)))))
+	 (rightmost-column
+	  (cond (current-prefix-arg fill-column)
+		((equal last-command 'repeat-last-character)
+		 (if (= curcol fill-column) prevcol fill-column))
+		(t (save-excursion
+		     (if (= prevcol 0) fill-column prevcol))))))
+    (end-of-line)
+    (if (> (current-column) rightmost-column)
+	;; shave characters off the end
+	(delete-region (- (point)
+			  (- (current-column) rightmost-column))
+		       (point))
+      ;; fill with last characters
+      (insert-char (preceding-char)
+		   (- rightmost-column (current-column))))))
+  ;; It would be useful if only these characters were repeated:
+  ;; =-`:.'"~^_*+#<>!$%&(),/;?@[\]{|}
+  ;; Especially, empty lines shouldn't be repeated.
