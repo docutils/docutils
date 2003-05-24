535f1db4beacc1f068297689bfde0dbccba34c5f
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-05-24 20:52:07 +0000

    new test module
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1339 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_transforms/test_class.py
@@ -0,0 +1,104 @@
+#! /usr/bin/env python
+
+# Author: David Goodger
+# Contact: goodger@users.sourceforge.net
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+Tests for `docutils.transforms.misc.ClassAttribute`.
+"""
+
+from __init__ import DocutilsTestSupport
+from docutils.parsers.rst import Parser
+
+
+def suite():
+    parser = Parser()
+    s = DocutilsTestSupport.TransformTestSuite(parser)
+    s.generateTests(totest)
+    return s
+
+totest = {}
+
+totest['tables_of_contents'] = ((), [
+["""\
+.. class:: one
+
+paragraph
+""",
+"""\
+<document source="test data">
+    <paragraph class="one">
+        paragraph
+"""],
+["""\
+.. class:: two
+..
+
+    Block quote
+""",
+"""\
+<document source="test data">
+    <comment xml:space="preserve">
+    <block_quote class="two">
+        <paragraph>
+            Block quote
+"""],
+["""\
+    Block quote
+
+    .. class:: three
+
+Paragraph
+""",
+"""\
+<document source="test data">
+    <block_quote>
+        <paragraph>
+            Block quote
+    <paragraph class="three">
+        Paragraph
+"""],
+["""\
+.. class:: four
+
+Section Title
+=============
+
+Paragraph
+""",
+"""\
+<document source="test data">
+    <section class="four" id="section-title" name="section title">
+        <title>
+            Section Title
+        <paragraph>
+            Paragraph
+"""],
+["""\
+.. class::
+
+.. class:: 99
+""",
+"""\
+<document source="test data">
+    <system_message level="3" line="1" source="test data" type="ERROR">
+        <paragraph>
+            Error in "class" directive:
+            1 argument(s) required, 0 supplied.
+        <literal_block xml:space="preserve">
+            .. class::
+    <system_message level="3" line="3" source="test data" type="ERROR">
+        <paragraph>
+            Invalid class attribute value for "class" directive: 99
+        <literal_block xml:space="preserve">
+            .. class:: 99
+"""],
+])
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
