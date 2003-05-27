af28ad143109e7bebba917db39dc3c9be323ca49
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-05-27 06:43:46 +0000

    Option --no-doc-info and --no-doc-title to standalone reader.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1352 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/frontend.py
+++ docutils/docutils/frontend.py
@@ -43,10 +43,17 @@ def read_config_file(option, opt, value, parser):
     """
     config_parser = ConfigParser()
     config_parser.read(value)
-    settings = config_parser.get_section('options')
-    make_paths_absolute(settings, parser.relative_path_settings,
+
+    default_section = 'options'
+    for section_name in config_parser.sections:
+        if section_name == default_section:
+            prefix = ''
+        else:
+            prefix = section_name + '_'
+        settings = config_parser.get_section(section_name)
+        make_paths_absolute(settings, parser.relative_path_settings,
                         os.path.dirname(value))
-    parser.values.__dict__.update(settings)
+        parser.values.__dict__.update(settings)
 
 def make_paths_absolute(pathdict, keys, base_path=None):
     """
