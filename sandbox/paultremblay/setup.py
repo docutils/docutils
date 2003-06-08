8b3cff804a9cac807e7c5949e1e70019b1ce5a0f
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-06-08 22:32:32 +0000

    correcting missing files/bad setup.py
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1402 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/setup.py
+++ sandbox/paultremblay/setup.py
@@ -9,11 +9,11 @@ setup(name="docutils_extension_tools",
     description="Extensions that add features to docutils.",
     author="Paul Tremblay",
     author_email="phthenry@earthlink.net",
-    packages=['nest_inline', 'xml_tools_trem', 'txt_to_xml', 'rst_tools_trem', '.'],
+    packages=[docutils_nest],
     # apparently, the first in the tupple below
     # is the dirctory to install to, and the second
     # is the current location to build from?
-data_files = [("/etc/nest_docutils", ["data/configure.xml"])],
+data_files = [("/etc/docutils_nest", ["data/configure.xml"])],
     scripts=["scripts/docutils-nest-xml.py"],
     )
 
