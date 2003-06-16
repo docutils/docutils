19196c7ae8770f41864147d12c3b068e5507681c
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-06-16 03:13:23 +0000

    Updated with PyPI Trove classifiers. Conditional installation of third-party modules. Is there a better way?
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1463 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/setup.py
+++ docutils/setup.py
@@ -2,26 +2,91 @@
 # $Id$
 # Copyright: This file has been placed in the public domain.
 
+import sys
 from distutils.core import setup
 
 def do_setup():
-    dist = setup(
-          name = 'docutils',
-          description = 'Python Documentation Utilities',
-          #long_description = '',
-          url = 'http://docutils.sourceforge.net/',
-          version = '0.2+',
-          author = 'David Goodger',
-          author_email = 'goodger@users.sourceforge.net',
-          license = 'public domain, Python, BSD (see COPYING.txt)',
-          packages = ['docutils', 'docutils.languages',
-                      'docutils.parsers', 'docutils.parsers.rst',
-                      'docutils.parsers.rst.directives',
-                      'docutils.parsers.rst.languages',
-                      'docutils.readers', 'docutils.readers.python',
-                      'docutils.transforms',
-                      'docutils.writers',])
+    extras = get_extras()
+    if extras:
+        setup(py_modules = extras,
+              package_dir = {'': 'extras'})
+    kwargs = package_data.copy()
+    if sys.hexversion >= 0x02030000:    # Python 2.3
+        kwargs['classifiers'] = classifiers
+    dist = setup(**kwargs)
+    #import pprint
+    #pprint.pprint(dist.__dict__)
     return dist
+
+package_data = {
+    'name': 'docutils',
+    'description': 'Python Documentation Utilities',
+    'long_description': """\
+Docutils is a modular system for processing documentation into useful formats,
+such as HTML, XML, and TeX.  For input Docutils supports reStructuredText, an
+easy-to-read, what-you-see-is-what-you-get plaintext markup syntax.""",
+    'url': 'http://docutils.sourceforge.net/',
+    'version': '0.2+',
+    'author': 'David Goodger',
+    'author_email': 'goodger@users.sourceforge.net',
+    'license': 'public domain, Python, BSD, GPL (see COPYING.txt)',
+    'packages': ['docutils', 'docutils.languages',
+                 'docutils.parsers', 'docutils.parsers.rst',
+                 'docutils.parsers.rst.directives',
+                 'docutils.parsers.rst.languages',
+                 'docutils.readers', 'docutils.readers.python',
+                 'docutils.transforms',
+                 'docutils.writers',],}
+"""Distutils setup parameters."""
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Other Audience',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: Public Domain',
+    'License :: OSI Approved :: Python Software Foundation License',
+    'License :: OSI Approved :: BSD License',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Documentation',
+    'Topic :: Software Development :: Documentation',
+    'Topic :: Text Processing',
+    'Natural Language :: English',
+    'Natural Language :: French',
+    'Natural Language :: German',
+    'Natural Language :: Italian',
+    'Natural Language :: Slovak',
+    'Natural Language :: Spanish',
+    'Natural Language :: Swedish',]
+"""Trove classifiers for the Distutils "register" command;
+Python 2.3 and up."""
+
+extra_modules = [('optparse', '1.4.1', None),
+                 ('textwrap', None, None),
+                 ('roman', '1.4', ['toRoman', 'fromRoman',
+                                   'InvalidRomanNumeralError'])]
+"""Third-party modules to install if they're not already present.
+List of (module name, minimum __version__ string, [attribute names])."""
+
+def get_extras():
+    extras = []
+    for module_name, version, attributes in extra_modules:
+        try:
+            module = __import__(module_name)
+            if version and module.__version__ < version:
+                raise ValueError
+            for attribute in attributes or []:
+                getattr(module, attribute)
+            print ('"%s" module already present; ignoring extras/%s.py.'
+                   % (module_name, module_name))
+        except (ImportError, AttributeError, ValueError):
+            extras.append(module_name)
+    return extras
+
 
 if __name__ == '__main__' :
     do_setup()
