e40aa735ee66315c9f8c37d9e227c95df3639dec
rnd0110 <rnd0110@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-06-28 15:01:21 +0000

    More and better translations
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1521 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/parsers/rst/languages/ru.py
+++ docutils/docutils/parsers/rst/languages/ru.py
@@ -17,16 +17,20 @@ reStructuredText.
 __docformat__ = 'reStructuredText'
 
 directives = {
- u'line-block': u'line-block',
+ u'\u0431\u043b\u043e\u043a-\u0441\u0442\u0440\u043e\u043a': u'line-block',
  u'meta': u'meta',
- u'parsed-literal': u'parsed-literal',
- u'pull-quote': u'pull-quote',
- u'raw': u'raw',
- u'replace': u'replace',
- u'restructuredtext-test-directive': u'restructuredtext-test-directive',
- u'target-notes': u'target-notes',
+ u'\u043e\u0431\u0440\u0430\u0431\u043e\u0442\u0430\u043d\u043d\u044b\u0439-\u043b\u0438\u0442\u0435\u0440\u0430\u043b':
+ u'parsed-literal',
+ u'\u0432\u044b\u0434\u0435\u043b\u0435\u043d\u043d\u0430\u044f-\u0446\u0438\u0442\u0430\u0442\u0430':
+ u'pull-quote',
+ u'\u0441\u044b\u0440\u043e\u0439': u'raw',
+ u'\u0437\u0430\u043c\u0435\u043d\u0430': u'replace',
+ u'\u0442\u0435\u0441\u0442\u043e\u0432\u0430\u044f-\u0434\u0438\u0440\u0435\u043a\u0442\u0438\u0432\u0430-restructuredtext':
+ u'restructuredtext-test-directive',
+ u'\u0446\u0435\u043b\u0435\u0432\u044b\u0435-\u0441\u043d\u043e\u0441\u043a\u0438': 
+ u'target-notes',
  u'unicode': u'unicode',
- u'\u0431\u043e\u043a\u043e\u0432\u043e\u0435-\u043c\u0435\u043d\u044e':
+ u'\u0431\u043e\u043a\u043e\u0432\u0430\u044f-\u043f\u043e\u043b\u043e\u0441\u0430':
  u'sidebar',
  u'\u0432\u0430\u0436\u043d\u043e': u'important',
  u'\u0432\u043a\u043b\u044e\u0447\u0430\u0442\u044c': u'include',
@@ -84,6 +88,6 @@ roles = {
   'footnote-reference',
  u'\u0446\u0438\u0442\u0430\u0442\u043d\u0430\u044f-\u0441\u0441\u044b\u043b\u043a\u0430':
   'citation-reference',
- u'\u044f\u043a\u043e\u0440\u044c': 'target'}
+ u'\u0446\u0435\u043b\u044c': 'target'}
 """Mapping of Russian role names to canonical role names for interpreted text.
 """
