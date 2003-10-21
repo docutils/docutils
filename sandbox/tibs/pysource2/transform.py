1fe29ced0ed507acc8b056db33f1284d5d671172
tibs <tibs@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2003-10-21 11:24:11 +0000

    Packages and subpackages.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1722 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/tibs/pysource2/transform.py
+++ sandbox/tibs/pysource2/transform.py
@@ -1,4 +1,10 @@
 """transform.py - create a docutils Document tree from a Package or Module tree
+
+:Author:    Tibs
+:Contact:   tibs@tibsnjoan.co.uk
+:Revision:  $1.0$
+:Date:      $Date$
+:Copyright: This module has been placed in the public domain.
 """
 
 from docutils.utils import new_document
@@ -9,10 +15,27 @@ def make_document(tree):
 
     The tree given must be either a Package or Module tree.
     """
-    document = new_document("Package trivial_package")
-    section = nodes.section(id="package-trivial-package",
-                            name="package trivial_package")
-    title = nodes.title(text="Package trivial_package")
-    section.append(title)
+
+    # @@@ Assumes a Package at the head of the tree
+    document = new_document("Package %s"%tree.filename)
+    section = make_package_section(tree)
     document.append(section)
     return document
+
+def make_package_section(package,parent=None):
+    """Return a docutils tree constructed from this Package tree
+    """
+    if parent:
+        title = "Package %s.%s"%(parent.filename,package.filename)
+    else:
+        title = "Package %s"%(package.filename)
+    section = nodes.section(id=nodes.make_id(title),
+                            name=nodes.fully_normalize_name(title))
+    title = nodes.title(text=title)
+    section.append(title)
+
+    for child in package.children:
+        if child.__class__.__name__ == "Package":
+            subsection = make_package_section(child,parent=package)
+            section.append(subsection)
+    return section
