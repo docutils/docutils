8070708c59094ddb176edbee852c94fa5c82fbbf
reggie <reggie@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-03-22 01:08:19 +0000

    The "contents" directive does more work up-front, creating the "topic" and "title", and leaving the "pending" node for the transform.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1854 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/parsers/rst/directives/parts.py
+++ docutils/docutils/parsers/rst/directives/parts.py
@@ -10,7 +10,7 @@ Directives for document parts.
 
 __docformat__ = 'reStructuredText'
 
-from docutils import nodes
+from docutils import nodes, languages
 from docutils.transforms import parts
 from docutils.parsers.rst import directives
 
@@ -27,17 +27,42 @@ def backlinks(arg):
 def contents(name, arguments, options, content, lineno,
              content_offset, block_text, state, state_machine):
     """Table of contents."""
+    document = state_machine.document
+    language = languages.get_language(document.settings.language_code)
+
     if arguments:
         title_text = arguments[0]
         text_nodes, messages = state.inline_text(title_text, lineno)
         title = nodes.title(title_text, '', *text_nodes)
     else:
         messages = []
-        title = None
-    pending = nodes.pending(parts.Contents, {'title': title}, block_text)
+        if options.has_key('local'):
+            title = None
+        else:
+            title = nodes.title('', language.labels['contents'])
+
+    topic = nodes.topic(CLASS='contents')
+
+    cls = options.get('class')
+    if cls:
+        topic.set_class(cls)
+
+    if title:
+        name = title.astext()
+        topic += title
+    else:
+        name = language.labels['contents']
+
+    name = nodes.fully_normalize_name(name)
+    if not document.has_name(name):
+        topic['name'] = name
+    document.note_implicit_target(topic)
+
+    pending = nodes.pending(parts.Contents, rawsource=block_text)
     pending.details.update(options)
-    state_machine.document.note_pending(pending)
-    return [pending] + messages
+    document.note_pending(pending)
+    topic += pending
+    return [topic] + messages
 
 contents.arguments = (0, 1, 1)
 contents.options = {'depth': directives.nonnegative_int,
