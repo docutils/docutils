5901ede9302176baef4080cad8fb49e7ac7a9e87
mmgilbe <mmgilbe@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-03-22 00:20:37 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1852 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/mmgilbe/rst.py
+++ sandbox/mmgilbe/rst.py
@@ -59,14 +59,12 @@ class MoinWriter(html4css1.Writer):
         
 
 class Parser:
-    """ Parse RST via "docutils". """
-
     def __init__(self, raw, request, **kw):
         self.raw = raw
         self.request = request
         self.form = request.form
-        self._ = request.getText
-
+        # self._ = request.getText
+        
     def format(self, formatter):
         text = publish_string(source = self.raw, 
                               writer = MoinWriter(formatter, self.request),
@@ -81,19 +79,160 @@ class MoinTranslator(html4css1.HTMLTranslator):
         html4css1.HTMLTranslator.__init__(self, document)
         self.formatter = formatter
         self.request = request
+        self.level = 0
+        self.oldWrite = self.request.write
+        self.request.write = self.faux_write
+    
+    def __del__(self):
+        self.request.write = oldWrite
         
-    def faux_paragraph(*args):
-        return ''
-        
+    def astext(self):
+        self.request.write = self.oldWrite
+        return html4css1.HTMLTranslator.astext(self)
+    
     def faux_write(self, string):
         self.body.append(string)
+
+    def request(self):
+        pass
         
-    # def visit_Text(self, node):
-        # from MoinMoin.parser.wiki import Parser
-        # oldWrite = self.request.write
-        # self.request.write = self.faux_write
-        # wikiparser = Parser(node.astext(), self.request)
-        # self.formatter.paragraph = self.faux_paragraph
-        # wikiparser.format(self.formatter)
-        # self.request.write = oldWrite
-        # # self.body.append(self.formatter.text(node.astext()))
+    def visit_document(self, node):
+        pass
+
+    def depart_document(self, node):
+        pass
+        
+    def visit_section(self, node):
+        self.level += 1
+
+    def depart_section(self, node):
+        self.level -= 1
+
+    def visit_title(self, node):
+        self.request.write(self.formatter.heading(self.level, '', on=1))
+
+    def depart_title(self, node):
+        self.request.write(self.formatter.heading(self.level, '', on=0))
+
+    #
+    # Text markup
+    #
+
+    def visit_emphasis(self, node):
+        self.request.write(self.formatter.emphasis(1))
+
+    def depart_emphasis(self, node):
+        self.request.write(self.formatter.emphasis(0))
+
+    def visit_strong(self, node):
+        self.request.write(self.formatter.strong(1))
+
+    def depart_strong(self, node):
+        self.request.write(self.formatter.strong(0))
+
+    def visit_literal(self, node):
+        self.request.write(self.formatter.code(1))
+
+    def depart_literal(self, node):
+        self.request.write(self.formatter.code(0))
+
+
+    #
+    # Blocks
+    #
+
+    def visit_paragraph(self, node):
+        #if self.topic_class != 'contents':
+        self.request.write(self.formatter.paragraph(1))
+
+    def depart_paragraph(self, node):
+        self.request.write(self.formatter.paragraph(0))
+
+    def visit_literal_block(self, node):
+        self.request.write(self.formatter.preformatted(1))
+
+    def depart_literal_block(self, node):
+        self.request.write(self.formatter.preformatted(0))
+
+
+    #
+    # Simple Lists
+    #
+
+    def visit_bullet_list(self, node):
+        self.request.write(self.formatter.bullet_list(1))
+
+    def depart_bullet_list(self, node):
+        self.request.write(self.formatter.bullet_list(0))
+
+    def visit_enumerated_list(self, node):
+        self.request.write(self.formatter.number_list(1, start=node.get('start', None)))
+
+    def depart_enumerated_list(self, node):
+        self.request.write(self.formatter.number_list(0))
+
+    def visit_list_item(self, node):
+        self.request.write(self.formatter.listitem(1))
+
+    def depart_list_item(self, node):
+        self.request.write(self.formatter.listitem(0))
+
+
+    #
+    # Definition List
+    #
+
+    def visit_definition_list(self, node):
+        self.request.write(self.formatter.definition_list(1))
+
+    def visit_definition_list_item(self, node):
+        pass
+
+    def visit_term(self, node):
+        self.request.write('<dt>')
+
+    def depart_term(self, node):
+        self.request.write('</dt>')
+
+    def visit_definition(self, node):
+        self.request.write('<dd>')
+
+    def depart_definition(self, node):
+        self.request.write('</dd>')
+
+    def depart_definition_list(self, node):
+        self.request.write(self.formatter.definition_list(0))
+
+
+    #
+    # Block Quote
+    #
+
+    def visit_block_quote(self, node):
+        self.request.write(self.formatter.definition_list(1))
+
+    def depart_block_quote(self, node):
+        self.request.write(self.formatter.definition_list(0))
+
+
+    #
+    # Admonitions
+    #
+
+    def visit_warning(self, node):
+        self.request.write(self.formatter.highlight(1))
+
+    def depart_warning(self, node):
+        self.request.write(self.formatter.highlight(0))
+
+
+    #
+    # Misc
+    #
+
+    # def visit_system_message(self, node):
+        # self.request.write(self.formatter.highlight(1))
+        # self.request.write('[%s]' % node.astext())
+        # self.request.write(self.formatter.highlight(0))
+
+
