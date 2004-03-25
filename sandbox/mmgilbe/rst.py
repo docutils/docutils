4a60a61db9ecdd80648a203dfbe059e98515b71b
mmgilbe <mmgilbe@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-03-25 20:55:13 +0000

    *** empty log message ***
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1893 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/mmgilbe/rst.py
+++ sandbox/mmgilbe/rst.py
@@ -25,6 +25,7 @@ import docutils
 from docutils import core, frontend, nodes, utils, writers, languages
 from docutils.core import publish_string
 from docutils.writers import html4css1
+from docutils.nodes import fully_normalize_name
 
 
 def html_escape_unicode(node):
@@ -42,13 +43,19 @@ class MoinWriter(html4css1.Writer):
     output = None
     
     def wiki_resolver(self, node):
-        # If the node has an 'id' attribute then it is an implicit hyperlink
-        # that we shouldn't make into a moin link.
-        if 'id' in node.attributes:
+        if getattr(node, 'indirect_reference_name', None):
+            node['refuri'] = node.indirect_reference_name
+            return 1
+        elif 'id' in node.attributes:
+            # I'm pretty sure the first test should catch any targets or
+            # references with the "id" attribute. Therefore, if we get to here
+            # its probably an internal link that didn't work so we let it go
+            # through as an error.
             return 0
         node['refuri'] = node['refname']
         del node['refname']
-        return '1'
+        self.nodes.append(node)
+        return 1
     
     wiki_resolver.priority = 001
     
@@ -65,13 +72,15 @@ class MoinWriter(html4css1.Writer):
         self.wikiparser = Parser('', self.request)
         self.wikiparser.formatter = self.formatter
         self.wikiparser.hilite_re = None
+        self.nodes = []
         
         
     def translate(self):
         visitor = MoinTranslator(self.document, 
                                  self.formatter, 
                                  self.request,
-                                 self.wikiparser)
+                                 self.wikiparser,
+                                 self)
         self.document.walkabout(visitor)
         self.visitor = visitor
         self.output = html_escape_unicode(visitor.astext())
@@ -94,7 +103,7 @@ class Parser:
 
 class MoinTranslator(html4css1.HTMLTranslator):
 
-    def __init__(self, document, formatter, request, parser):
+    def __init__(self, document, formatter, request, parser, writer):
         html4css1.HTMLTranslator.__init__(self, document)
         self.formatter = formatter
         self.request = request
@@ -109,6 +118,7 @@ class MoinTranslator(html4css1.HTMLTranslator):
         # When we use MoinMoin to interpret a MoinMoin refuri we want to strip
         # the paragraph tags to keep the correct formatting.
         self.strip_paragraph = 0
+        self.writer = writer
         
     def astext(self):
         self.request.write = self.old_write
@@ -126,54 +136,42 @@ class MoinTranslator(html4css1.HTMLTranslator):
                 string = string[:-1]
         self.body.append(string)
         
-    def visit_section(self, node):
-        self.level += 1
-
-    def depart_section(self, node):
-        self.level -= 1
-
-    def visit_title(self, node):
-        self.request.write(self.formatter.heading(self.level, '', on=1))
-
-    def depart_title(self, node):
-        self.request.write(self.formatter.heading(self.level, '', on=0))
+    # def visit_section(self, node):
+        # self.level += 1
+    # 
+    # def depart_section(self, node):
+        # self.level -= 1
+# 
+    # def visit_title(self, node):
+        # self.request.write(self.formatter.heading(self.level, '', on=1))
+        # 
+    # def depart_title(self, node):
+        # self.request.write(self.formatter.heading(self.level, '', on=0))
         
     def visit_reference(self, node):
+        target = None
         if 'refuri' in node.attributes:
-            # Set this if our test handles the node
-            handled = 0
-            # We don't want these pieces wrapped in <p> tags, I think.
-            self.strip_paragraph = 1
-            # Unless the user has explicitly added the type of the uri then we
-            # let MoinMoin handle it. Also explicitly handle interwiki links
-            if (node['refuri'].find('wiki:') != -1) or \
-               (':' not in node['refuri']):
-                # The node should have a whitespace normalized name if the
-                # docutlis restructured text parser would normally fully
-                # normalize the name. 
-                # if 'name' in node.attributes and \
-                # node['name'].lower() == node['refuri'].lower():
-                if 'origuri' in node.attributes and len(node['origuri']):
-                    target = node['origuri']
-                elif 'name' in node.attributes:
-                    target = node['name']
-                else:
-                    target = node['refuri']
-                # Pass targets with ":" in them verbatim, otherwise wrap the
-                # link to let MoinMoin know we really mean it is a link.
-                if ':' in target:
-                    self.wikiparser.raw = '[%s %s]' % (target, 
-                                                         node.astext())
-                else:
-                    self.wikiparser.raw = '[:%s: %s]' % (target, 
-                                                         node.astext())
+            if ('name' in node.attributes and 
+                fully_normalize_name(node['name']) == node['refuri']):
+                target = ':%s:' % (node['name'])
+            elif node['refuri'].find('wiki:') != -1:
+                target = node['refuri']
+            # The node should have a whitespace normalized name if the
+            # docutlis restructured text parser would normally fully
+            # normalize the name. 
+            else:
+                target = ':%s:' % (node['refuri'])
+            
+            if target:
+                self.strip_paragraph = 1
+                self.wikiparser.raw = '[%s %s]' % (target, 
+                                                   node.astext())
                 self.wikiparser.format(self.formatter)
-                handled = 1
-            self.strip_paragraph = 0
-            if handled:
+                self.strip_paragraph = 0
                 raise docutils.nodes.SkipNode
                 return
         html4css1.HTMLTranslator.visit_reference(self, node)
+
 
     #
     # Text markup
@@ -207,7 +205,6 @@ class MoinTranslator(html4css1.HTMLTranslator):
 
     def depart_literal_block(self, node):
         self.request.write(self.formatter.preformatted(0))
-
 
     #
     # Simple Lists
