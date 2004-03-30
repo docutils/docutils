debba51708ad492fc38abf109c3081b09d33f78d
fdrake <fdrake@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-03-30 22:17:25 +0000

    - allow RCS/CVS $keywords$ to be handled even if there is other text in the   relevant docinfo fields   (closes SF bug #926198) - make the tests of the RCS/CVS $keyword$ handling less fragile in the face   of non-default -k options   (closes SF bug #921085)
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@1898 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/transforms/frontmatter.py
+++ docutils/docutils/transforms/frontmatter.py
@@ -334,10 +334,10 @@ class DocInfo(Transform):
         return 1
 
     rcs_keyword_substitutions = [
-          (re.compile(r'\$' r'Date: (\d\d\d\d)/(\d\d)/(\d\d) [\d:]+ \$$',
+          (re.compile(r'\$' r'Date: (\d\d\d\d)/(\d\d)/(\d\d) [\d:]+ \$',
                       re.IGNORECASE), r'\1-\2-\3'),
-          (re.compile(r'\$' r'RCSfile: (.+),v \$$', re.IGNORECASE), r'\1'),
-          (re.compile(r'\$[a-zA-Z]+: (.+) \$$'), r'\1'),]
+          (re.compile(r'\$' r'RCSfile: (.+),v \$', re.IGNORECASE), r'\1'),
+          (re.compile(r'\$[a-zA-Z]+: (.+) \$'), r'\1'),]
 
     def extract_authors(self, field, name, docinfo):
         try:
