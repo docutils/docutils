8fec562620c72224d1cd82aafaf114c005d7fe6c
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-04-27 18:46:34 +0000

    Added pseudo-XML test.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@1995 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_writers/test_pseudoxml.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+# Author: Felix Wiemann
+# Contact: Felix_Wiemann@ososo.de
+# Revision: $ $
+# Date: $ $
+# Copyright: This module has been placed in the public domain.
+
+"""
+Test for pseudo-XML writer.
+"""
+
+from __init__ import DocutilsTestSupport
+
+def suite():
+    s = DocutilsTestSupport.PublishTestSuite('pseudoxml')
+    s.generateTests(totest)
+    return s
+
+totest = {}
+
+totest['basic'] = [
+# input
+["""\
+This is the title
+=================
+
+This is a paragraph.
+
+----------
+
+This is another paragraph.
+
+A subsection
+------------
+
+Foo.
+""",
+# output
+"""\
+<document id="this-is-the-title" name="this is the title" source="<string>">
+    <title>
+        This is the title
+    <paragraph>
+        This is a paragraph.
+    <transition>
+    <paragraph>
+        This is another paragraph.
+    <section id="a-subsection" name="a subsection">
+        <title>
+            A subsection
+        <paragraph>
+            Foo.
+"""]
+]
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
