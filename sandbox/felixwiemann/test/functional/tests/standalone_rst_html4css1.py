57c33a827325099d0db4a1f1b5aa1da58596d0e8
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-06-20 17:13:11 +0000

    added configuration files
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@2326 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/felixwiemann/test/functional/tests/standalone_rst_html4css1.py
@@ -0,0 +1,8 @@
+# Source and destination file names.
+test_source = "standalone_rst_html4css1.txt"
+test_destination = "standalone_rst_html4css1.html"
+
+# Keyword parameters passed to publish_string.
+reader_name = "standalone"
+parser_name = "rst"
+writer_name = "html4css1"
