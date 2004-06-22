78535b3155e7cb65d4d3fb5dcc8c244de14b90e2
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-06-22 20:15:10 +0000

    one test case per file
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@2355 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/felixwiemann/test/test_functional.py
+++ sandbox/felixwiemann/test/test_functional.py
@@ -6,26 +6,28 @@
 # Date: $Date$
 # Copyright: This module has been placed in the public domain.
 
-"""Perform the tests with the data in the functional/ directory.
+"""Perform tests with the data in the functional/ directory.
 
-Read the README.txt files for details on how this is done.
-
-TODO:
-One test per file, not one test for all files.
-Use publish_file?
-Add an option to copy files from output/ to expected/?
-"""
+Read README.txt for details on how this is done."""
 
 import docutils.core
 import os
 import os.path
 import unittest
 import difflib
+import DocutilsTestSupport
 
 datadir = 'functional'
 """The directory to store the data needed for the functional tests."""
 
-class TestFunctional(unittest.TestCase):
+class FunctionalTestSuite(DocutilsTestSupport.CustomTestSuite):
+
+    """Test suite containing test cases for all config files."""
+
+    def __init__(self):
+        """Process all config files in functional/tests/."""
+        DocutilsTestSupport.CustomTestSuite.__init__(self)
+        os.path.walk(os.path.join(datadir, 'tests'), self.walker, None)
     
     def walker(self, dummy, dirname, names):
         """Process all config files among ``names`` in ``dirname``.
@@ -33,70 +35,92 @@ class TestFunctional(unittest.TestCase):
         This is a helper function for os.path.walk.  A config file is
         a Python file (*.py) which sets several veriables."""
         
-        for i in filter(lambda x: x.endswith('.py') and not x.startswith('_'),
-                        names):
-            configfile = os.path.join(dirname, i)
-            """Name of current config file to process."""
+        for i in filter(lambda x: x.endswith('.py') and not
+                        x.startswith('_'), names):
             
-            params = {'settings_overrides': {}}
-            """Keyword parameters for publish_file."""
-            # Note that settings_overrides has been initialized to an
-            # empty dictionary.
-
-            # Read the variables set in the default config file and in
-            # the current config file into params.
-            execfile(os.path.join(datadir, 'tests', '_default.py'), params)
-            execfile(configfile, params)
+            c = os.path.join(dirname, i)
+            """Full path of configuration file."""
             
-            assert params.has_key('test_source'),\
-                   "No 'test_source' supplied in " + configfile
-            assert params.has_key('test_destination'),\
-                   "No 'test_destination' supplied in " + configfile
+            self.addTestCase(
+                FunctionalTestCase, 'test', None, None, id=c, configfile=c)
+            
+class FunctionalTestCase(DocutilsTestSupport.CustomTestCase):
 
-            # Set source_path and destination_path if not given.
-            params.setdefault(
-                'source_path',
-                os.path.join(datadir, 'input', params['test_source']))
-            params.setdefault(
-                'destination_path',
-                os.path.join(datadir, 'output', params['test_destination']))
-            expected_path = os.path.join(datadir, 'expected',
-                                         params['test_destination'])
+    """Test case for one config file."""
 
-            # test_source and test_destination aren't needed anymore.
-            del params['test_source']
-            del params['test_destination']
+    def __init__(self, *args, **kwargs):
+        """Set self.configfile, pass arguments to parent __init__."""
+        self.configfile = kwargs['configfile']
+        del kwargs['configfile']
+        self.shortDescription = lambda: ('Functional test for ' +
+                                         self.configfile)
+        DocutilsTestSupport.CustomTestCase.__init__(self, *args, **kwargs)
 
-            # Delete private stuff like params['__builtins__'].
-            for key in params.keys():
-                if key.startswith('_'):
-                    del params[key]
+    def test(self):
 
-            # Get output and write it to the output/ directory.
-            output = docutils.core.publish_file(**params)
+        """Process self.configfile."""
+    
+        params = {'settings_overrides': {}}
+        """Keyword parameters for publish_file."""
+        # Note that settings_overrides has been initialized to an
+        # empty dictionary.
 
-            # Get the expected output *after* writing the actual output.
-            self.assert_(os.access(expected_path, os.R_OK),\
-                         'Cannot find expected output at\n' + expected_path)
-            expected = open(expected_path).read()
-            # Generate diff if unified_diff available.
-            if hasattr(difflib, 'unified_diff'):
-                diff = ('Functional test failed:\n\n' +
-                        ''.join(difflib.unified_diff(
-                    expected.splitlines(1),
-                    output.splitlines(1),
-                    expected_path,
-                    params['destination_path'])))
-            else:
-                diff = ('Functional test failed. '
-                        'Please compare the following files:\n%s\n%s\n' %
-                        (expected_path, params['destination_path']))
-            self.assertEqual(output, expected, diff)
+        # Read the variables set in the default config file and in
+        # the current config file into params.
+        execfile(os.path.join(datadir, 'tests', '_default.py'), params)
+        execfile(self.configfile, params)
 
-    def test_functional(self):
-        """Process all config files in functional/tests/."""
+        assert params.has_key('test_source'),\
+               "No 'test_source' supplied in " + self.configfile
+        assert params.has_key('test_destination'),\
+               "No 'test_destination' supplied in " + self.configfile
+
+        # Set source_path and destination_path if not given.
+        params.setdefault(
+            'source_path',
+            os.path.join(datadir, 'input', params['test_source']))
+        params.setdefault(
+            'destination_path',
+            os.path.join(datadir, 'output', params['test_destination']))
+        """Path for actual output."""
         
-        os.path.walk(os.path.join(datadir, 'tests'), self.walker, None)
+        expected_path = os.path.join(datadir, 'expected',
+                                     params['test_destination'])
+        """Path for expected output."""
+
+        # test_source and test_destination aren't needed anymore.
+        del params['test_source']
+        del params['test_destination']
+
+        # Delete private stuff like params['__builtins__'].
+        for key in params.keys():
+            if key.startswith('_'):
+                del params[key]
+
+        # Get output.  (Automatically written to the output/ directory
+        # by publish_file.)
+        output = docutils.core.publish_file(**params)
+
+        # Get the expected output *after* writing the actual output.
+        self.assert_(os.access(expected_path, os.R_OK),\
+                     'Cannot find expected output at\n' + expected_path)
+        expected = open(expected_path).read()
+        # Generate diff if unified_diff available.
+        if hasattr(difflib, 'unified_diff'):
+            diff = ('Functional test failed:\n\n' +
+                    ''.join(difflib.unified_diff(
+                expected.splitlines(1),
+                output.splitlines(1),
+                expected_path,
+                params['destination_path'])))
+        else:
+            diff = ('Functional test failed. '
+                    'Please compare the following files:\n%s\n%s\n' %
+                    (expected_path, params['destination_path']))
+        self.assertEqual(output, expected, diff)
+
+def suite():
+    return FunctionalTestSuite()
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(defaultTest='suite')
