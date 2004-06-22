f1756fef8810d0bd379af86e53340273ad4f6def
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-06-22 21:24:21 +0000

    moved functional tests to main tree
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@2357 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/functional/tests/_default.py
@@ -0,0 +1,5 @@
+# Default settings for all tests.
+
+settings_overrides['report_level'] = 2
+settings_overrides['halt_level'] = 5
+settings_overrides['warning_stream'] = ''
---
+++ docutils/test/functional/tests/standalone_rst_latex.py
@@ -0,0 +1,8 @@
+# Source and destination file names.
+test_source = "standalone_rst_latex.txt"
+test_destination = "standalone_rst_latex.tex"
+
+# Keyword parameters passed to publish_file.
+reader_name = "standalone"
+parser_name = "rst"
+writer_name = "latex"
