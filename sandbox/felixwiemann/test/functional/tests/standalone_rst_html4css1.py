f16b78f4baa73f65e9f164ba3e07d4633050f649
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-06-22 17:07:23 +0000

    use publish_file
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@2352 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/felixwiemann/test/functional/tests/standalone_rst_html4css1.py
+++ sandbox/felixwiemann/test/functional/tests/standalone_rst_html4css1.py
@@ -2,7 +2,7 @@
 test_source = "standalone_rst_html4css1.txt"
 test_destination = "standalone_rst_html4css1.html"
 
-# Keyword parameters passed to publish_string.
+# Keyword parameters passed to publish_file.
 reader_name = "standalone"
 parser_name = "rst"
 writer_name = "html4css1"
--- sandbox/felixwiemann/test/functional/tests/standalone_rst_latex.py
+++ sandbox/felixwiemann/test/functional/tests/standalone_rst_latex.py
@@ -2,7 +2,7 @@
 test_source = "standalone_rst_latex.txt"
 test_destination = "standalone_rst_latex.tex"
 
-# Keyword parameters passed to publish_string.
+# Keyword parameters passed to publish_file.
 reader_name = "standalone"
 parser_name = "rst"
 writer_name = "latex"
