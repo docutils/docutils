b4b6a4720523800128c876f5f982a0905b0e5742
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2004-07-07 07:56:26 +0000

    Indent literal-blocks.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@2423 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/latex2e.py
+++ docutils/docutils/writers/latex2e.py
@@ -1532,29 +1532,31 @@ class LaTeXTranslator(nodes.NodeVisitor):
               # in case of a parsed-literal containing just a "**bold**" word:
               and isinstance(node[0], nodes.Text)):
             self.verbatim = 1
-            self.body.append('\\begin{verbatim}\n')
+            self.body.append('\\begin{quote}\\begin{verbatim}\n')
         else:
             self.literal_block = 1
             self.insert_none_breaking_blanks = 1
             if self.active_table.is_open():
                 self.body.append('\n{\\ttfamily \\raggedright \\noindent\n')
-            else:    
-                self.body.append('\\begin{ttfamily}')
-                self.body.append('\\begin{flushleft}\n')
+            else:
+                # no quote inside tables, to avoid vertical sppace between
+                # table border and literal block.
+                # BUG: fails if normal text preceeds the literal block.
+                self.body.append('\\begin{quote}')
+                self.body.append('{\\ttfamily \\raggedright \\noindent\n')
             # * obey..: is from julien and never worked for me (grubert).
             #   self.body.append('{\\obeylines\\obeyspaces\\ttfamily\n')
 
     def depart_literal_block(self, node):
         if self.verbatim:
-            self.body.append('\n\\end{verbatim}\n')
+            self.body.append('\n\\end{verbatim}\\end{quote}\n')
             self.verbatim = 0
         else:
             if self.active_table.is_open():
                 self.body.append('\n}\n')
             else:
                 self.body.append('\n')
-                self.body.append('\\end{flushleft}')
-                self.body.append('\\end{ttfamily}\n')
+                self.body.append('}\\end{quote}\n')
             self.insert_none_breaking_blanks = 0
             self.literal_block = 0
             # obey end: self.body.append('}\n')
