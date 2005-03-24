9a5bfa649b0a740f63a775b28e74375c40201fd5
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-03-24 16:20:26 +0000

    added -f option to prevent feedback
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3109 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/felixwiemann/fsfsbackup.sh
+++ sandbox/felixwiemann/fsfsbackup.sh
@@ -6,70 +6,87 @@
 # Date: $Date$
 # Copyright: This file has been placed in the public domain.
 
+# Options:
+#
+# -f  Do not print feedback.
+
 set -e
+
+trap "echo; echo Exiting with error." 0 1 2 3 15
+
+printfeedback=1
+test "$1" == -f && printfeedback=
+
+function feedback() {
+    test "$printfeedback" && echo "$@" || true
+}
 
 function do_backup() {
     # If any of the tests fails, the script terminates silently.
-    echo "Checking that all necessary variables are set."
+    feedback "Checking that all necessary variables are set."
     test -n "$BACKUPDIR"
     test -n "$HOST"
     test -n "$REMOTEDIR"
     if test ! -d "$BACKUPDIR"; then
-        echo "Creating backup directory $BACKUPDIR."
+        feedback "Creating backup directory $BACKUPDIR."
         mkdir --parents --verbose "$BACKUPDIR"
     fi
     if test ! -f "$BACKUPDIR/db/fs-type"; then
-        echo
+        feedback
         echo "$BACKUPDIR/db/fs-type not found."
-        echo Please fetch the first copy of the repository database yourself.
-        echo I only assist in doing incremental backups of repository databases.
+        echo "Please fetch the first copy of the repository database yourself."
+        echo "I only assist in doing incremental backups of repository databases."
         exit 1
     fi
-    echo "Checking that repository format is FSFS."
+    feedback "Checking that repository format is FSFS."
     test "`cat "$BACKUPDIR/db/fs-type"`" == fsfs
-    echo "Changing to database directory $BACKUPDIR/db."
+    feedback "Changing to database directory $BACKUPDIR/db."
     cd "$BACKUPDIR/db"
-    echo "Getting local current revision number."
+    feedback "Getting local current revision number."
     LOCALREVNUM="`cat current | sed 's/ .*//'`"
-    echo "Checking that current.new doesn't exist."
+    feedback "Checking that current.new doesn't exist."
     if test -f current.new; then
-        echo
+        feedback
         echo "Make sure that no other instance of this script"
         echo "is running and delete the following file:"
         echo "$BACKUPDIR/db/current.new"
         exit 2
     fi
-    echo "Getting remote 'current' file."
+    feedback "Getting remote 'current' file."
     ssh "$HOST" "cat '$REMOTEDIR/db/current'" > current.new
-    echo "Getting remote current revision number."
+    feedback "Getting remote current revision number."
     REMOTEREVNUM="`cat current.new | sed 's/ .*//'`"
-    echo "Checking that we got a response from the server."
+    feedback "Checking that we got a response from the server."
     test -n "$REMOTEREVNUM"
     if ! test "$LOCALREVNUM" -le "$REMOTEREVNUM"; then
-        echo
+        feedback
         echo "ERROR: Local revision number ($LOCALREVNUM) greater"
         echo "       than remote revision number ($REMOTEREVNUM)."
         echo "Wrong backup directory or changed repository?"
         exit 2
     fi
     if test "$LOCALREVNUM" -eq "$REMOTEREVNUM"; then
-        echo "No backup needed; at revision $LOCALREVNUM."
-        echo "Removing 'current.new'."
+        feedback "No backup needed; at revision $LOCALREVNUM."
+        feedback "Removing 'current.new'."
         rm current.new
-        echo "Done."
-        exit 0
+        feedback "Done."
+        return
     fi
     LOCALREVNUM="$[$LOCALREVNUM+1]"
-    echo "Backing up from revision $LOCALREVNUM to revision $REMOTEREVNUM."
+    feedback "Backing up from revision $LOCALREVNUM to revision $REMOTEREVNUM."
+    verbose=
+    test "$1" != -f && verbose=-v
     ssh "$HOST" "
         set -e;
         cd $REMOTEDIR/db/;
         nice -n 10 tar cf - \`seq -f revs/%g $LOCALREVNUM $REMOTEREVNUM\` \`seq -f revprops/%g $LOCALREVNUM $REMOTEREVNUM\` | nice -n 10 bzip2 -c" \
-        | tar xjvf -
-    echo "Renaming 'current.new' to 'current'."
+        | tar $verbose -xjf -
+    feedback "Renaming 'current.new' to 'current'."
     mv current.new current
-    echo "Done."
+    feedback "Done."
 }
 
-echo 'Reading ~/.fsfsbackup.'
+feedback 'Reading ~/.fsfsbackup.'
 source ~/.fsfsbackup
+feedback 'Finished.'
+trap - 0 1 2 3 15
