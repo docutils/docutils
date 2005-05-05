9b4c056ed9a24887fa7a3757c23ce0460de2e360
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-05-05 05:46:52 +0000

    made S5 writer work better with reST: instead of |s5 title|, use the document title; instead of |s5 location|, use the document footer ("footer" directive); instead of |s5 theme|, use a runtime setting or command-line option.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3301 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/cliechti/slideshow/s5.py
+++ sandbox/cliechti/slideshow/s5.py
@@ -22,12 +22,9 @@ class Writer(html4css1.Writer):
     settings_spec = html4css1.Writer.settings_spec + (
         'S5 Slideshow Specific Options',
         None,
-        (('Specify an S5 theme directory.  Overrides the theme selected '
-          'by the document.  The default is "ui" if this option is absent '
-          'and the document does not redefine it.',
+        (('Specify an S5 theme directory.  The default is "ui".',
           ['--theme'],
-          {'default': None, 'metavar': '<file>'}),
-    ))
+          {'default': 'ui', 'metavar': '<file>'})))
 
     config_section = 's5 writer'
     config_section_dependencies = ('writers', 'html4css1 writer')
@@ -39,67 +36,72 @@ class Writer(html4css1.Writer):
 
 class HTMLTranslator(html4css1.HTMLTranslator):
 
-    def __init__(self, *args):
-        html4css1.HTMLTranslator.__init__(self, *args)
-        #get theme from document substitutions, or use the default
-        if self.document.substitution_defs.has_key('s5 theme'):
-            theme = self.document.substitution_defs['s5 theme'].astext()
-        else:
-            theme = 'ui'
-        #the command line overrides the theme setting
-        if self.document.settings.theme is not None:
-            theme = self.document.settings.theme
-        #insert S5 styleshet and script stuff in the HTML header info
-        self.body_prefix.insert(0, '''\
-<meta name="version" content="S5 1.0" />
+    s5_stylesheet_template = '''\
 <link rel="stylesheet" href="%(s5_theme_dir)s/slides.css" type="text/css" media="projection" id="slideProj" />
 <link rel="stylesheet" href="%(s5_theme_dir)s/opera.css" type="text/css" media="projection" id="operaFix" />
 <link rel="stylesheet" href="%(s5_theme_dir)s/print.css" type="text/css" media="print" id="slidePrint" />
-<script src="%(s5_theme_dir)s/slides.js" type="text/javascript"></script>\n''' % {
-'s5_theme_dir': theme,
-        })
-
-    def visit_document(self, node):
-        #~ # empty or untitled document?
-        #~ if not len(node) or not isinstance(node[0], nodes.title):
-            #~ # for XHTML conformance, modulo IE6 appeasement:
-            #~ self.head.insert(0, '<title></title>\n')
-
-        #try to use substitution to set title
-        title = ''
-        if self.document.substitution_defs.has_key('s5 title'):
-            title = self.document.substitution_defs['s5 title'].astext()
-        else:
-            #try to get the presentation title
-            if isinstance(node[0], nodes.title):
-                title = node[0].astext()
-        #additional footer information can be set trough document substitutions
-        if self.document.substitution_defs.has_key('s5 location'):
-            location = '<h2>%s</h2>' % self.document.substitution_defs['s5 location'].astext()
-        else:
-            location = ''
-        #insert the slide layout master once in the HTML body
-        self.body_prefix.append('''\
+<script src="%(s5_theme_dir)s/slides.js" type="text/javascript"></script>\n'''
+    layout_template = '''\
 <div class="layout">
-    <div id="currentSlide"></div>
-    <div id="header"></div>
-    <div id="footer">
-        %s%s
-        <div id="controls"></div>
-    </div>
-</div>\n''' % (title and '<h1>%s</h1>' % title or '', location)
-        )
-        #set document title. lets hope the user did not use a document title
-        #as we would get two of these in the HTML headers...
-        self.head.append('<title>%s</title>\n' % title)
+<div id="currentSlide"></div>
+<div id="header">
+%s
+</div>
+<div id="footer">
+%s%s
+<div id="controls"></div>
+</div>
+</div>\n'''
+        
+    def __init__(self, *args):
+        html4css1.HTMLTranslator.__init__(self, *args)
+        #insert S5-specific stylesheet and script stuff:
+        theme = self.document.settings.theme
+        self.stylesheet.append(self.s5_stylesheet_template
+                               % {'s5_theme_dir': theme})
+        self.add_meta('<meta name="version" content="S5 1.0" />\n')
+        self.s5_footer = []
+        self.s5_header = []
+        self.section_count = 0
 
     def depart_document(self, node):
+        header = ''.join(self.s5_header)
+        footer = ''.join(self.s5_footer)
+        title = ''.join(self.html_title).replace('<h1 class="title">', '<h1>')
+        layout = self.layout_template % (header, title, footer)
         self.fragment.extend(self.body)
-        self.body_prefix.append(
-            self.starttag(node, 'div', CLASS='presentation'))
+        self.body_prefix.extend(layout)
+        self.body_prefix.append('<div class="presentation">\n')
+        self.body_prefix.append(self.starttag(node, 'div', CLASS='slide'))
         self.body_suffix.insert(0, '</div>\n')
+        # skip content-type meta tag with interpolated charset value:
+        self.html_head.extend(self.head[1:])
+        self.html_body.extend(self.body_prefix[1:] + self.body_pre_docinfo
+                              + self.docinfo + self.body
+                              + self.body_suffix[:-1])
+
+#     def visit_footer(self, node):
+#         # check for one paragraph?
+        
+    def depart_footer(self, node):
+        start = self.context.pop()
+        self.s5_footer.append('<h2>')
+        self.s5_footer.extend(self.body[start:])
+        self.s5_footer.append('</h2>')
+        del self.body[start:]
+
+    def depart_header(self, node):
+        start = self.context.pop()
+        header = ['<div id="header">\n']
+        header.extend(self.body[start:])
+        header.append('\n</div>\n')
+        del self.body[start:]
+        self.s5_header.extend(header)
 
     def visit_section(self, node):
+        if not self.section_count:
+            self.body.append('\n</div>\n')
+        self.section_count += 1
         self.section_level += 1
         if self.section_level > 1:
             # dummy for matching div's
@@ -116,16 +118,14 @@ def handout_directive(name, arguments, options, content, lineno,
     if not text:
         warning = state_machine.reporter.warning(
             'The handout block is empty; content required.',
-            '',
             nodes.literal_block(block_text, block_text), line=lineno)
         return [warning]
     node = nodes.section(text)
-    #~ if options.has_key('class'):
-        #~ node.set_class(options['class'])
-    node.set_class('handout')
+    node['classes'] += options.get('class', [])
+    node['classes'].append('handout')
     state.nested_parse(content, content_offset, node)
     return [node]
 
 handout_directive.content = 1
-#~ handout_directive.options = {'class': directives.class_option}
+handout_directive.options = {'class': directives.class_option}
 directives.register_directive('handout', handout_directive)
