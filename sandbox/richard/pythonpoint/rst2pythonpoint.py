da1e2d63bbda1347c8a53417ab0c8b049b79a0c6
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-06-15 22:57:26 +0000

    added front end for PythonPoint writer
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3481 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/richard/pythonpoint/rst2pythonpoint.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Author: Felix Wiemann
+# Contact: Felix_Wiemann@ososo.de
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing PythonPoint XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates PythonPoint documents from standalone reStructuredText '
+               'sources.  ' + default_description)
+
+publish_cmdline(writer_name='pythonpoint', description=description)
