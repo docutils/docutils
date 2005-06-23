be126154fcfeb99b67e784c97e36a4a3f30e7c82
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-06-23 17:56:44 +0000

    separated different types of tests
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@3570 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_writers/test_html4css1_misc.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: Felix Wiemann
+# Contact: Felix_Wiemann@ososo.de
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+Miscellaneous HTML writer tests.
+"""
+
+from __init__ import DocutilsTestSupport
+from docutils import core
+
+
+class EncodingTestCase(DocutilsTestSupport.StandardTestCase):
+
+    def test_xmlcharrefreplace(self):
+        # Test that xmlcharrefreplace is the default output encoding
+        # error handler.
+        settings_overrides={
+            'output_encoding': 'latin1',
+            'stylesheet': '',
+            '_disable_config': 1}
+        result = core.publish_string(
+            'äöü€', writer_name='html4css1',
+            settings_overrides=settings_overrides)
+        self.assert_(result.find('\xe4\xf6\xfc&#8364;') != -1)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
