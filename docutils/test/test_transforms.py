ec3c0d0aa1a5eda38be61835d2956b144466c357
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-06-27 14:21:27 +0000

    pass kwargs as single parameter to add_transform; to not pass on the kwargs to Transform.__init__ but to Transform.apply
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3604 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_transforms.py
+++ docutils/test/test_transforms.py
@@ -19,20 +19,22 @@ class TestTransform(transforms.Transform):
     default_priority = 100
 
     applied = 0
-    def apply(self):
+    
+    def apply(self, **kwargs):
         self.applied += 1
+        self.kwargs = kwargs
 
 class KwargsTestCase(unittest.TestCase):
 
     def test_kwargs(self):
         transformer = transforms.Transformer(utils.new_document('test data'))
-        transformer.add_transform(TestTransform, foo=42)
+        transformer.add_transform(TestTransform, kwargs={'foo': 42})
         transformer.apply_transforms()
         self.assertEqual(len(transformer.applied), 1)
         self.assertEqual(len(transformer.applied[0]), 4)
         transform = transformer.applied[0][0]
         self.assertEqual(transform.__class__, TestTransform)
-        self.assertEqual(transform.data, {'foo': 42})
+        self.assertEqual(transform.kwargs, {'foo': 42})
 
 
 if __name__ == '__main__':
