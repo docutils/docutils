ff4afa8149a46b743a360230eb8dace799711291
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-06-27 21:01:17 +0000

    Pythonicized kwargs --dump-transforms
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3609 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_transforms.py
+++ docutils/test/test_transforms.py
@@ -11,7 +11,6 @@ Test module for transforms/__init__.py.
 """
 
 from docutils import transforms, utils
-
 import unittest
 
 
@@ -30,13 +29,13 @@ class KwargsTestCase(unittest.TestCase):
 
     def test_kwargs(self):
         transformer = transforms.Transformer(utils.new_document('test data'))
-        transformer.add_transform(TestTransform, kwargs={'foo': 42})
+        transformer.add_transform(TestTransform, foo=42)
         transformer.apply_transforms()
         self.assertEqual(len(transformer.applied), 1)
         self.assertEqual(len(transformer.applied[0]), 4)
-        transform = transformer.applied[0][0]
-        self.assertEqual(transform.__class__, TestTransform)
-        self.assertEqual(transform.kwargs, {'foo': 42})
+        transform_record = transformer.applied[0]
+        self.assertEqual(transform_record[1], TestTransform)
+        self.assertEqual(transform_record[3], {'foo': 42})
 
 
 if __name__ == '__main__':
