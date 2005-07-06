98de70e900994cbe0d95170b938ae7a9f7786a77
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-07-06 12:43:05 +0000

    fixed TestMessages priority to conform to docs
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3660 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/transforms/universal.py
+++ docutils/docutils/transforms/universal.py
@@ -133,9 +133,11 @@ class TestMessages(Transform):
 
     """
     Append all post-parse system messages to the end of the document.
+
+    Used for testing purposes.
     """
 
-    default_priority = 890
+    default_priority = 880
 
     def apply(self):
         for msg in self.document.transform_messages:
