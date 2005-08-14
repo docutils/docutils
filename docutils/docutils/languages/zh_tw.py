c59f0219176a4071d695a1855e33bc8ea96cb0f4
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-08-14 16:37:53 +0000

    updated Chinese translations, closing SF patch #1243780; thanks Joe YS Jaw
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@3798 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/languages/zh_tw.py
+++ docutils/docutils/languages/zh_tw.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 # Author: Joe YS Jaw
 # Contact: joeysj@users.sourceforge.net
 # Revision: $Revision$
@@ -17,28 +18,28 @@ __docformat__ = 'reStructuredText'
 
 labels = {
       # fixed: language-dependent
-      'author': u'\u4f5c\u8005', # 'Author',
-      'authors': u'\u4f5c\u8005\u7fa4', # 'Authors',
-      'organization': u'\u7d44\u7e54', # 'Organization',
-      'address': u'\u5730\u5740', # 'Address',
-      'contact': u'\u9023\u7d61', # 'Contact',
-      'version': u'\u7248\u672c', # 'Version',
-      'revision': u'\u4fee\u8a02', # 'Revision',
-      'status': u'\u72c0\u614b', # 'Status',
-      'date': u'\u65e5\u671f', # 'Date',
-      'copyright': u'\u7248\u6b0a', # 'Copyright',
-      'dedication': u'\u984c\u737b', # 'Dedication',
-      'abstract': u'\u6458\u8981', # 'Abstract',
-      'attention': u'\u6ce8\u610f\uff01', # 'Attention!',
-      'caution': u'\u5c0f\u5fc3\uff01', # 'Caution!',
-      'danger': u'\uff01\u5371\u96aa\uff01', # '!DANGER!',
-      'error': u'\u932f\u8aa4', # 'Error',
-      'hint': u'\u63d0\u793a', # 'Hint',
-      'important': u'\u91cd\u8981', # 'Important',
-      'note': u'\u8a3b\u89e3', # 'Note',
-      'tip': u'\u79d8\u8a23', # 'Tip',
-      'warning': u'\u8b66\u544a', # 'Warning',
-      'contents': u'\u76ee\u9304' # 'Contents'
+      'author': u'\u4f5c\u8005', # '作者' <-- Chinese word
+      'authors': u'\u4f5c\u8005\u7fa4', # '作者群',
+      'organization': u'\u7d44\u7e54', # '組織',
+      'address': u'\u5730\u5740', # '地址',
+      'contact': u'\u9023\u7d61', # '連絡',
+      'version': u'\u7248\u672c', # '版本',
+      'revision': u'\u4fee\u8a02', # '修訂',
+      'status': u'\u72c0\u614b', # '狀態',
+      'date': u'\u65e5\u671f', # '日期',
+      'copyright': u'\u7248\u6b0a', # '版權',
+      'dedication': u'\u984c\u737b', # '題獻',
+      'abstract': u'\u6458\u8981', # '摘要',
+      'attention': u'\u6ce8\u610f\uff01', # '注意！',
+      'caution': u'\u5c0f\u5fc3\uff01', # '小心！',
+      'danger': u'\uff01\u5371\u96aa\uff01', # '！危險！',
+      'error': u'\u932f\u8aa4', # '錯誤',
+      'hint': u'\u63d0\u793a', # '提示',
+      'important': u'\u91cd\u8981', # '注意！',
+      'note': u'\u8a3b\u91cb', # '註釋',
+      'tip': u'\u79d8\u8a23', # '秘訣',
+      'warning': u'\u8b66\u544a', # '警告',
+      'contents': u'\u76ee\u9304' # '目錄'
 } 
 """Mapping of node class name to label text."""
 
@@ -58,7 +59,10 @@ bibliographic_fields = {
       'abstract': 'abstract'}
 """Traditional Chinese to canonical name mapping for bibliographic fields."""
 
-author_separators = [u'\uff1b', u'\uff0c', u'\u3001',
-                     ';', ',']
+author_separators = [';', ',',
+                     u'\uff1b', # '；'
+                     u'\uff0c', # '，'
+                     u'\u3001', # '、'
+                    ]
 """List of separator strings for the 'Authors' bibliographic field. Tried in
 order."""
