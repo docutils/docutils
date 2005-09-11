76465ee3a498fab2a4b5e6fc5a66b1331669e14e
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-09-11 22:23:43 +0000

    added writer_aux module containing Compound transform, which flattens the compound paragraph structure
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@3872 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_transforms/test_writer_aux.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+
+# Author: Felix Wiemann
+# Contact: Felix_Wiemann@ososo.de
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+Test module for writer_aux transforms.
+"""
+
+from docutils.transforms import writer_aux
+from __init__ import DocutilsTestSupport
+from docutils.parsers.rst import Parser
+
+def suite():
+    parser = Parser()
+    s = DocutilsTestSupport.TransformTestSuite(parser)
+    s.generateTests(totest)
+    return s
+
+
+totest = {}
+
+totest['compound'] = ((writer_aux.Compound,), [
+["""\
+.. class:: compound
+
+.. compound::
+
+   .. class:: paragraph1
+
+   Paragraph 1.
+
+   .. class:: paragraph2
+
+   Paragraph 2.
+
+       Block quote.
+""",
+"""\
+<document source="test data">
+    <paragraph classes="paragraph1 compound">
+        Paragraph 1.
+    <paragraph classes="paragraph2 continued">
+        Paragraph 2.
+    <block_quote classes="continued">
+        <paragraph>
+            Block quote.
+"""],
+])
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
