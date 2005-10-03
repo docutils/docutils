1c4b517b8960ea4fc207ffba41bffadd5b9cb819
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-10-03 18:33:46 +0000

    added correct link to stylesheet path for visual checkability of HTML output
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@3919 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/field_name_limit.py
+++ docutils/test/functional/tests/field_name_limit.py
@@ -10,5 +10,4 @@ writer_name = "html"
 # Settings
 settings_overrides['field_name_limit'] = 0 # no limit
 settings_overrides['docinfo_xform'] = 0
-settings_overrides['stylesheet'] = None
-settings_overrides['stylesheet_path'] = "../tools/stylesheets/default.css"
+settings_overrides['stylesheet_path'] = "../docutils/writers/support/html4css1.css"
