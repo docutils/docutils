e4c9cc2da40b5832b4f4da9efb2e1838d94b6f7f
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2005-12-10 02:29:55 +0000

    completed Japanese mappings
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@4167 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/languages/ja.py
+++ docutils/docutils/languages/ja.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Author: Hisashi Morita
 # Contact: hisashim@kt.rim.or.jp
 # Revision: $Revision$
@@ -17,44 +18,44 @@ __docformat__ = 'reStructuredText'
 
 labels = {
       # fixed: language-dependent
-      'author': u'\u8457\u8005',
-      'authors': u'\u8457\u8005',
-      'organization': u'\u7d44\u7e54',
-      'address': u'\u4f4f\u6240',
-      'contact': u'\u9023\u7d61\u5148',
-      'version': u'\u30d0\u30fc\u30b8\u30e7\u30f3',
-      'revision': u'\u30ea\u30d3\u30b8\u30e7\u30f3',
-      'status': u'30b9\u30c6\u30fc\u30bf\u30b9',
-      'date': u'\u65e5\u4ed8',
-      'copyright': u'\u8457\u4f5c\u6a29',
-      'dedication': u'\u732e\u8f9e',
-      'abstract': u'\u6982\u8981',
-      'attention': u'\u6ce8\u76ee\u0021',
-      'caution': u'\u6ce8\u610f\u0021',
-      'danger': u'\u0021\u5371\u967a\u0021',
-      'error': u'\u30a8\u30e9\u30fc',
-      'hint': u'\u30d2\u30f3\u30c8',
-      'important': u'\u91cd\u8981',
-      'note': u'\u5099\u8003',
-      'tip': u'\u0054\u0069\u0070',
-      'warning': u'\ufeff\u8b66\u544a',
-      'contents': u'\u76ee\u6b21'}
+      'author': u'著者',
+      'authors': u'著者',
+      'organization': u'組織',
+      'address': u'住所',
+      'contact': u'連絡先',
+      'version': u'バージョン',
+      'revision': u'リビジョン',
+      'status': u'ステータス',
+      'date': u'日付',
+      'copyright': u'著作権',
+      'dedication': u'献辞',
+      'abstract': u'概要',
+      'attention': u'注目!',
+      'caution': u'注意!',
+      'danger': u'!危険!',
+      'error': u'エラー',
+      'hint': u'ヒント',
+      'important': u'重要',
+      'note': u'備考',
+      'tip': u'通報',
+      'warning': u'警告',
+      'contents': u'目次'}
 """Mapping of node class name to label text."""
 
 bibliographic_fields = {
       # language-dependent: fixed
-      'author (translation required)': 'author',
-      'authors (translation required)': 'authors',
-      'organization (translation required)': 'organization',
-      'address (translation required)': 'address',
-      'contact (translation required)': 'contact',
-      'version (translation required)': 'version',
-      'revision (translation required)': 'revision',
-      'status (translation required)': 'status',
-      'date (translation required)': 'date',
-      'copyright (translation required)': 'copyright',
-      'dedication (translation required)': 'dedication',
-      'abstract (translation required)': 'abstract'}
+      u'著者': 'author',
+      u' n/a': 'authors',
+      u'組織': 'organization',
+      u'住所': 'address',
+      u'連絡先': 'contact',
+      u'バージョン': 'version',
+      u'リビジョン': 'revision',
+      u'ステータス': 'status',
+      u'日付': 'date',
+      u'著作権': 'copyright',
+      u'献辞': 'dedication',
+      u'概要': 'abstract'}
 """Japanese (lowcased) to canonical name mapping for bibliographic fields."""
 
 author_separators = [';', ',']
