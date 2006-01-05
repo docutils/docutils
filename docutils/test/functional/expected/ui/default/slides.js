104971f90ff8cc7b3a4ce31a28379da535d89a42
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-05 19:30:30 +0000

    updated
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4241 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/expected/ui/default/slides.js
+++ docutils/test/functional/expected/ui/default/slides.js
@@ -305,7 +305,7 @@ function clicker(e) {
 		target = window.event.srcElement;
 		e = window.event;
 	} else target = e.target;
-	if (target.getAttribute('href') != null || hasValue(target.rel, 'external') || isParentOrSelf(target, 'controls') || isParentOrSelf(target,'embed') || isParentOrSelf(target,'object')) return true;
+    if (target.href != null || hasValue(target.rel, 'external') || isParentOrSelf(target, 'controls') || isParentOrSelf(target,'embed') || isParentOrSelf(target, 'object')) return true; 
 	if (!e.which || e.which == 1) {
 		if (!incrementals[snum] || incpos >= incrementals[snum].length) {
 			go(1);
