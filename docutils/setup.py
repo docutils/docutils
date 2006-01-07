fb064b4934a1f4ee9c1d481431169c44ecd4bbce
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-07 17:05:13 +0000

    Release 0.4: set version number to 0.4
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4243 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/setup.py
+++ docutils/setup.py
@@ -49,7 +49,7 @@ into useful formats, such as HTML, XML, and LaTeX.  For
 input Docutils supports reStructuredText, an easy-to-read,
 what-you-see-is-what-you-get plaintext markup syntax.""", # wrap at col 60
     'url': 'http://docutils.sourceforge.net/',
-    'version': '0.3.10',
+    'version': '0.4',
     'author': 'David Goodger',
     'author_email': 'goodger@users.sourceforge.net',
     'license': 'public domain, Python, BSD, GPL (see COPYING.txt)',
