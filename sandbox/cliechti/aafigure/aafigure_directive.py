3eb3e7fd8781caeffccf1c183e575ffe2ca09865
cliechti <cliechti@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-09 03:12:17 +0000

    - remove unneeded debug output - don't prefix 'current directory' to paths
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@4255 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/cliechti/aafigure/aafigure_directive.py
+++ sandbox/cliechti/aafigure/aafigure_directive.py
@@ -10,21 +10,20 @@ from docutils.parsers.rst.directives import register_directive
 
 aafigure_counter = 0
 
-def AAFigureDrective (name, arguments, options, content, lineno,
+def AAFigureDrective(name, arguments, options, content, lineno,
                   content_offset, block_text, state, state_machine):
     text = '\n'.join(content)
     
     global aafigure_counter
     aafigure_counter += 1
-    output_path = '.'
-    print output_path
-    output_name = os.path.join(output_path, 'aafigure-%i.svg' % aafigure_counter)
+    output_name = 'aafigure-%i.svg' % aafigure_counter
     
     aaimg = aafigure.AsciiArtImage(text)
     #~ print text
     aaimg.recognize()
     if not options.has_key('scale'):
         options['scale'] = 1
+    
     svgout = svg.SVGOutputVisitor(
         file(output_name, 'w'),
         scale = options['scale']*10,
