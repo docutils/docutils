4fc9b54b044ca78fd4fa2680dd8642dd89f86a00
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-21 17:13:59 +0000

    added uploaddocutils.sh script
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@4304 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/fwiemann/uploaddocutils.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Author: Felix Wiemann
+# Contact: Felix_Wiemann@ososo.de
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This script has been placed in the public domain.
+
+set -e
+
+date="`date --utc --iso-8601`"
+
+if test -z "$1" -o "$1" = -h -o "$1" = --help; then
+    uploaddocutils="`basename "$0"`"
+    echo This script uploads files to http://docutils.sf.net/tmp/ using scp.
+    echo It inserts the current date in the file names and prints the
+    echo resulting URLs.
+    echo
+    echo Usage: "$uploaddocutils" files ...
+    echo
+    echo For example, \""$uploaddocutils" test.tar.gz\" will upload
+    echo test.tar.gz to "<http://docutils.sf.net/tmp/test-$date.tar.gz>".
+    exit 1
+fi
+
+urllist=
+while test -n "$1"; do
+    localfile="$1"
+    remotefile="`basename $1`"
+    # Add date in front of first period or at the end of the file name
+    # if there is no period.
+    remotefile="`echo "$remotefile" | sed 's/\(\.\|$\)/-'"$date"'\1/'`"
+    scp "$localfile" "shell.sourceforge.net:/home/groups/d/do/docutils/htdocs/tmp/$remotefile"
+    urllist="$urllist
+http://docutils.sf.net/tmp/$remotefile"
+    shift
+done
+echo "$urllist"
