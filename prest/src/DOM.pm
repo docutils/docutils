f3e1bea75299b4b7dcb15e0aa71f1841f20e8caa
mnodine <mnodine@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-01-30 03:03:07 +0000

    Updated svn:keywords properties on prest files.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@4329 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- prest/src/DOM.pm
+++ prest/src/DOM.pm
@@ -1,6 +1,6 @@
 package DOM;
 
-# $Id: DOM.pm 768 2006-01-28 03:33:28Z marknodine $
+# $Id$
 # Copyright (C) 2002-2005 Freescale Semiconductor, Inc.
 # Distributed under terms of the GNU General Public License (GPL).
 
