7a47d96c961419eca622426f4fd0d3071a4af077
wiemann <wiemann@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-04-01 00:46:03 +0000

    support multiple revisions (each backported separately)
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@4464 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/infrastructure/dbackport.sh
+++ sandbox/infrastructure/dbackport.sh
@@ -12,32 +12,34 @@
 
 set -e
 
-r="$1"
-
-if test ! "$r"; then
-    echo Usage: "`basename "$0"` <revision>"
+if test -z "$1"; then
+    echo Usage: "`basename "$0"` <revision> [<revision> ...]"
     exit 1
 fi
 
-if test ! "$DOCUTILS_MAINT_BRANCH"; then
-    echo '$DOCUTILS_MAINT_BRANCH must point to the directory of the'
-    echo 'maintenance branch.'
-    exit 1
-fi
+while test -n "$1"; do
+    r="$1"
+    if test ! "$DOCUTILS_MAINT_BRANCH"; then
+        echo '$DOCUTILS_MAINT_BRANCH must point to the directory of the'
+        echo 'maintenance branch.'
+        exit 1
+    fi
 
-cd "$DOCUTILS_MAINT_BRANCH"
-svn revert . -R
-svn up
-svn merge -r"$[$r-1]:$r" ../../trunk/docutils .
-svn diff
-# Python 2.4 is faster and outputs unified diffs for the functional
-# tests, so it comes first.
-nice python2.4 -u test/alltests.py
-nice python2.1 -u test/alltests.py
-nice python2.2 -u test/alltests.py
-nice python2.3 -u test/alltests.py
-echo
-echo Press enter to commit or Ctrl+C to abort.
-read
-svn ci -m "--- MERGE: merged r$r to maintenance branch; original log message:
-`svn pg svn:log --revprop -r "$r"`"
+    cd "$DOCUTILS_MAINT_BRANCH"
+    svn revert . -R
+    svn up
+    svn merge -r"$[$r-1]:$r" ../../trunk/docutils .
+    svn diff
+    # Python 2.4 is faster and outputs unified diffs for the functional
+    # tests, so it comes first.
+    nice python2.4 -u test/alltests.py
+    nice python2.1 -u test/alltests.py
+    nice python2.2 -u test/alltests.py
+    nice python2.3 -u test/alltests.py
+    echo
+    echo Press enter to commit or Ctrl+C to abort.
+    read
+    svn ci -m "--- MERGE: merged r$r to maintenance branch; original log message:
+    `svn pg svn:log --revprop -r "$r"`"
+    shift
+done
