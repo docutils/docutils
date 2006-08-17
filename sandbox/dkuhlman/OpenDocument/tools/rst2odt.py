31530f1acdd8c48c6fd2eea2a01f976e12175a16
dkuhlman <dkuhlman@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-08-17 16:27:02 +0000

    Initial checkin for ODF/odtwriter
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4692 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/dkuhlman/OpenDocument/tools/rst2odt.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+
+# $Id$
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing Docutils XML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+#from docutils.writers.docutils_xml import Writer
+#from docutils.writers.html4css1 import Writer
+#from OOwriter import Writer
+from odtwriter import Writer
+
+
+description = ('Generates Docutils-native XML from standalone '
+               'reStructuredText sources.  ' + default_description)
+
+#publish_cmdline(writer_name='xml', description=description)
+
+writer = Writer()
+output = publish_cmdline(writer=writer, description=description)
+
+
