8d6f26cbc2deac6f94202444bc1f10cf8a4a809f
dkuhlman <dkuhlman@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-08-28 22:17:16 +0000

    Fixes so that rst2odt.py/odtwriter will work when it is installed under site-packages/docutils.
    Fixes so that odtwriter will find the default styles.odt file in the
    directory where odtwriter.py lives.
    
    Added visit_/depart_ methods in support of the source link and
    docutils footer.
    
    
    
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4702 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/dkuhlman/OpenDocument/tools/rst2odt.py
+++ sandbox/dkuhlman/OpenDocument/tools/rst2odt.py
@@ -18,7 +18,7 @@ from docutils.core import publish_cmdline, default_description
 #from docutils.writers.docutils_xml import Writer
 #from docutils.writers.html4css1 import Writer
 #from OOwriter import Writer
-from odtwriter import Writer
+from docutils.writers.odtwriter import Writer
 
 
 description = ('Generates Docutils-native XML from standalone '
