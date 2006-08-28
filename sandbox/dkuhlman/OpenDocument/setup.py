3af2bfdea589aeae0d15ac9401984f67e6c360dd
dkuhlman <dkuhlman@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-08-28 22:19:46 +0000

    Added setup.py in support of installing the odt writer etc under docutils
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4703 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/dkuhlman/OpenDocument/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys, os
+from distutils.core import setup
+
+
+setup(name="rst_to_odt",
+    version="1.0a",
+    description="convert rst to ODF/odt.",
+    author="Dave Kuhlman",
+    author_email="dkuhlman@rexx.com",
+    packages=['docutils.writers.odtwriter'],
+    package_dir={'docutils.writers.odtwriter': 'odtwriter'},
+    package_data={'docutils.writers.odtwriter': ['styles.odt',]},
+    scripts=["tools/rst2odt.py"],
+    )
+
+
+
