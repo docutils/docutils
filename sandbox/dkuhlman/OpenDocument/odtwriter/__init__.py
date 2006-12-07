b254aaa40387558ff3fecacd916ddbdb3f1aabe5
dkuhlman <dkuhlman@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2006-12-07 22:33:02 +0000

    Removed references to IPShell
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4821 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/dkuhlman/OpenDocument/odtwriter/__init__.py
+++ sandbox/dkuhlman/OpenDocument/odtwriter/__init__.py
@@ -44,12 +44,12 @@ except ImportError, e:
         raise
 
 
-from IPython.Shell import IPShellEmbed
-args = ['-pdb', '-pi1', 'In <\\#>: ', '-pi2', '   .\\D.: ',
-    '-po', 'Out<\\#>: ', '-nosep']
-ipshell = IPShellEmbed(args,
-    banner = 'Entering IPython.  Press Ctrl-D to exit.',
-    exit_msg = 'Leaving Interpreter, back to program.')
+##from IPython.Shell import IPShellEmbed
+##args = ['-pdb', '-pi1', 'In <\\#>: ', '-pi2', '   .\\D.: ',
+##    '-po', 'Out<\\#>: ', '-nosep']
+##ipshell = IPShellEmbed(args,
+##    banner = 'Entering IPython.  Press Ctrl-D to exit.',
+##    exit_msg = 'Leaving Interpreter, back to program.')
 
 
 #
