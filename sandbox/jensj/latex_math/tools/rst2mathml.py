3b67ec2d30605f225a12fa12cce248bc143ef130
jensj <jensj@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-01-26 17:26:36 +0000

    Added the \! character.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4897 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/jensj/latex_math/tools/rst2mathml.py
+++ sandbox/jensj/latex_math/tools/rst2mathml.py
@@ -375,12 +375,12 @@ def parse_latex_math(string, inline=True):
                 node = entry
                 skip = 2
             else:
-                raise SyntaxError, 'Syntax error!'
+                raise SyntaxError('Syntax error: "%s%s"' % (c, c2))
         elif c.isalpha():
             node = node.append(mi(c))
         elif c.isdigit():
             node = node.append(mn(c))
-        elif c in '+-/()[]|=<>,.':
+        elif c in '+-/()[]|=<>,.!':
             node = node.append(mo(c))
         elif c == '_':
             child = node.delete_child()
@@ -409,7 +409,7 @@ def parse_latex_math(string, inline=True):
             node.close().append(entry)
             node = entry
         else:
-            raise SyntaxError, 'Syntax error!'
+            raise SyntaxError('Illegal character: "%s"' % c)
         string = string[skip:]
     return tree
 
@@ -453,7 +453,7 @@ def handle_keyword(name, node, string):
         skip = 1
     if name == 'begin':
         if not string.startswith('{matrix}'):
-            raise SyntaxError, 'Expected "\begin{matrix}"!'
+            raise SyntaxError('Expected "\begin{matrix}"!')
         skip += 8
         entry = mtd()
         table = mtable(mtr(entry))
@@ -461,15 +461,15 @@ def handle_keyword(name, node, string):
         node = entry
     elif name == 'end':
         if not string.startswith('{matrix}'):
-            raise SyntaxError, 'Expected "\end{matrix}"!'
+            raise SyntaxError('Expected "\end{matrix}"!')
         skip += 8
         node = node.close().close().close()
     elif name == 'text':
         if string[0] != '{':
-            raise SyntaxError, 'Expected "\text{...}"!'
+            raise SyntaxError('Expected "\text{...}"!')
         i = string.find('}')
         if i == -1:
-            raise SyntaxError, 'Expected "\text{...}"!'
+            raise SyntaxError('Expected "\text{...}"!')
         node = node.append(mtext(string[1:i]))
         skip += i + 1
     elif name == 'sqrt':
@@ -485,7 +485,7 @@ def handle_keyword(name, node, string):
             if string.startswith(par):
                 break
         else:
-            raise SyntaxError, 'Missing left-brace!'
+            raise SyntaxError('Missing left-brace!')
         fenced = mfenced(par)
         node.append(fenced)
         node = fenced
@@ -495,7 +495,7 @@ def handle_keyword(name, node, string):
             if string.startswith(par):
                 break
         else:
-            raise SyntaxError, 'Missing right-brace!'
+            raise SyntaxError('Missing right-brace!')
         node.closepar = par
         node = node.close()
         skip += len(par)
@@ -504,7 +504,7 @@ def handle_keyword(name, node, string):
             if string.startswith(operator):
                 break
         else:
-            raise SyntaxError, 'Expected something to negate: "\\not ..."!'
+            raise SyntaxError('Expected something to negate: "\\not ..."!')
         node = node.append(mo(negatables[operator]))
         skip += len(operator)
     elif name == 'mathbf':
@@ -513,7 +513,7 @@ def handle_keyword(name, node, string):
         node = style
     elif name == 'mathbb':
         if string[0] != '{' or not string[1].isupper() or string[2] != '}':
-            raise SyntaxError, 'Expected something like "\mathbb{A}"!'
+            raise SyntaxError('Expected something like "\mathbb{A}"!')
         node = node.append(mi(mathbb[string[1]]))
         skip += 3
     elif name in greek:
@@ -531,7 +531,7 @@ def handle_keyword(name, node, string):
             node.append(ovr)
             node = ovr
         else:
-            raise SyntaxError, 'Unknown LaTeX command: ' + name
+            raise SyntaxError('Unknown LaTeX command: ' + name)
 
     return node, skip
 
