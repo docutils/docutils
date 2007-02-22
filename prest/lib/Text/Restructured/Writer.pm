d350e607d0b9c09a7b39f62bc36eb84af5a760f1
mnodine <mnodine@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-02-22 19:29:51 +0000

    Execute evals in Text::Restructured::Writer::Eval instead of Eval_::.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@4929 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- prest/lib/Text/Restructured/Writer.pm
+++ prest/lib/Text/Restructured/Writer.pm
@@ -40,7 +40,7 @@ sub new {
     foreach my $key (keys %{$writer->{opt}{W}}) {
 	(my $var = $key) =~ tr/a-zA-Z0-9/_/c;
 	no strict 'refs';
-	${"Eval_::$var"} = $writer->{opt}{W}{$key};
+	${"Text::Restructured::Writer::Eval::$var"} = $writer->{opt}{W}{$key};
     }
     $writer->{opt}{d} ||= 0;
     $writer->{opt}{w} = $writer_name;
@@ -206,7 +206,7 @@ sub TraverseDOM : method {
 # Returns: anonymous subroutine reference
 # Exceptions: Program termination if error in evaluation
 # Uses globals: None
-# Sets globals: ``Eval_::<subname>``
+# Sets globals: ``Text::Restructured::Writer::Eval::<subname>``
 sub DoEval : method {
     my ($self, $str, $line, $subname) = @_;
     my ($file, $lineno) = $line =~ /(.*), line (\d+)/;
@@ -217,12 +217,13 @@ sub DoEval : method {
 	$subname = "$f, line $lineno";
     }
     $subname =~ s/\W/_/g;
-    my $sub = "sub Eval_::$subname {package Eval_;\n $str}";
+    my $sub = "package Text::Restructured::Writer::Eval;sub $subname {\n $str}";
     my $line_directive = defined $self->{opt}{D}{no_line_directives} ? "" :
 	qq(\#line $lineno "$file"\n);
     my $val = eval("$line_directive$sub");
     die "Error: $line: $@" if $@;
-    return \&{$Eval_::{$subname}};
+    $self->{sub}{$subname} = \&{$Text::Restructured::Writer::Eval::{$subname}};
+    return \&{$Text::Restructured::Writer::Eval::{$subname}};
 }
 
 1;
