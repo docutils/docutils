fb4aa294e9675e50432b1b63e125783599409353
blackbird <blackbird@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-05-22 11:06:41 +0000

    added quickjump
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5081 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/py-rest-doc/sphinx/web/application.py
+++ sandbox/py-rest-doc/sphinx/web/application.py
@@ -17,7 +17,7 @@ from .util import Request, Response, RedirectResponse, SharedDataMiddleware, \
      NotFound, render_template
 
 
-special_urls = set(['index', 'genindex', 'modindex', 'search'])
+special_urls = set(['index', 'genindex', 'modindex'])
 
 
 def get_target_uri(source_filename):
@@ -33,8 +33,35 @@ class DocumentationApplication(object):
     def __init__(self, conf):
         self.cache = {}
         self.data_root = conf['data_root_path']
+        with file(path.join(self.data_root, 'environment.pickle')) as f:
+            self.environment = pickle.load(f)
+
+    def update_context(self, req, context):
+        """
+        Put some request specific globals into the context
+        """
+        url = req.path
+        def relative_path_to(otheruri, resource=False):
+            if not resource:
+                otheruri = get_target_uri(otheruri)
+            return relative_uri(url, otheruri)
+        context['pathto'] = relative_path_to
+
+    def search(self, req):
+        """
+        Search the database.
+        """
+        if req.method == 'POST':
+            pass
+        context = {}
+        self.update_context(req, context)
+        return Response(render_template('search.html', context))
 
     def get_page(self, req, url):
+        """
+        Show the requested documentation page or raise an
+        `NotFound` exception to display a page with close matches.
+        """
         try:
             filename, mtime, text = self.cache[url]
         except KeyError:
@@ -61,33 +88,55 @@ class DocumentationApplication(object):
                 raise NotFound()
             templatename = 'page.html'
 
-        def relative_path_to(otheruri, resource=False):
-            if not resource:
-                otheruri = get_target_uri(otheruri)
-            return relative_uri(url + '/', otheruri)
-        context['pathto'] = relative_path_to
-
+        self.update_context(req, context)
         text = render_template(templatename, context)
         self.cache[url] = (filename, path.getmtime(filename), text)
         return Response(text)
 
     def get_close_matches(self, req, url):
+        """
+        Lookup close matches. If there is an exact match (for example
+        http://docs.python.org/os.path.exists would automatically
+        redirect to http://docs.python.org/modules/os.path/#os.path.exists.
+        """
+        # direct references
+        if url in self.environment.descrefs:
+            filename, ref_type = self.environment.descrefs[url]
+            url = get_target_uri(filename) + '#' + url
+            return RedirectResponse(url)
         raise NotImplementedError(url)
 
     def __call__(self, environ, start_response):
+        """
+        Dispatch the requests.
+        """
         req = Request(environ)
-        if not req.path.endswith('/'):
-            resp = RedirectResponse(req.path + '/')
+        if not req.path.endswith('/') and req.method == 'GET':
+            query = req.environ.get('QUERY_STRING', '')
+            if query:
+                query = '?' + query
+            resp = RedirectResponse(req.path + '/' + query)
         else:
             url = req.path.strip('/') or 'index'
-            try:
-                resp = self.get_page(req, url)
-            except NotFound:
-                resp = self.get_close_matches(req, url)
+            if url == 'search':
+                resp = self.search(req)
+            else:
+                try:
+                    resp = self.get_page(req, url)
+                except NotFound:
+                    resp = self.get_close_matches(req, url)
         return resp(environ, start_response)
 
 
 def make_app(conf=None):
+    """
+    Creates the WSGI application based on a configuration passed.
+    Handled configuration values so far:
+
+    `data_root_path`
+        the folder containing the documentation data as generated
+        by sphinx with the web builder.
+    """
     app = DocumentationApplication(conf or {})
     app = SharedDataMiddleware(app, {
         '/style':   path.join(path.dirname(__file__), '..', 'style')
