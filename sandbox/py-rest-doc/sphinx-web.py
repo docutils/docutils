ac50254035cc737087ecbc6080bf368f99b092af
gbrandl <gbrandl@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-05-25 16:24:41 +0000

    Fix spelling error and add check for superuser at app startup.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5151 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/py-rest-doc/sphinx-web.py
+++ sandbox/py-rest-doc/sphinx-web.py
@@ -19,7 +19,7 @@ def main(argv):
         print 'usage: %s [-d] <doc_root>' % argv[0]
         print ' -d: use werkzeug debugger if installed'
         return 2
-    app = make_app({'data_root_path': args[0]})
+    orig_app, app = make_app({'data_root_path': args[0]})
 
     if '-d' in opts:
         try:
@@ -28,6 +28,22 @@ def main(argv):
             pass
         else:
             app = DebuggedApplication(app, True)
+
+    # check if there is a superuser
+    if not orig_app.userdb.users:
+        print 'Warning: you have no user database or no master "admin" account.'
+        create = raw_input('Do you want to create an admin account now? [y/n] ')
+        if not create or create.lower().startswith('y'):
+            import getpass
+            print 'Creating "admin" user.'
+            pw1 = getpass.getpass('Enter password: ')
+            pw2 = getpass.getpass('Enter password again: ')
+            if pw1 != pw2:
+                print 'Error: Passwords don\'t match.'
+                sys.exit(1)
+            orig_app.userdb.set_password('admin', pw1)
+            orig_app.userdb.privileges['admin'] = set(['master'])
+            orig_app.userdb.save()
 
     srv = make_server('localhost', 3000, app)
     try:
