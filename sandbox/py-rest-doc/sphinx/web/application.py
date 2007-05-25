2949c6f7df62d6ce3d1d6f351168ffa2b5573c3d
gbrandl <gbrandl@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-05-25 17:13:35 +0000

    Move admin stuff to a separate module.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5152 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/py-rest-doc/sphinx/web/application.py
+++ sandbox/py-rest-doc/sphinx/web/application.py
@@ -16,11 +16,12 @@ import cPickle as pickle
 from os import path
 
 from .feed import Feed
+from .admin import get_admin_page
 from .antispam import AntiSpam
 from .database import connect, set_connection, Comment
 from .userdb import UserDatabase
 from .util import Request, Response, RedirectResponse, SharedDataMiddleware, \
-     NotFound, jinja_env
+     NotFound, render_template
 from ..search import SearchFrontend
 from ..util import relative_uri, shorten_result
 
@@ -37,24 +38,6 @@ def get_target_uri(source_filename):
     if source_filename.endswith('/index.rst'):
         return source_filename[:-9] # up to /
     return source_filename[:-4] + '/'
-
-
-def render_template(req, template_name, context=None):
-    context = context or {}
-    tmpl = jinja_env.get_template(template_name)
-
-    def relative_path_to(otheruri, resource=False):
-        if not resource:
-            otheruri = get_target_uri(otheruri)
-        return relative_uri(req.path, otheruri)
-    context['pathto'] = relative_path_to
-
-    # add it here a second time for templates that don't
-    # get the builder information from the environment (such as search)
-    context['builder'] = 'web'
-    context['req'] = req
-
-    return tmpl.render(context)
 
 
 class DocumentationApplication(object):
@@ -206,197 +189,6 @@ class DocumentationApplication(object):
                           comment.parsed_comment_body, comment.pub_date)
         return Response(feed.generate(), mimetype='application/rss+xml')
 
-    def get_admin_page(self, req, page):
-        """
-        Get some administration pages.
-        """
-        is_logged_in = req.user is not None
-        if is_logged_in:
-            privileges = self.userdb.privileges[req.user]
-            is_master_admin = 'master' in privileges
-            can_change_password = 'frozenpassword' not in privileges
-        else:
-            privileges = set()
-            can_change_password = is_master_admin = False
-
-        if page == 'login':
-            if req.user is not None:
-                return RedirectResponse('admin/')
-            login_failed = False
-            if req.method == 'POST':
-                if req.form.get('cancel'):
-                    return RedirectResponse('')
-                username = req.form.get('username')
-                password = req.form.get('password')
-                if self.userdb.check_password(username, password):
-                    req.login(username)
-                    return RedirectResponse('admin/')
-                login_failed = True
-            return Response(render_template(req, 'admin/login.html', {
-                'login_failed': login_failed
-            }))
-        elif not is_logged_in:
-            return RedirectResponse('admin/login/')
-        elif page == 'logout':
-            req.logout()
-            return RedirectResponse('admin/')
-        elif page == 'change_password' and can_change_password:
-            change_failed = change_successful = False
-            if req.method == 'POST':
-                if req.form.get('cancel'):
-                    return RedirectResponse('admin/')
-                pw = req.form.get('pw1')
-                if pw and pw == req.form.get('pw2'):
-                    self.userdb.set_password(req.user, pw)
-                    self.userdb.save()
-                    change_successful = True
-                else:
-                    change_failed = True
-            return Response(render_template(req, 'admin/change_password.html', {
-                'change_failed':        change_failed,
-                'change_successful':    change_successful
-            }))
-        elif page.split('/')[0] == 'moderate_comments':
-            details_for = page[18:] + '.rst' or None
-            to_delete = set()
-            edit_detail = None
-
-            if 'edit' in req.args:
-                try:
-                    edit_detail = Comment.get(int(req.args['edit']))
-                except ValueError:
-                    pass
-
-            if req.method == 'POST':
-                for item in req.form.getlist('delete'):
-                    try:
-                        to_delete.add(int(item))
-                    except ValueError:
-                        pass
-                if req.form.get('cancel'):
-                    return RedirectResponse('admin/')
-                elif req.form.get('confirmed'):
-                    for comment_id in to_delete:
-                        try:
-                            Comment.get(comment_id).delete()
-                        except ValueError:
-                            pass
-                    return RedirectResponse('admin/' + page)
-                elif req.form.get('aborted'):
-                    return RedirectResponse('admin/' + page)
-                elif req.form.get('edit') and not to_delete:
-                    if 'delete_this' in req.form:
-                        try:
-                            to_delete.add(req.form['delete_this'])
-                        except ValueError:
-                            pass
-                    else:
-                        try:
-                            edit_detail = c = Comment.get(int(req.args['edit']))
-                        except ValueError:
-                            pass
-                        else:
-                            if req.form.get('view'):
-                                return RedirectResponse(c.url)
-                            c.author = req.form.get('author', '')
-                            c.author_mail = req.form.get('author_mail', '')
-                            c.title = req.form.get('title', '')
-                            c.comment_body = req.form.get('comment_body', '')
-                            c.save()
-                            self.cache.pop(edit_detail.associated_page, None)
-                        return RedirectResponse('admin/' + page)
-
-            return Response(render_template(req, 'admin/moderate_comments.html', {
-                'pages_with_comments': [{
-                    'page_id':      page_id,
-                    'title':        page_id,        #XXX: get title somehow
-                    'has_details':  details_for == page_id,
-                    'comments':     comments
-                } for page_id, comments in Comment.get_overview(details_for)],
-                'to_delete':        to_delete,
-                'ask_confirmation': req.method == 'POST' and to_delete,
-                'edit_detail':      edit_detail
-            }))
-        elif page == 'manage_users' and is_master_admin:
-            add_user_mode = False
-            user_privileges = {}
-            users = sorted((user, []) for user in self.userdb.users)
-            to_delete = set()
-            generated_user = generated_password = None
-            user_exists = False
-
-            if req.method == 'POST':
-                for item in req.form.getlist('delete'):
-                    try:
-                        to_delete.add(item)
-                    except ValueError:
-                        pass
-                for name, item in req.form.iteritems():
-                    if name.startswith('privileges-'):
-                        user_privileges[name[11:]] = [x.strip() for x
-                                                      in item.split(',')]
-                if req.form.get('cancel'):
-                    return RedirectResponse('admin/')
-                elif req.form.get('add_user'):
-                    username = req.form.get('username')
-                    if username:
-                        if username in self.userdb.users:
-                            user_exists = username
-                        else:
-                            generated_password = self.userdb.add_user(username)
-                            self.userdb.save()
-                            generated_user = username
-                    else:
-                        add_user_mode = True
-                elif req.form.get('aborted'):
-                    return RedirectResponse('admin/manage_users/')
-
-            users = {}
-            for user in self.userdb.users:
-                if not user in user_privileges:
-                    users[user] = sorted(self.userdb.privileges[user])
-                else:
-                    users[user] = user_privileges[user]
-
-            new_users = users.copy()
-            for user in to_delete:
-                new_users.pop(user, None)
-
-            self_destruction = not req.user in new_users or \
-                               'master' not in new_users[req.user]
-
-            if req.method == 'POST' and (not to_delete or
-               (to_delete and req.form.get('confirmed'))) and \
-               req.form.get('update'):
-                old_users = self.userdb.users.copy()
-                for user in old_users:
-                    if user not in new_users:
-                        del self.userdb.users[user]
-                    else:
-                        self.userdb.privileges[user].clear()
-                        self.userdb.privileges[user].update(new_users[user])
-                self.userdb.save()
-                return RedirectResponse('admin/manage_users/')
-
-            return Response(render_template(req, 'admin/manage_users.html', {
-                'users':                users,
-                'add_user_mode':        add_user_mode,
-                'to_delete':            to_delete,
-                'ask_confirmation':     req.method == 'POST' and to_delete \
-                                        and not self_destruction,
-                'generated_user':       generated_user,
-                'generated_password':   generated_password,
-                'self_destruction':     self_destruction,
-                'user_exists':          user_exists
-            }))
-        elif page == '':
-            return Response(render_template(req, 'admin/index.html', {
-                'is_master_admin':      is_master_admin,
-                'can_change_password':  can_change_password
-            }))
-        else:
-            raise RedirectResponse('admin/')
-
     pretty_type = {
         'data': 'module data',
         'cfunction': 'C function',
@@ -478,7 +270,7 @@ class DocumentationApplication(object):
             elif url.startswith('q/'):
                 resp = self.get_keyword_matches(req, url[2:])
             elif url == 'admin' or url.startswith('admin/'):
-                resp = self.get_admin_page(req, url[6:])
+                resp = get_admin_page(req, url[6:], self.userdb, self.cache)
             else:
                 try:
                     resp = self.get_page(req, url)
