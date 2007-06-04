20b8b1b430d6fee16ddace3e7f791165a06a2d55
agurtovoy <agurtovoy@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-06-04 01:38:13 +0000

    New directory structure
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5200 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/agurtovoy/html_frames/tools/htmlframes.py
+++ sandbox/agurtovoy/html_frames/tools/htmlframes.py
@@ -19,4 +19,4 @@ from docutils.core import publish_cmdline, default_description
 description = ('Generates "framed" (X)HTML documents from standalone reStructuredText '
                'sources.  ' + default_description)
 
-publish_cmdline(writer_name='html4frames', description=description)
+publish_cmdline(writer_name='html4_frames', description=description)
