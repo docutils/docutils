4232ca0ccbdd78402e45b5346d788d0068c2664d
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-06-18 16:17:28 +0000

    renaming test_html4trans.py to check_html4trans.py; adding function to convert syntax-samples file (rendering tools/test-syntax-samples obsolete)
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5249 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/html4trans/check_html4trans.py
+++ sandbox/html4trans/check_html4trans.py
@@ -9,11 +9,17 @@
 """
 Test script for the "html4trans" writer, producing HTML not requiring
 a CSS stylesheet.
+
+This is not a proper unit test but a script for interactive checking
+and modifying just to get a picture of the working
 """
 
-from docutils.core import publish_string
+from docutils.core import publish_string, publish_file
 
-import html4trans
+from html4trans import Writer
+
+# RST text samples
+# ================
 
 title = """\
 Title of the Document
@@ -116,29 +122,61 @@ this link should `trigger a system message`_ as it has no defined target.
 
 """
 
+
 # no CSS stylesheet needed, so do not include (referencing does not harm)
 overrides = {'embed_stylesheet': False}
 
-# Sample input text::
-sample = "".join([
-                  title,
-                  bibliographic_list,
-                  admonitions,
-                  literal_block,
-                  # line_block,
-                  system_message
-                 ])
 
-output = publish_string(sample, 
-                        writer=html4trans.Writer(),
-                        settings_overrides=overrides)
+def publish_some_samples():
+    # Sample input text::
+    sample = "".join([
+                      title,
+                      bibliographic_list,
+                      admonitions,
+                      literal_block,
+                      # line_block,
+                      system_message
+                     ])
+    
+    doctree = publish_string(sample, writer_name="pseudoxml")
 
-# print to stdout:
+    output = publish_string(sample, 
+                            writer=Writer(),
+                            settings_overrides=overrides)
+    return (doctree, output)
+
+
+def publish_syntax_samples():
+    """Convert the full sample list from the standalone_rst_html4css1.txt file"""
+    
+    syntax_samples_path = \
+        '../../docutils/test/functional/input/standalone_rst_html4css1.txt'
+
+    doctree = '' #publish_string(open(syntax_samples_path), 
+                 #            writer_name="pseudoxml")
+    
+    output = publish_file(open(syntax_samples_path),
+                          writer=Writer(),
+                          settings_overrides=overrides)
+    
+    # Replace image links
+    output = output.replace('../../../docs/user/rst/images/',
+                            '../../../docutils/docs/user/rst/images/')
+    return (doctree, output)
+
+
+# Convert and print
+# -----------------
+
+# Uncomment one of the following:
+
+(doctree, output) = publish_some_samples()
+#(doctree, output) = publish_syntax_samples()
 
 # * document tree (as pseudoxml rendering) for comparision
-# * html4trans output
+print doctree
 
-print publish_string(sample, writer_name="pseudoxml")
+# * html4trans output
 print output
 
 # save output to data sub-dir
