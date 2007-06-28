81ca3ba2fb2467d68de76b127dec85d2e13e2b64
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-06-28 15:42:01 +0000

    Set up recommended directory structure, moving tools to 'tools/' Merged the complete syntax sample test into check_html4trans.py
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5304 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/html4trans/tools/rst2html_trans.py
+++ sandbox/html4trans/tools/rst2html_trans.py
@@ -6,16 +6,12 @@
 # Date: $Date$
 # Copyright: Licensed under the Academic Free License version 1.2
 
-# Hint: run this with the "Examples of Syntax Constructs" in the
-# `reStructuredText Test Document`__ via test-syntax-examples
-#
-# __ file:/usr/src/docutils-svn/docutils/test/functional/input/standalone_rst_html4css1.txt
-
 """
 Front end to the Docutils Publisher, producing HTML not requiring
 a CSS stylesheet.
 """
 
+import sys
 try:
     import locale
     locale.setlocale(locale.LC_ALL, '')
@@ -23,7 +19,13 @@ except:
     pass
 
 from docutils.core import publish_cmdline, default_description
-import html4trans 
+
+# Prepend parent dir to the PYTHONPATH
+# (This is a hack to get rst2html_trans.py working without install,
+#  not needed if the html4trans.py module is installed in the PYTHONPATH)
+sys.path.insert(0, '..')
+
+from html4trans import Writer
 
 description = ('Generates (X)HTML documents from standalone reStructuredText '
                'sources that do not require a CSS stylesheet.  ' 
@@ -32,6 +34,6 @@ description = ('Generates (X)HTML documents from standalone reStructuredText '
 # no CSS stylesheet needed, so do not include (referencing does not harm)
 overrides = {'embed_stylesheet': False}
 
-publish_cmdline(writer=html4trans.Writer(), 
+publish_cmdline(writer=Writer(), 
                 settings_overrides=overrides,
                 description=description)
