8e688eaa1acfaa54e9cb3d7d885d49a8a3417dbb
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2007-11-24 08:54:36 +0000

    More file documentation
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5463 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/manpage-writer/writers/manpage.py
+++ sandbox/manpage-writer/writers/manpage.py
@@ -3,12 +3,27 @@
 # Copyright: This module is put into the public domain.
 
 """
-Man page formatting for reStructuredText.
+Simple man page writer for reStructuredText.
+
+Man pages (short for "manual pages") contain system documentation on unix-like
+systems. The pages are grouped in numbered sections: 
+
+ 1 executable programs and shell commands
+ 2 system calls
+ 3 library functions
+ 4 special files
+ 5 file formats
+ 6 games
+ 7 miscellaneous
+ 8 system administration
+
+Man pages are written *troff*, a text file formatting system.
 
 See http://www.tldp.org/HOWTO/Man-Page for a start.
 
 Man pages have no subsection only parts.
 Standard parts
+
   NAME ,
   SYNOPSIS ,
   DESCRIPTION ,
@@ -16,8 +31,13 @@ Standard parts
   FILES ,
   SEE ALSO ,
   BUGS ,
+
 and
+
   AUTHOR .
+
+A unix-like system keeps an index of the DESCRIPTIONs, which is accesable
+by the command whatis or apropos.
 
 """
 
