3c569ed92557c192fe5969205bd7152184929771
goodger <goodger@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2008-09-05 15:07:51 +0000

    corrections from Oscar Garcia Amor
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5639 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/languages/gl.py
+++ docutils/docutils/languages/gl.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Author: David Goodger
 # Contact: goodger@users.sourceforge.net
 # Revision: $Revision: 2224 $
@@ -17,48 +18,46 @@ __docformat__ = 'reStructuredText'
 
 labels = {
       # fixed: language-dependent
-      'author': 'Autor',
-      'authors': 'Autores',
-      'organization': 'Organizaci\xf3n',
-      'address': 'Enderezo',
-      'contact': 'Contacto',
-      'version': 'Versi\xf3n',
-      'revision': 'Revisi\xf3n',
-      'status': 'Estado',
-      'date': 'Data',
-      'copyright': 'Dereitos de copia',
-      'dedication': 'Dedicatoria',
-      'abstract': 'Abstract',
-      'attention': 'Atenci\xf3n!',
-      'caution': 'Advertencia!',
-      'danger': 'PERIGO!',
-      'error': 'Erro',
-      'hint': 'Consello',
-      'important': 'Importante',
-      'note': 'Nota',
-      'tip': 'Suxesti\xf3n',
-      'warning': 'Aviso',
-      'contents': 'Contido'}
+      'author': u'Autor',
+      'authors': u'Autores',
+      'organization': u'Organizaci\u00f3n',
+      'address': u'Enderezo',
+      'contact': u'Contacto',
+      'version': u'Versi\u00f3n',
+      'revision': u'Revisi\u00f3n',
+      'status': u'Estado',
+      'date': u'Data',
+      'copyright': u'Dereitos de copia',
+      'dedication': u'Dedicatoria',
+      'abstract': u'Abstract',
+      'attention': u'Atenci\u00f3n!',
+      'caution': u'Advertencia!',
+      'danger': u'PERIGO!',
+      'error': u'Erro',
+      'hint': u'Consello',
+      'important': u'Importante',
+      'note': u'Nota',
+      'tip': u'Suxesti\u00f3n',
+      'warning': u'Aviso',
+      'contents': u'Contido'}
 """Mapping of node class name to label text."""
 
 bibliographic_fields = {
       # language-dependent: fixed
-      'autor': 'author',
-      'autores': 'authors',
-      'organizaci\xf3n': 'organization',
-      'enderezo': 'address',
-      'contacto': 'contact',
-      'versi\xf3n': 'version',
-      'revisi\xf3n': 'revision',
-      'estado': 'status',
-      'data': 'date',
-      'dereitos de copia': 'copyright',
-      'dedicatoria': 'dedication',
-      'abstract': 'abstract'}
+      u'autor': 'author',
+      u'autores': 'authors',
+      u'organizaci\u00f3n': 'organization',
+      u'enderezo': 'address',
+      u'contacto': 'contact',
+      u'versi\u00f3n': 'version',
+      u'revisi\u00f3n': 'revision',
+      u'estado': 'status',
+      u'data': 'date',
+      u'dereitos de copia': 'copyright',
+      u'dedicatoria': 'dedication',
+      u'abstract': 'abstract'}
 """Galician (lowcased) to canonical name mapping for bibliographic fields."""
 
 author_separators = [';', ',']
 """List of separator strings for the 'Authors' bibliographic field. Tried in
 order."""
-
- 	  	 
