2ae6a2ca1106cf1c3f70807ad6ed7e3c8ae78591
matt_harrison <matt_harrison@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2008-12-17 20:41:21 +0000

    refactor metadata, don't pull in everything during build/install
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5824 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/rst2odp/odplib/meta.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2008 Matt Harrison
+# Licensed under Apache License, Version 2.0 (current)
+
+__version__ = "0.2"
+__author__ = "matt harrison"
+__email__ = "matthewharrison@gmail.com"
+  
