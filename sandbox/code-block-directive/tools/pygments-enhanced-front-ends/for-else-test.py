9462c95d5b2bdd300c8ed1f4c11f8911395f881c
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2009-01-28 09:35:19 +0000

    sandbox/code-block-directive: documentation update
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5859 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/code-block-directive/tools/pygments-enhanced-front-ends/for-else-test.py
+++ sandbox/code-block-directive/tools/pygments-enhanced-front-ends/for-else-test.py
@@ -1,26 +1,32 @@
 # Example for syntax highlight with Pygments
 # ==========================================
-# 
+#
 # Translate this document to HTML with a pygments enhanced frontend::
-# 
+#
 #   rst2html-pygments --stylesheet=pygments-default.css
-#    
+#
 # or to LaTeX with::
-#      
+#
 #   rst2latex-pygments --stylesheet=pygments-default.sty
-#   
+#
 # to gain syntax highlight in the output.
-# 
-# .. Run the doctests with ``pylit --doctest for-else-test.py``.
-# 
-# 
+#
+# Convert between text <-> code source formats with::
+#
+#   pylit --code-block-marker='.. code-block:: python'
+#
+# Run the doctests with::
+#
+#   pylit --doctest for-else-test.py
+#
+#
 # for-else-test
 # -------------
-# 
+#
 # Test the flow in a `for` loop with `else` statement.
-# 
+#
 # First define a simple `for` loop.
-# 
+#
 # .. code-block:: python
 
 def loop1(iterable):
@@ -30,32 +36,33 @@ def loop1(iterable):
     else:
         print "iterable empty"
     print "Ende"
-  
+
 # Now test it:
-#   
+#
 # The first test runs as I expect: iterator empty -> else clause applies:
-#       
+#
 # .. code-block:: pycon
-# 
+#
+#    >>> execfile('for-else-test.py')
 #    >>> loop1(range(0))
 #    iterable empty
 #    Ende
-#   
+#
 # However, the else clause even runs if the iterator is not empty in the first
 # place but after it is "spent":
-# 
+#
 # .. code-block:: pycon
-# 
+#
 #    >>> loop1(range(3))
 #    0
 #    1
 #    2
 #    iterable empty
 #    Ende
-#    
+#
 # It seems like the else clause can only be prevented, if we break out of
 # the loop. Let's try
-# 
+#
 # .. code-block:: python
 
 def loop2(iterable):
@@ -66,20 +73,20 @@ def loop2(iterable):
     else:
         print "iterable empty"
     print "Ende"
-  
+
 # And indeed, the else clause is skipped after breaking out of the loop:
-# 
+#
 # .. code-block:: pycon
-# 
+#
 #    >>> loop2(range(3))
 #    0
 #    Ende
-#    
+#
 # The empty iterator runs as expected:
-# 
+#
 # .. code-block:: pycon
-#       
+#
 #    >>> loop2(range(0))
 #    iterable empty
 #    Ende
-#   
+#
