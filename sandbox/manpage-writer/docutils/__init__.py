3cdf819787e877b6c83411c941d74e78d7c2cca8
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2009-04-29 08:58:51 +0000

    Apply patch from ben finney, to easy user and developer installation
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@5929 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/manpage-writer/docutils/__init__.py
@@ -0,0 +1,20 @@
+# docutils/__init__.py
+# Python package for in-development docutils packages.
+
+import pkgutil
+
+# Ensure we can also reach modules in the system package
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+# Make this directory come last when importing from this package
+__path__.reverse()
+
+# Make this package gain all the attributes of the system package
+_path_prev = __path__
+import __init__
+globals().update(vars(__init__))
+__path__ = _path_prev
+del _path_prev
+
+# Make this directory come first when importing from this package
+__path__.reverse()
--- sandbox/manpage-writer/writers/__init__.py
+++
@@ -1,3 +0,0 @@
-# $Id$
-# Author: David Goodger <goodger@python.org>
-# Copyright: This module has been placed in the public domain.
