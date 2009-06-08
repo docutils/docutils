6e698bfbd39ac9c8edc9ed55d3ea41510da2a8f5
cliechti <cliechti@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2009-06-08 23:36:49 +0000

    - wrap main in a function for easier script usage - extend setup.py   - pypi registration   - install command line tool - tabs -> spaces
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@5980 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/aafigure/setup.py
+++ sandbox/aafigure/setup.py
@@ -1,16 +1,58 @@
 # $Id$
 # Author: Lea Wiemann <LeWiemann@gmail.com>
+# Author: Chris Liechti <cliechti@gmx.net>
 # Copyright: This file has been placed in the public domain.
 
 from setuptools import setup, find_packages
 setup(
     name = 'docutils-aafigure',
-    version = '0.1',
-    description = 'ASCII art figures for reStructuredText',
+    version = '0.2',
+    description = "ASCII art figures for reStructuredText",
+    long_description = """\
+This module provides:
+
+- a plugin for docutils (>= 0.5) -> aafigure directive
+- a command line application docutils-aafigure
+
+reST example::
+
+    .. aafigure::
+
+            +-----+   ^
+            |     |   |
+        --->+     +---o--->
+            |     |   |
+            +-----+   V
+
+Command line example::
+
+  docutils-aafigure test.txt -t svg -o test.svg
+
+Please see README.txt for examples.
+""",
     author = 'Chris Liechti',
-    author_email = '<cliechti@gmx.net>',
+    author_email = 'cliechti@gmx.net',
     install_requires = 'docutils>=0.5',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        #~ 'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms = 'any',
     packages = find_packages(),
-    entry_points = {'docutils.parsers.rst.directives': [
-    'aafigure = aafigure.aafigure_directive:AAFigureDirective']},
+    entry_points = {
+        'docutils.parsers.rst.directives': [
+            'aafigure = aafigure.aafigure_directive:AAFigureDirective'
+        ],
+        'console_scripts': [
+            'docutils-aafigure = aafigure.aafigure:main',
+        ],
+    },
+
 )
