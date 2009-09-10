7282c9487e300fa395f696fd4a052ba9eacb1107
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2009-09-10 11:02:27 +0000

    Fix [ 2781629 ] support non-ASCII chars in file names.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6120 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/parsers/rst/directives/misc.py
+++ docutils/docutils/parsers/rst/directives/misc.py
@@ -52,6 +52,7 @@ class Include(Directive):
             path = os.path.join(self.standard_include_path, path[1:-1])
         path = os.path.normpath(os.path.join(source_dir, path))
         path = utils.relative_path(None, path)
+        path = nodes.reprunicode(path)
         encoding = self.options.get(
             'encoding', self.state.document.settings.input_encoding)
         try:
