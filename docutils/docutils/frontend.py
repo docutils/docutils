0f0e5330abbcb61f43548315191c6d17fedba339
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2009-11-13 20:21:18 +0000

    filter_settings_spec() now also allows to replace option specifications
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6195 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/frontend.py
+++ docutils/docutils/frontend.py
@@ -201,22 +201,33 @@ def make_paths_absolute(pathdict, keys, base_path=None):
 def make_one_path_absolute(base_path, path):
     return os.path.abspath(os.path.join(base_path, path))
 
-def filter_settings_spec(settings_spec, *exclude):
-    """Return a copy of `setting_spec` excluding the specified settings.
+def filter_settings_spec(settings_spec, *exclude, **replace):
+    """Return a copy of `setting_spec` excluding/replacing some settings.
 
     `setting_spec` is a tuple of configuration settings with a structure
     described for docutils.SettingsSpec.settings_spec.
 
-    The optional arguments are 'lists of option strings' of the
-    to-be-excluded options, e.g. ``['-Q', '--quux'], ['spam']``.
-
-    This can be used for components that share most but not all of the
-    settings. (See the html4strict writer for an example.)
+    Optional positional arguments are names of to-be-excluded settings.
+    Keyword arguments are option specification replacements.
+    (See the html4strict writer for an example.)
     """
     settings = list(settings_spec)
+    # every third item is a sequence of option tuples
     for i in range(2, len(settings), 3):
-        settings[i] = tuple([option for option in settings[i]
-                             if option[1] not in exclude])
+        newopts = []
+        for opt_spec in settings[i]:
+            # opt_spec is ("<help>", [<option strings>], {<keyword args>})
+            opt_name = [opt_string[2:].replace('-', '_')
+                        for opt_string in opt_spec[1]
+                            if opt_string.startswith('--')
+                       ][0]
+            if opt_name in exclude:
+                continue
+            if opt_name in replace.keys():
+                newopts.append(replace[opt_name])
+            else:
+                newopts.append(opt_spec)
+        settings[i] = tuple(newopts)
     return tuple(settings)
 
 
