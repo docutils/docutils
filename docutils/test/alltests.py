506c1cfdd815bdeea083dcd4b3dadee0eeb25885
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2010-06-02 11:49:22 +0000

    simplify returning of test result (by Arfrever Frehtes Taifersar Arahesis)
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@6334 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/alltests.py
+++ docutils/test/alltests.py
@@ -83,8 +83,4 @@ if __name__ == '__main__':
     #    print >>sys.stderr, pformat(suite) # check the test suite
     finish = time.time()
     print 'Elapsed time: %.3f seconds' % (finish - start)
-    if result.errors or result.failures:
-        # return test result object to enable processing in programmatic use
-        sys.exit(result)
-    else: # no problems, exit with zero
-        sys.exit()
+    sys.exit(not result.wasSuccessful())
