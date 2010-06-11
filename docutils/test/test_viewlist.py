27986ceba4b408c031a901e82813736dbaf06043
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2010-06-11 10:31:18 +0000

    use failUnless instead of assertTrue for 2.3 compatibility
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@6340 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_viewlist.py
+++ docutils/test/test_viewlist.py
@@ -39,7 +39,7 @@ class ViewListTests(unittest.TestCase):
         self.assertEqual(self.b, self.b_list)
         self.assertEqual(self.c, self.c_list)
         self.assertEqual(len(self.a), len(self.a_list))
-        self.assertTrue('d' in self.a) # __contains__
+        self.failUnless('d' in self.a) # __contains__
         self.assertEqual([value for value in self.a], self.a_list)
         # get and set values
         self.assertEqual(self.a[2], self.a_list[2])
