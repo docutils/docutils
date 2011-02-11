aa31d59750a32feefebe04652b65362bdbed1204
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-02-11 07:13:28 +0000

    Added ability to read standard in, handle error messages better, etc.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6823 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/scripts/docutils_to_fo.py
+++ sandbox/paultremblay/scripts/docutils_to_fo.py
@@ -7,13 +7,18 @@ import docutilsToFo.rst2xml_lib
 import docutilsToFo.make_stylesheet
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Converts a file to XSLFO') 
-    parser.add_argument('--xslt', nargs=1, choices = ['xsltproc', 'lxml', 'saxon'], default=['lxml'], 
-            dest='xsl_transform', help = 'choose which processer to use when transforming' ) 
+    use = 'Converts a file to XSL-FO\n'
+    parser = argparse.ArgumentParser(description=use) 
+    # parser.add_argument('--xslt', nargs=1, choices = ['xsltproc', 'lxml', 'saxon'], default=['lxml'], 
+            # dest='xsl_transform', help = 'choose which processer to use when transforming' ) 
     parser.add_argument('--strict', nargs=1, choices = ['True', 'False'],  
             default = 'True', help = 'whether to quit on errors', dest='strict')
-    parser.add_argument('--clean', action="store_const", const=True, help = 'Whether to remove files after test', dest='clean')
-    parser.add_argument('in_path', nargs=1, help = 'The file to convert')
+    parser.add_argument('--clean', action="store_const", const=True, 
+            help = 'Whether to remove files after test', dest='clean')
+    parser.add_argument('--debug', action="store_const", 
+            const=True, help = 'print out verbose messages', dest='debug')
+    in_file_msg = 'The file to convert. Will either be a path to a file, or standard in.'
+    parser.add_argument('in_file', nargs='?', help = in_file_msg, default=sys.stdin)
     parser.add_argument('-s, -stylesheet', nargs=1, help = 'The root stylesheet to use',
             dest='root_stylesheet')
     parser.add_argument('-o, --out', nargs=1, help = 'Path to output result', dest='out_path')
@@ -22,15 +27,19 @@ def get_args():
  
 
 
-home= os.environ.get('HOME')
-home_config_file = os.path.join(home, '.docutils')
-cwd = os.getcwd()
-project_config_file = os.path.join(cwd, '.docutils.conf')
+home_config_file = os.path.join(os.environ.get('HOME'), '.docutils')
+project_config_file = os.path.join(os.getcwd(), '.docutils.conf')
 config = ConfigParser.SafeConfigParser()
 config.read([home_config_file, project_config_file])
 
 
 arg = get_args()
+debug = arg.debug
+if debug: sys.stderr.write('In debug mode\n')
+if debug: sys.stderr.write('script is "%s"\n' % __file__)
+in_file = arg.in_file
+if debug:
+    sys.stderr.write('in_file is "%s"\n' % str(in_file))
 
 if arg.root_stylesheet:
     root_stylesheet = arg.root_stylesheet[0]
@@ -38,23 +47,14 @@ else:
     try:
         root_stylesheet = config.get('FO', 'xsl-stylesheet')
     except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-        sys.stderr.write('No stylesheet found.\n')
-        sys.stderr.write('Script quiting.\n')
-        sys.exit(1)
+        root_stylesheet = None
 
-
-
+if debug: sys.stderr.write('root_stylesheet is "%s"\n' % str(root_stylesheet))
 out_path = None
-in_path = arg.in_path[0]
 if arg.out_path:
     out_path = arg.out_path[0]
-    # filename, ext = os.path.splitext(out_path)
-    # out_xml = '%s.xml' % (filename)
 else:
     out_xml = tempfile.mkstemp(suffix = '.fo')[1]
-
-# convert to XML
-# docutilsToFo.rst2xml_lib.publish_xml_cmdline(in_path = in_path, out_path = out_xml)
 
 # get path to put stylesheet
 if out_path:
@@ -62,19 +62,25 @@ if out_path:
     out_xsl = '%s.xsl' % (filename)
 else:
     out_xsl = tempfile.mkstemp(suffix = '.xsl')[1]
+if debug:
+    sys.stderr.write('out_xsl (file to output XSL stylesheet) is "%s"\n' % (out_xsl))
 
+verbose = 0
+if debug:
+    verbose = 5
 # make a stylesheet
-ss_obj = docutilsToFo.make_stylesheet.ReadConfig(import_ss = root_stylesheet )
+ss_obj = docutilsToFo.make_stylesheet.ReadConfig(import_ss = root_stylesheet, verbose = verbose)
 ss_string = ss_obj.make_stylesheet()
 write_obj = file(out_xsl, 'w')
 write_obj.write(ss_string)
 write_obj.close()
 
 # convert to FO 
-error = docutilsToFo.rst2xml_lib.transform_lxml(xslt_file = out_xsl, xml_file = in_path, 
-        param_dict = {}, out_file = out_path )
+error = docutilsToFo.rst2xml_lib.transform_lxml(xslt_file = out_xsl, xml_file = in_file, 
+        param_dict = {}, out_file = out_path, verbose = verbose )
 if error:
-    sys.stderr.write(error)
+    if type(error) == type(' '):
+        sys.stderr.write(error)
     sys.exit(1)
 
 
