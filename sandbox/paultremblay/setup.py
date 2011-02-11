c6e5a1c0c31c7b9f789a1a5746fd7a6f2fe75f77
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-02-11 03:37:27 +0000

    Script for installation.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@6813 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/paultremblay/setup.py
@@ -0,0 +1,28 @@
+import sys, os, shutil, glob
+from distutils.core import setup
+
+def remove_build():
+    if os.path.isdir('build'):
+        shutil.rmtree('build')
+
+# get the script name
+def get_script_name():
+    return os.path.join('scripts', 'docutils_to_fo.py')
+
+xsl_files = glob.glob('xsl_fo/*')
+
+remove_build()
+script_name = get_script_name()
+
+setup(name="docutilsToFo",
+    version= '.6' ,
+    description="Convert Docutils to FO",
+    author="Paul Tremblay",
+    # author_email="phthenry@iglou.com",
+    license = 'GNU GPL',
+    # url = "http://rtf2xml.sourceforge.net/",
+    packages=['docutilsToFo'],
+    package_dir = {'docutilsToFo': 'docutilsToFo'},
+    package_data={'docutilsToFo': ['xsl_fo/*.xsl']},
+    scripts=[script_name],
+    )
