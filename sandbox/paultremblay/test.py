70b265db2e7982341e514d56eaf90e3aa18cdcef
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-02-12 04:48:46 +0000

    added test.py
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6832 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/test.py
+++ sandbox/paultremblay/test.py
@@ -7,6 +7,21 @@ from docutilsToFo.rst2xml_lib import transform_lxml, publish_xml_cmdline
 # $Id$ 
 TEST = False
 STRICT = True
+
+parser = argparse.ArgumentParser(description='Test all the files') 
+parser.add_argument('--xslt', nargs=1, choices = ['xsltproc', 'lxml', 'saxon'], default=['lxml'], 
+        dest='xsl_transform', help = 'choose which processer to use when transforming' ) 
+parser.add_argument('--no-pdf', action="store_const", const=True, help = 'don\'t convert to PDF', dest='no_pdf')
+parser.add_argument('--no-rst', action="store_const", const=True, help = 'don\'t convert to RST', dest='no_rst')
+parser.add_argument('--strict', nargs=1, choices = ['True', 'False'],  
+        default = 'True', help = 'whether to quit on errors', dest='strict')
+parser.add_argument('--clean', action="store_const", const=True, help = 'whether to remove files after test', dest='clean')
+arg = parser.parse_args()
+if  arg.strict == 'True':
+    STRICT = True
+else:
+    STRICT = False
+
 test_dict = {
         'long_plain.xml':[({'page-layout':'simple'}, 'simple_no_page_nos.fo'), 
                 ({'page-layout': 'first'}, 'first_page_diff_no_page_nos.fo'),  
@@ -85,8 +100,11 @@ test_dict = {
             ]
         }
 
-def error_func(msg):
-    sys.stderr.write(msg)
+def error_func(msg, the_path = None):
+    if the_path:
+        sys.stderr.write('Problems with "%s"\n' % the_path)
+    if type(msg) == type(''):
+        sys.stderr.write(msg)
     if STRICT:
         sys.exit(1)
         pass
@@ -127,46 +145,46 @@ def transform_xsl(xsl_file, xml_file, param_dict = {}, out_file = None, xsl_tran
             msg += '\n'
             error_func(msg)
     elif xsl_transform == 'lxml':
-        error = transform_lxml(xsl_file, xml_file, param_dict, out_file)
+        error = transform_lxml(xslt_file = xsl_file, xml_file= xml_file, param_dict=param_dict, out_file= out_file)
         if error:
-            error_func(error)
+            error_func(error, xml_file)
     else:
         sys.stderr.write('"%s" not a valid xsl_transform. Fix. Script now quitting\n' % (xsl_transform))
         sys.exit(1)
 
 
 def convert_to_xml(path_list):
-    print 'converting to xml...'
+    # print 'converting to xml...'
     num_files = len(path_list)
     counter = 0
     for the_path in path_list:
         counter += 1
-        print 'converting %s of %s files' % (counter, num_files)
+        # print 'converting %s of %s files' % (counter, num_files)
         base, ext = os.path.splitext(the_path)
         out_file = '%s.xml' % (base)
         publish_xml_cmdline (in_path = the_path, out_path = out_file)
 
 # simple, fail-proof method
 def convert_to_xml_command(path_list):
-    print 'converting to xml...'
+    # print 'converting to xml...'
     num_files = len(path_list)
     counter = 0
     for the_path in path_list:
         counter += 1
-        print 'converting %s of %s files' % (counter, num_files)
+        # print 'converting %s of %s files' % (counter, num_files)
         base, ext = os.path.splitext(the_path)
         out_file = '%s.xml' % (base)
         command = 'rst2xml.py --trim-footnote-reference-space %s %s' % (the_path, out_file)
         status, output = commands.getstatusoutput(command)
 
 def convert_to_fo(xsl_transform):
-    print 'converting to fo...'
+    # print 'converting to fo...'
     xml_files =  glob.glob('*.xml')
     len_simple = len(xml_files)
     the_keys = test_dict.keys()
     for the_key in the_keys:
         if the_key not in xml_files:
-            print the_key, 'not found in test_files; fix. Script now quitting'
+            sys.stderr.write('%s  not found in test_files; fix. Script now quitting' % (the_key))
             sys.exit(1)
     len_complex = len(the_keys)
     len_inside = 0
@@ -181,25 +199,26 @@ def convert_to_fo(xsl_transform):
         if transform_info:
             for info in transform_info:
                 counter += 1
-                print 'converting %s of %s files' % (counter, num_files)
+                # print 'converting %s of %s files' % (counter, num_files)
                 added_params = info[0]
                 out_file = info[1]
                 params.update(added_params)
-                transform_xsl('../xsl_fo/docutils_to_fo.xsl', xml_file = xml_file, param_dict= params, out_file = out_file,
+                transform_xsl(xsl_file = '../docutilsToFo/xsl_fo/docutils_to_fo.xsl', 
+                        xml_file = xml_file, param_dict= params, out_file = out_file,
                         xsl_transform = xsl_transform)
         else:
             counter += 1
-            print 'converting %s of %s files' % (counter, num_files)
-            transform_xsl('../xsl_fo/docutils_to_fo.xsl', xml_file = xml_file, param_dict=params, xsl_transform = xsl_transform)
+            # print 'converting %s of %s files' % (counter, num_files)
+            transform_xsl(xsl_file = '../docutilsToFo/xsl_fo/docutils_to_fo.xsl', xml_file = xml_file, param_dict=params, xsl_transform = xsl_transform)
 
 def convert_to_pdf():
-    print 'converting to pdf...'
+    # print 'converting to pdf...'
     fo_files =  glob.glob('*.fo')
     num_files = len(fo_files)
     counter = 0
     for fo_file in fo_files:
         counter += 1
-        print 'converting %s of %s files' % (counter, num_files)
+        # print 'converting %s of %s files' % (counter, num_files)
         base, ext = os.path.splitext(fo_file)
         out_file = '%s.pdf' % (base)
         command = 'fop  %s %s' % (fo_file, out_file)
@@ -212,17 +231,6 @@ def convert_to_pdf():
             error(msg)
 
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Test all the files') 
-    parser.add_argument('--xslt', nargs=1, choices = ['xsltproc', 'lxml', 'saxon'], default=['lxml'], 
-            dest='xsl_transform', help = 'choose which processer to use when transforming' ) 
-    parser.add_argument('--no-pdf', action="store_const", const=True, help = 'don\'t convert to PDF', dest='no_pdf')
-    parser.add_argument('--no-rst', action="store_const", const=True, help = 'don\'t convert to RST', dest='no_rst')
-    parser.add_argument('--strict', nargs=1, choices = ['True', 'False'],  
-            default = 'True', help = 'whether to quit on errors', dest='strict')
-    parser.add_argument('--clean', action="store_const", const=True, help = 'whether to remove files after test', dest='clean')
-    return  parser.parse_args()
-
 def clean():
     xml_files = glob.glob('*.xml')
     for xml_file in xml_files:
@@ -231,8 +239,13 @@ def clean():
     for fo_file in fo_files:
         os.remove(fo_file)
 
+def test_docutils_to_fo_script(script_command):
+    command = '%s -h' % script_command
+    status, output = commands.getstatusoutput(command)
+
 def main():
-    arg = get_args()
+    script_path  = os.path.abspath(os.path.join('scripts', 'docutils_to_fo.py'))
+    script_command = 'python %s ' % script_path
     current_dir = os.getcwd()
     os.chdir('test_files')
     if not arg.no_rst:
@@ -241,6 +254,7 @@ def main():
     convert_to_fo(xsl_transform = arg.xsl_transform[0])
     if not arg.no_pdf:
         convert_to_pdf()
+    test_docutils_to_fo_script(script_command)
     os.chdir(current_dir)
 
 try:
