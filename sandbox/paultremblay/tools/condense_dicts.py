76511e9321d5d47bc0a54f4d2e76b6fdd00370ba
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-02-14 05:17:08 +0000

    Nothing important.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6860 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/tools/condense_dicts.py
+++ sandbox/paultremblay/tools/condense_dicts.py
@@ -1,63 +1,85 @@
 from docutilsToFo.docutils_fo_dicts import *
-import pprint
-def find_diff(dict1, dict2):
-    set1 = set(dict1)
-    set2 = set(dict2)
-    diff = set1.symmetric_difference(set2)
-    return len(diff), diff
-
-def find_common(dict1, dict2):
-    set1 = set(dict1)
-    set2 = set(dict2)
-    common = set1.intersection(set2)
-    return common
-
+import pprint, copy
 def dump(object):
-    pp = pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=0)
     pp.pprint(object)
 
-list_of_dicts = []
-all_dicts = which_dict.keys()
-for the_str in all_dicts:
-    the_dict = which_dict[the_str]
-    pair = (the_str, the_dict)
-    list_of_dicts.append(pair)
-
-list_of_dicts.sort()
-
-counter = 0
-for pair in list_of_dicts:
-    first = pair[0]
-    if counter == len(list_of_dicts) - 1:
-        break
-    for j in range(counter + 1, len(list_of_dicts)):
-        # print list_of_dicts[j][0]
-        second = list_of_dicts[j][0]
-        if first == second:
-            print 'woops'
-        first_dict = list_of_dicts[counter][1]
-        second_dict = list_of_dicts[j][1]
-        the_diff, diff_dict = find_diff(first_dict, second_dict )
-        if the_diff < 20:
-            if len(first_dict) < 20 or len(second_dict) < 20:
-                pass
-            else:
-                print '%s and %s have %s diff' % (first, second, the_diff)
-                common_dict = find_common(first_dict, second_dict)
-                # dump(common_dict)
-                dump(diff_dict)
-    counter += 1
+list_of_dicts2_ = [
+block_dict,
+block_container_dict,
+cell_dict,
+flow_dict,
+footnote_body_dict,
+header_dict,
+inline_dict,
+item_body_dict,
+item_label_dict,
+list_block_dict,
+list_item_dict,
+page_sequence_dict,
+region_after_dict,
+region_before_dict,
+region_body_dict,
+simple_page_master_dict,
+table_dict,
+table_body_dict,
+table_row_dict,
+]
 
 
 
-block_set = set(block_dict.keys())
-block_container_set = set(block_container_dict.keys())
-cell_set= set(cell_dict.keys())
-intersect_block_block_container = block_set.intersection(block_container_set)
-intersect_block_container_cell = block_container_set.intersection(cell_set)
-diff = block_container_set.symmetric_difference(block_set)
+s_block = set(block_dict)
+s_block_container = set(block_container_dict)
+s_cell = set(cell_dict)
+s_flow = set(flow_dict)
+s_footnote_body = set(footnote_body_dict)
+s_header = set(header_dict)
+s_inline = set(inline_dict)
+s_item_body = set(item_body_dict)
+s_item_label = set(item_label_dict)
+s_list_block = set(list_block_dict)
+s_list_item = set(list_item_dict)
+s_page_sequence = set(page_sequence_dict)
+s_region_after= set(region_after_dict)
+s_region_before = set(region_before_dict)
+s_region_body = set(region_body_dict)
+s_simple_page_master= set(simple_page_master_dict)
+s_table = set(table_dict)
+s_table_body = set(table_body_dict)
+s_table_row = set(table_row_dict)
 
-rbe_set = set(region_before_dict.keys())
-rbo_set = set(region_body_dict.keys())
-diff = rbe_set.symmetric_difference(rbo_set)
+
+
+"""
+new_block_container_dict = {}
+diff = set(block_container_dict).difference(set(azimuth_dict))
+new_dict = {}
+for item in diff:
+    new_dict[item] = item
+dump(new_dict)
+"""
+"""
+new_region_after_dict = {}
+new_region_after_dict.update(common_dict1)
+new_region_after_dict.update(padding_dict)
+diff = set(new_region_after_dict).symmetric_difference(set(region_after_dict))
+new_dict = {}
+for item in diff:
+    new_dict[item] = item
+dump(new_dict)
+"""
+diff = set(new_region_after_dict).symmetric_difference(set(region_after_dict))
+print diff
+
+
+
+# items in the first (list_block_dict) not in second. the second is the base.
+# diff = set(list_block_dict).difference(set(list_item_dict))
+
+
+
+
+
+
+
 
