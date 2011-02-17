3d11fb1392665d2773348d47646be9bc41896720
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-02-17 15:39:55 +0000

    Added command to document rst howto.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@6878 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/docs/make_xsl-documentation.sh
+++ sandbox/paultremblay/docs/make_xsl-documentation.sh
@@ -9,11 +9,13 @@ if [ "$PWD" != 'docs' ];then
     exit 1
 fi
 
-xsltproc xsl/document_parameters.xsl ../xsl_fo/parameters.xsl > doc.xml
+xsltproc xsl/document_parameters.xsl ../docutilsToFo/xsl_fo/parameters.xsl > doc.xml
 xml2txt.py doc.xml > rst/parameters.rst
 rst2html.py  rst/parameters.rst >  html/parameters.html 
 xsltproc xsl/document_stylesheet.xsl xsl/document_stylesheet.xsl > doc.xml
 xml2txt.py doc.xml > rst/attribute_sets.rst
 rst2html.py  rst/attribute_sets.rst >  html/attribute_sets.html 
+
+rst2html.py rst/howto.rst > html/howto.html
 # rm doc.xml
 
