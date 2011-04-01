a6135fd03b859280a090a896f8369948452be434
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-04-01 11:47:29 +0000

    Fix command line test.
    Decode output with "utf8" (Docutils standard output encoding).
    Compare only the first line of output, strip line ending
    (removes CR vs. LFCR issues).
    
    Thanks to Alan for testing on Windows.
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@6998 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_command_line.py
+++ docutils/test/test_command_line.py
@@ -8,7 +8,7 @@
 Test module for the command line.
 """
 
-import os.path
+from os.path import normpath, dirname
 import unittest
 import sys
 import DocutilsTestSupport              # must be imported before docutils
@@ -25,37 +25,31 @@ try:
 except:
     argv_encoding = None
 
-testoutput = u"""\
-<document source="<stdin>" title="Dornröschen">
-    <decoration>
-        <footer>
-            <paragraph>
-"""
+testoutput = u'<document source="<stdin>" title="Dornröschen">'
 
 class CommandLineEncodingTests(unittest.TestCase):
-
-    # This does not work, as there is no "encoding" argument!
-    # def test_argv_encoding(self):
-    #     if argv_encoding is None:
-    #         # failure to load "locale" module
-    #         return
-    #     if sys.argv:
-    #         self.assertEqual(sys.argv[0].encoding,
-    #                          locale.getpreferredencoding())
 
     def test_argv_decoding(self):
         if argv_encoding is None:
             # failure to load "locale" or "subprocess" module
             return # nothing to test
-        cmd_str = os.path.normpath(os.path.dirname(os.path.dirname(__file__)) +
-                                   '/tools/rst2pseudoxml.py')
-        cmd_str += u' --no-generator --no-datestamp --title=Dornröschen'
+        
+        # This does not work, as there is no "encoding" argument!
+        # if sys.argv:
+        #     self.assertEqual(sys.argv[0].encoding,
+        #                      locale.getpreferredencoding())
+        # so instead, we check if a command line with non-ASCII char works
+        
+        cmd_str = normpath(dirname(dirname(__file__)) + 
+                           '/tools/rst2pseudoxml.py')
+        cmd_str += u' --title=Dornröschen'
         p = subprocess.Popen([cmd_str.encode(argv_encoding)], shell=True,
                              stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         p.stdin.close()
-        output = p.stdout.read().decode(argv_encoding)
+        output = p.stdout.readlines()
         p.stdout.close()
-        self.assertEqual(output, testoutput)
+        self.assertEqual(output[0].decode('utf8').strip(), testoutput)
+
 
 if __name__ == '__main__':
     unittest.main()
