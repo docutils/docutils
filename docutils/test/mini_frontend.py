9e85d26e490038e5252c13f6698600b9a12f74ec
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-04-08 19:57:44 +0000

    Let command line test work without installing Docutils
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7002 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/mini_frontend.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# :Copyright: © 2011 Günter Milde.
+#             Released  without warranties or conditions of any kind
+#             under the terms of the Apache License, Version 2.0
+#             http://www.apache.org/licenses/LICENSE-2.0
+# :Id: $Id$
+
+"""
+A minimal Docutils front-end for testing the command line
+"""
+
+# allow import also if Doctuils is not installed
+import sys, os
+testroot = os.path.abspath(os.path.dirname(__file__) or os.curdir)
+sys.path.insert(0, os.path.normpath(os.path.join(testroot, '..')))
+
+from docutils.core import publish_cmdline
+
+publish_cmdline(description='Generates pseudo-XML for testing purposes.')
