8dd1337227f68924a078488484ee92d5b499da1e
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-04-12 07:16:39 +0000

    Simpler and more robust test of command line decoding.
    Test `Publisher` class after modifying sys.argv 
    instead of testing frontend.
    
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7006 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_command_line.py
+++ docutils/test/test_command_line.py
@@ -8,12 +8,10 @@
 Test module for the command line.
 """
 
-from os.path import normpath, dirname
 import unittest
 import sys
 import DocutilsTestSupport              # must be imported before docutils
 import docutils.core
-import docutils.utils
 
 try:
     import subprocess  # new in 2.4
@@ -25,29 +23,16 @@ try:
 except:
     argv_encoding = None
 
-testoutput = u'<document source="<stdin>" title="Dornröschen">'
-
 class CommandLineEncodingTests(unittest.TestCase):
-
-    def test_argv_decoding(self):
-        if argv_encoding is None:
-            # failure to load "locale" or "subprocess" module
-            return # nothing to test
-
-        # This does not work, as there is no "encoding" argument!
-        # if sys.argv:
-        #     self.assertEqual(sys.argv[0].encoding,
-        #                      locale.getpreferredencoding())
-        # so instead, we check if a command line with non-ASCII char works
-        cmd_str = (u'%s %s/mini_frontend.py --title=Dornröschen' %
-                   (sys.executable, normpath(dirname(__file__) + '')))
-        p = subprocess.Popen([cmd_str.encode(argv_encoding)], shell=True,
-                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        p.stdin.close()
-        output = p.stdout.readlines()
-        p.stdout.close()
-        self.assertEqual(output[0].decode('utf8').strip(), testoutput)
-
+    
+    def test_sys_argv_decoding(self):
+        if argv_encoding is None: # cannot test
+            return
+        sys.argv.append(u'--title=Dornröschen'.encode(argv_encoding))
+        publisher = docutils.core.Publisher()
+        publisher.process_command_line()
+        self.assertEqual(publisher.settings.title, u'Dornröschen')
+        sys.argv.pop()
 
 if __name__ == '__main__':
     unittest.main()
