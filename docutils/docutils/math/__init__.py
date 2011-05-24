f7f409514afb682541e7a858568182cccb357728
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-05-24 12:33:32 +0000

    Update the copyright/licensing info. Fixes [3304675]
    BSD license is not used in Docutils.
    Some new parts use the Apache License version 2.0.
    PSF contributing policy is that "contributors remain copyright holders
    of their contributions" (http://www.python.org/psf/contrib/).
    
    
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7040 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/math/__init__.py
+++ docutils/docutils/math/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-# :Copyright: © 2008 Günter Milde.
+# :Copyright: © 2011 Günter Milde.
 #             Released  without warranties or conditions of any kind
 #             under the terms of the Apache License, Version 2.0
-#             http://www.apache.org/licenses/LICENSE-2.0
+#             (http://www.apache.org/licenses/LICENSE-2.0)
 # :Id: $Id$
 
 """
@@ -17,3 +17,15 @@ It contains various modules for conversion between different math formats
 :mathtools:    helpers for Docutils math support
 :unimathsymbols2tex: Unicode symbol to LaTeX math translation table
 """
+
+
+# While Docutils components can be used as parts in programs released
+# under GPL v3, it is not possible to include GPLed-code in a public
+# domain or Apache-2 licensed program.
+#
+# math2html.py is part of eLyXer_, released under the `GNU General
+# Public License`_ version 3 or later.  The author kindly relicensed it
+# for Docutils under the Apache License, Version 2.0.
+#
+# .. _Docutils policies: docs/dev/policies.txt
+# .. _eLyXer: http://www.nongnu.org/elyxer/
