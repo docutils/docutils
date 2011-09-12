81489b0a25648cdcd780e534e209ba5d3326fc78
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-09-12 08:01:29 +0000

    Apply [ 3405410 ]: Use utf-8 encoded literal Cyrillic characters
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7122 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/languages/ru.py
+++ docutils/docutils/languages/ru.py
@@ -14,51 +14,43 @@ Russian-language mappings for language-dependent features of Docutils.
 __docformat__ = 'reStructuredText'
 
 labels = {
-      u'abstract': u'\u0410\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f',
-      u'address': u'\u0410\u0434\u0440\u0435\u0441',
-      u'attention': u'\u0412\u043d\u0438\u043c\u0430\u043d\u0438\u0435!',
-      u'author': u'\u0410\u0432\u0442\u043e\u0440',
-      u'authors': u'\u0410\u0432\u0442\u043e\u0440\u044b',
-      u'caution': u'\u041e\u0441\u0442\u043e\u0440\u043e\u0436\u043d\u043e!',
-      u'contact': u'\u041a\u043e\u043d\u0442\u0430\u043a\u0442',
-      u'contents':
-      u'\u0421\u043e\u0434\u0435\u0440\u0436\u0430\u043d\u0438\u0435',
-      u'copyright': u'\u041f\u0440\u0430\u0432\u0430 '
-      u'\u043a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u044f',
-      u'danger': u'\u041e\u041f\u0410\u0421\u041d\u041e!',
-      u'date': u'\u0414\u0430\u0442\u0430',
-      u'dedication':
-      u'\u041f\u043e\u0441\u0432\u044f\u0449\u0435\u043d\u0438\u0435',
-      u'error': u'\u041e\u0448\u0438\u0431\u043a\u0430',
-      u'hint': u'\u0421\u043e\u0432\u0435\u0442',
-      u'important': u'\u0412\u0430\u0436\u043d\u043e',
-      u'note': u'\u041f\u0440\u0438\u043c\u0435\u0447\u0430\u043d\u0438\u0435',
-      u'organization':
-      u'\u041e\u0440\u0433\u0430\u043d\u0438\u0437\u0430\u0446\u0438\u044f',
-      u'revision': u'\u0420\u0435\u0434\u0430\u043a\u0446\u0438\u044f',
-      u'status': u'\u0421\u0442\u0430\u0442\u0443\u0441',
-      u'tip': u'\u041f\u043e\u0434\u0441\u043a\u0430\u0437\u043a\u0430',
-      u'version': u'\u0412\u0435\u0440\u0441\u0438\u044f',
-      u'warning': u'\u041f\u0440\u0435\u0434\u0443\u043f\u0440\u0435\u0436'
-      u'\u0434\u0435\u043d\u0438\u0435'}
+      u'abstract': u'Аннотация',
+      u'address': u'Адрес',
+      u'attention': u'Внимание!',
+      u'author': u'Автор',
+      u'authors': u'Авторы',
+      u'caution': u'Осторожно!',
+      u'contact': u'Контакт',
+      u'contents': u'Содержание',
+      u'copyright': u'Права копирования',
+      u'danger': u'ОПАСНО!',
+      u'date': u'Дата',
+      u'dedication': u'Посвящение',
+      u'error': u'Ошибка',
+      u'hint': u'Совет',
+      u'important': u'Важно',
+      u'note': u'Примечание',
+      u'organization': u'Организация',
+      u'revision': u'Редакция',
+      u'status': u'Статус',
+      u'tip': u'Подсказка',
+      u'version': u'Версия',
+      u'warning': u'Предупреждение'}
 """Mapping of node class name to label text."""
 
 bibliographic_fields = {
-      u'\u0430\u043d\u043d\u043e\u0442\u0430\u0446\u0438\u044f': u'abstract',
-      u'\u0430\u0434\u0440\u0435\u0441': u'address',
-      u'\u0430\u0432\u0442\u043e\u0440': u'author',
-      u'\u0430\u0432\u0442\u043e\u0440\u044b': u'authors',
-      u'\u043a\u043e\u043d\u0442\u0430\u043a\u0442': u'contact',
-      u'\u043f\u0440\u0430\u0432\u0430 \u043a\u043e\u043f\u0438\u0440\u043e'
-      u'\u0432\u0430\u043d\u0438\u044f': u'copyright',
-      u'\u0434\u0430\u0442\u0430': u'date',
-      u'\u043f\u043e\u0441\u0432\u044f\u0449\u0435\u043d\u0438\u0435':
-      u'dedication',
-      u'\u043e\u0440\u0433\u0430\u043d\u0438\u0437\u0430\u0446\u0438\u044f':
-      u'organization',
-      u'\u0440\u0435\u0434\u0430\u043a\u0446\u0438\u044f': u'revision',
-      u'\u0441\u0442\u0430\u0442\u0443\u0441': u'status',
-      u'\u0432\u0435\u0440\u0441\u0438\u044f': u'version'}
+      u'аннотация': u'abstract',
+      u'адрес': u'address',
+      u'автор': u'author',
+      u'авторы': u'authors',
+      u'контакт': u'contact',
+      u'права копирования': u'copyright',
+      u'дата': u'date',
+      u'посвящение': u'dedication',
+      u'организация': u'organization',
+      u'редакция': u'revision',
+      u'статус': u'status',
+      u'версия': u'version'}
 """Russian (lowcased) to canonical name mapping for bibliographic fields."""
 
 author_separators =  [';', ',']
