351a1be220a530bf5f3706a90dba1f2b119e333e
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-09-27 19:06:53 +0000

    Added options.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7140 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/scripts/docutils_to_fo.sh
+++ sandbox/paultremblay/scripts/docutils_to_fo.sh
@@ -1,7 +1,11 @@
 XSLFO_HOME=/Users/cejohnsonlouisville/Documents/docutils/paultremblay/xsl_fo
+if [ "$XSLFO_PDF" != "" ]; then
+    PDF='true'
+else
+    PDF='false'
+fi
 set -e
 set -u
-#  $Id: oo_unzip.sh 214 2011-09-24 01:47:45Z cejohnsonlouisville $
 Usage() {
     echo Converts a restructured text document to FO, then to PDF 
     echo "docutils_to_fo.sh [options] <RST file>"
@@ -9,13 +13,16 @@ Usage() {
     echo --noclean: no not remove files created
     echo --pdf: create a PDF document
     echo --valid: validate the FO document
+    echo "-s | --stylesheet <stylesheet> : the stylesheet to use"
+    echo "-o | --out: file to output to"
 }
 CLEAN='true'
 FORMAT=
 TEST=
 TESTQ=
-PDF='false'
 VALID='false'
+STYLESHEET=
+OUT=''
  while [ $# -gt 0 ]
  do
      case "$1" in
@@ -26,18 +33,25 @@ VALID='false'
          --verbose) VERBOSE='true';;
          --format) FORMAT='true';;
          --test) CLEAN='false';FORMAT='true';VALID='true';PDF='true';TEST='true';;
-         --testq) CLEAN='false';FORMAT='true';VALID='true';PDF='true';;
+         --testq) CLEAN='false';VALID='true';FORMAT='true';;
          --noclean) CLEAN='false';;
          --pdf) PDF='true';;
          --valid) VALID='true';;
          --out) shift;OUT=$1;;
          -o) shift;OUT=$1;;
+         --stylesheet) shift;STYLESHEET=$1;;
+         -s) shift;STYLESHEET=$1;;
  	-*)     echo "Illegal command $1"; Usage; exit 1;;                 # ===> illegal help
           *)     break;;			# first argument
      esac   
      shift
  done
-MAIN_XSL=${XSLFO_HOME}/docutils_to_fo.xsl
+
+if [ "$STYLESHEET" == "" ]; then
+    MAIN_XSL=${XSLFO_HOME}/docutils_to_fo.xsl
+else
+    MAIN_XSL=$STYLESHEET
+fi
 
 EXT=`echo "$1"|awk -F . '{print $NF}'` 
 BASENAME=`basename $1 .${EXT}`
@@ -48,22 +62,43 @@ else
     DIRNAME=$DIR
 fi
 
-RAW_XML=${DIRNAME}/docutils_to_fo_resulting_rst.xml
-FO_FILE=${DIRNAME}/docutils_to_fo_resulting_fo.fo
-
-if [ "$FORMAT" == "true" ]; then
-    rst2xml.py --strip-comments --trim-footnote-reference-space $1 | xmlformat.pl > $RAW_XML
-elif [ "$TEST" == "true" ];then 
-    echo just a test
-else
-    rst2xml.py --strip-comments --trim-footnote-reference-space $1 > $RAW_XML
+if [ ! -w "$DIRNAME" ]; then
+    echo "has write permission"
 fi
 
-if [ "$FORMAT" == "true" ]; then
-    xsltproc $MAIN_XSL $RAW_XML  > $FO_FILE
-    xmlformat.pl -i $FO_FILE
+if [ "${OUT}" != "" ];then
+    touch ${OUT} 
+    rm -f ${OUT}
+    EXT=`echo "${OUT}"|awk -F . '{print $NF}'` 
+    if [ ${OUT} == ${EXT} ]; then
+        OUT_BASE=${OUT}
+    else
+        OUT_BASE=`basename ${OUT} .${EXT}`
+    fi
+fi
+
+if [ "${OUT}" != "" ]; then
+    RAW_XML=${OUT_BASE}.xml
 else
-    xsltproc $MAIN_XSL $RAW_XML  > $FO_FILE
+    RAW_XML=${DIRNAME}/${BASENAME}.xml
+fi
+
+if [ "${OUT}" != "" ]; then
+    FO_FILE=${OUT_BASE}.fo
+else
+    FO_FILE=${DIRNAME}/${BASENAME}.fo
+fi
+
+rst2xml.py --strip-comments --trim-footnote-reference-space $1 > $RAW_XML
+
+if [ "$FORMAT" == "true" ]; then
+    xmlformat.pl -i  $RAW_XML
+fi
+
+xsltproc $MAIN_XSL $RAW_XML  > $FO_FILE
+
+if [ "$FORMAT" == "true" ]; then
+    xmlformat.pl -i $FO_FILE
 fi
 
 if [ "$VALID" == 'true' ]; then
@@ -76,7 +111,6 @@ if [ "$PDF" == 'true' ]; then
 fi
 
 if [ "$CLEAN" == "true" ]; then
-    rm -f $FO_FILE
     rm -f $RAW_XML
     if [ "$TEST" == 'true' ]; then
         rm $PDF_FILE
