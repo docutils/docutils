c50dd91600ec8199f8083d0d0b4775bb1abcf28c
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-10-06 03:43:28 +0000

    Small changes.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7151 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/test.sh
+++ sandbox/paultremblay/test.sh
@@ -1,5 +1,10 @@
 set -e
 set -u
+RST_COMMAND="rst2xml.py --strip-comments --trim-footnote-reference-space"
 docutils_to_fo.sh  --test test_files/header_footer.rst
 docutils_to_fo.sh  --test test_files/footnotes.rst
+$RST_COMMAND test_files/endnotes.rst test_files/endnotes.xml
+fop -xml test_files/endnotes.xml -xsl test_files/endnotes.xsl -pdf test_files/endnotes.pdf
 docutils_to_fo.sh  --test test_files/toc.rst
+docutils_to_fo.sh  --test test_files/bibliographic_fields.rst
+
