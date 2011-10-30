5c0bb2ba2b118d1986f0a86be61d271fe85064a8
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-10-30 19:42:28 +0000

    Further work on module. Very simple expressions and simple parenthesis now work.
    git-svn-id: https://docutils.svn.sourceforge.net/svnroot/docutils/trunk@7210 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/paultremblay/other/asciitomathml.py
+++ sandbox/paultremblay/other/asciitomathml.py
@@ -9,14 +9,20 @@ class AsciiMathML:
     symbol_dict = {
     'alpha': u"\u03B1",
         }
+    special_dict = {
+            '(':'open_paren',
+            ')':'close_paren',
+            }
     symbol_names = sorted(symbol_dict.keys(), key=lambda key_string: len(key_string), reverse=True)
+    special_names = ['(', ')']
 
-    def __init__(self):
+    def __init__(self, output_encoding = 'utf8'):
         self.__number_re = re.compile('-?(\d+\.(\d+)?|\.?\d+)')
         self.__tree = Element('math')
         mstyle = etree.SubElement(self.__tree, 'mstyle')
         self.__mathml_ns = 'http://www.w3.org/1998/Math/MathML'
         self.__append_el = mstyle
+        self.__output_encoding = output_encoding
 
     def make_element(self, tag, text=None, *children, **attrib):
         element = Element(tag, **attrib)
@@ -79,19 +85,23 @@ class AsciiMathML:
         parent = child_parent_map.get(child)
         return parent
 
-    def parse(self, the_string):
-        pass
 
     def __add_namespace(self):
         attributes = self.__tree.attrib
         value = attributes.get('xmlns')
         if not value:
-            self.__tree.set('xmlsn', self.__mathml_ns)
+            self.__tree.set('xmlns', self.__mathml_ns)
 
-    def to_xml_string(self):
+    def to_xml_string(self, encoding=None):
+        if not encoding:
+            encoding = self.__output_encoding
         self.__add_namespace()
-        xml_string = tostring(self.__tree)
+        xml_string = tostring(self.__tree, encoding=encoding)
         return xml_string
+
+    def get_tree(self):
+        self.__add_namespace()
+        return self.__tree
 
     def __add_num_to_tree(self, token, the_type):
         element = self.make_element('mn', text=token)
@@ -104,6 +114,23 @@ class AsciiMathML:
         element = self.make_element('mn', text=num)
         self.__append_el.append(element)
 
+    def __add_symbol_alpha_to_tree(self, token, the_type):
+        element = self.make_element('mi', text=token)
+        self.__append_el.append(element)
+
+    def __add_operator_to_tree(self, token, the_type):
+        element = self.make_element('mo', text=token)
+        self.__append_el.append(element)
+
+    def __add_special_to_tree(self, token, the_type):
+        if token == '(':
+            element = self.make_element('mfenced', open='(')
+            self.__append_el.append(element)
+            self.__append_el = element
+        if token == ')':
+            if self.__append_el.tag == 'mfenced' and self.__append_el.get('open') == '(':
+                self.__append_el.set('close', ')')
+
     def parse_string(self, the_string):
         
         while the_string != '':
@@ -112,6 +139,13 @@ class AsciiMathML:
                 self.__add_num_to_tree(token, the_type)
             elif the_type == 'neg_number':
                 self.__add_neg_num_to_tree(token, the_type)
+            elif the_type == 'symbol' or the_type == 'alpha':
+                self.__add_symbol_alpha_to_tree(token, the_type)
+            elif the_type == 'operator':
+                self.__add_operator_to_tree(token, the_type)
+            elif the_type == 'special':
+                self.__add_special_to_tree(token, the_type)
+
 
     def parse_tokens(self, the_string):
 
@@ -126,6 +160,7 @@ class AsciiMathML:
         Else, get the next character, and process that with the rest of the string.
 
         """
+        the_string = the_string.strip()
 
         if the_string == '':
             return None, None, None
@@ -138,6 +173,12 @@ class AsciiMathML:
                 return the_string[match.end():], number, 'neg_number'
             else:
                 return the_string[match.end():], number, 'number'
+
+        for name in self.special_names:
+            if the_string.startswith(name):
+                special = the_string[:len(name)]
+                info = self.special_dict[special] # do nothing with this for now
+                return the_string[len(name):], special, 'special'
 
         for name in self.symbol_names: # found a special symbol
             if the_string.startswith(name):
@@ -152,3 +193,11 @@ class AsciiMathML:
             return the_string[1:], the_string[0], 'operator'
 
 
+def ascii_to_xml_string(the_string):
+    pass
+
+def ascii_to_math_tree(the_string):
+    math_obj =  AsciiMathML()
+    math_obj.parse_string(the_string)
+    math_tree = math_obj.get_tree()
+    return math_tree
