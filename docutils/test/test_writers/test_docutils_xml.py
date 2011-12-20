5d217ba6d1390f491e2630a9b96d15e39aec7841
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2011-12-20 09:36:10 +0000

    Allow new XML formatting also in version 2.7.2
    Some distributions backport the XML-whitespace patch.
    
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7266 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_writers/test_docutils_xml.py
+++ docutils/test/test_writers/test_docutils_xml.py
@@ -81,42 +81,74 @@ bodyindents_old = u"""\
 
 # New formatting introduced in versions 2.7.3 and 3.2.3 on 2011-11-18
 # to fix http://bugs.python.org/issue4147
+# (Some distributions ship also earlier versions with this patch.)
 if (sys.version_info < (2, 7, 3) or
     sys.version_info[0] == 3 and sys.version_info < (3, 2, 3)):
-    bodynewlines = bodynewlines_old
-    bodyindents = bodyindents_old
+    whitespace_fix = False
+else:
+    whitespace_fix = True
+
+def publish_xml(settings):
+    return docutils.core.publish_string(source=source.encode('utf8'),
+                                        reader_name='standalone',
+                                        writer_name='docutils_xml',
+                                        settings_overrides=settings)
 
 
 class DocutilsXMLTestCase(DocutilsTestSupport.StandardTestCase):
 
+    settings = {'input_encoding': 'utf8',
+                'output_encoding': 'iso-8859-1',
+                '_disable_config': 1}
+
     def test_publish(self):
-        settings = {'input_encoding': 'utf8',
-                    'output_encoding': 'iso-8859-1',
-                    '_disable_config': 1}
-        for settings['newlines'] in 0, 1:
-            for settings['indents'] in 0, 1:
-                for settings['xml_declaration'] in 0, 1:
-                    for settings['doctype_declaration'] in 0, 1:
+        for self.settings['xml_declaration'] in True, False:
+            for self.settings['doctype_declaration'] in True, False:
+                expected = u''
+                if self.settings['xml_declaration']:
+                    expected += xmldecl
+                if self.settings['doctype_declaration']:
+                    expected += doctypedecl
+                expected += generatedby
+                expected += bodynormal
+                result = publish_xml(self.settings)
+                self.assertEqual(result, expected.encode('latin1'))
 
-                        expected = u''
-                        if settings['xml_declaration']:
-                            expected += xmldecl
-                        if settings['doctype_declaration']:
-                            expected += doctypedecl
-                        expected += generatedby
-                        if settings['indents']:
-                            expected += bodyindents
-                        elif settings['newlines']:
-                            expected += bodynewlines
-                        else:
-                            expected += bodynormal
+    def test_publish_indents(self):
+        self.settings['indents'] = True
+        self.settings['newlines'] = False
+        self.settings['xml_declaration'] = False
+        self.settings['doctype_declaration'] = False
+        result = publish_xml(self.settings)
 
-                        self.assertEqual(docutils.core.publish_string
-                                         (source=source.encode('utf8'),
-                                          reader_name='standalone',
-                                          writer_name='docutils_xml',
-                                          settings_overrides=settings),
-                                         expected.encode('latin1'))
+        # New formatting introduced in versions 2.7.3 and 3.2.3
+        if whitespace_fix:
+            expected = (generatedby + bodyindents).encode('latin1')
+        else:
+            expected = (generatedby + bodyindents_old).encode('latin1')
+        # Some distributions patch also earlier versions:
+        if (result != expected and not whitespace_fix):
+            expected = (generatedby + bodyindents).encode('latin1')
+
+        self.assertEqual(result, expected)
+
+    def test_publish_newlines(self):
+        self.settings['newlines'] = True
+        self.settings['indents'] = False
+        self.settings['xml_declaration'] = False
+        self.settings['doctype_declaration'] = False
+        result = publish_xml(self.settings)
+
+        # New formatting introduced in versions 2.7.3 and 3.2.3
+        if whitespace_fix:
+            expected = (generatedby + bodynewlines).encode('latin1')
+        else:
+            expected = (generatedby + bodynewlines_old).encode('latin1')
+        # Some distributions patch also earlier versions:
+        if (result != expected and not whitespace_fix):
+            expected = (generatedby + bodynewlines).encode('latin1')
+
+        self.assertEqual(result, expected)
 
 
 if __name__ == '__main__':
