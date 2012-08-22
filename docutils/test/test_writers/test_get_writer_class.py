8b6541db14638572967467d4bf1fa2eafc4e43cf
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2012-08-22 19:38:14 +0000

    test get_writer_class failing to load local-writer from python2.7 up
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7500 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/test_writers/test_get_writer_class.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+# $Id$
+# Author: grubert
+# Maintainer: docutils-develop@lists.sourceforge.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+test get_writer_class
+"""
+
+from __init__ import DocutilsTestSupport
+from docutils.writers import get_writer_class
+
+class GetWriterClassTestCase(DocutilsTestSupport.StandardTestCase):
+    #tests = ( ('manpage', 1), ('nope', 0), ('dummy-writer', 1))
+
+    def test_registered_writer(self):
+        wr = get_writer_class('manpage')
+        # raises ImportError on failure
+
+    def test_bogus_writer(self):
+        self.assertRaises(ImportError,
+                          get_writer_class, 'nope')
+
+    def test_local_writer(self):
+        # TODO fails from 2.7 up
+        # requires local-writer.py in test directory (testroot)
+        wr = get_writer_class('local-writer')
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
+
