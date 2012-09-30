556b89d33df9d0de4255ab4913a376e555b3c8fc
paultremblay <paultremblay@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2012-09-30 05:24:00 +0000

    Adding tests for the new script. Also, what appeared to be a bug in root.xsl was not a bug, and so I am taking out that code.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7518 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/docbook/scripts/validate_docbook.py
@@ -0,0 +1,36 @@
+import sys, os, subprocess, glob
+
+class ValidateDocbook():
+
+    def __init__(self):
+        jing_dir = os.environ.get('JING_DIR')
+        if jing_dir == None:
+            raise IOError('You need to set the variable "JING_DIR"')
+        jing_jar = os.path.join(jing_dir, 'bin', 'jing.jar')
+        if not os.path.isfile(jing_jar):
+            raise IOError('You need download the jing.jar')
+        self.jing_jar = jing_jar
+        valid_home = os.environ.get('VALIDATE_HOME')
+        if valid_home == None:
+            raise IOError('You need to set the variable "VALIDATE_HOME"')
+        docbook_rng = os.path.join(valid_home, 'relax', 'docbook.rng')
+        if not os.path.isfile(docbook_rng):
+            raise IOError('You need download docbook.rng')
+        self.docbook_rng = docbook_rng
+
+    def is_valid(self, in_files):
+        command_list = ['java', '-jar', self.jing_jar, self.docbook_rng]
+        if isinstance(in_files, list):
+            for f in in_files:
+                command_list.append(f)
+        else:
+            command_list.append(in_files)
+        exit_status = subprocess.call(command_list)
+        if exit_status:
+            return False
+        return True
+
+if __name__ == '__main__':
+    in_files = sys.argv[1:]
+    valid_obj = ValidateDocbook()
+    valid = valid_obj.is_valid(in_files = in_files)
