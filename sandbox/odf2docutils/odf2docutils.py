a78cd2fa84b079d23ab496d975dcb596e522f3fc
smerten <smerten@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2013-08-01 18:09:59 +0000

    Corrected licence.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7709 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/odf2docutils/odf2docutils.py
+++ sandbox/odf2docutils/odf2docutils.py
@@ -5,7 +5,7 @@
 
 # Copyright (C) 2013 Stefan Merten
 
-# xml2rst.py is free software; you can redistribute it and/or modify
+# odf2docutils.py is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation; either version 2 of the License,
 # or (at your option) any later version.
