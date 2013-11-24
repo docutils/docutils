47adc0438b45ab379b4cef61bb2eab1a756af42d
smerten <smerten@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2013-11-24 13:05:15 +0000

    `XsltParser` accepts XPath extensions.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7732 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/docutils_xml/docutils_xml/parsers/xslt.py
+++ sandbox/docutils_xml/docutils_xml/parsers/xslt.py
@@ -29,6 +29,8 @@ import docutils.parsers
 
 from lxml import etree
 
+import math
+
 ###############################################################################
 ###############################################################################
 # Constants
@@ -45,18 +47,149 @@ from lxml import etree
 ###############################################################################
 # Classes
 
+class XPathExtension(object):
+    """
+    Abstract base class for XPath extension functions. All public methods
+    defined in a derived class are extension functions which can be called from
+    XSLT.
+
+    Methods to be used as extension functions need to accept an fixed first
+    parameter.
+
+    :Parameters:
+
+      context : `lxml.etree._XSLTContext`
+        The evaluation context.
+
+    Apart from that any number of parameters may be given according to the
+    signature of the XPath function.
+    """
+
+    namespace = None
+    """
+    :type: str
+
+    Namespace for these XSLT extension functions defined by this class.
+
+    Override this in derived classes.
+    """
+
+    def _stringParameter(self, string):
+        """
+        :return: The normalized string parameter from an XPath parameter.
+        :rtype: unicode
+
+        :Parameters:
+
+          string : `lxml.etree._ElementStringResult` | [ `lxml.etree._ElementStringResult`, ] | [ `lxml.etree._Element`, ] | [ ]
+            Original XPath string parameter.
+
+        :Exceptions:
+
+          TypeError
+            When a type is given which is incompatible with an XPath string.
+        """
+        if isinstance(string, list):
+            if len(string) == 0:
+                # Happens for forced but non-existing elements like `text()` in
+                # an empty element
+                return u""
+            elif len(string) == 1:
+                string = string[0]
+            else:
+                raise TypeError("Encountered an XPath string parameter with more than one (%d) element" % ( len(string), ))
+
+        if isinstance(string, basestring):
+            return unicode(string)
+        else:
+            raise TypeError("Encountered invalid type for XPath string parameter: %s"
+                % type(string) )
+
+    def _boolParameter(self, boolean):
+        """
+        :return: The normalized bool parameter from an XPath parameter.
+        :rtype: bool
+
+        :Parameters:
+
+          boolean : bool
+            Original XPath bool parameter.
+
+        :Exceptions:
+
+          TypeError
+            When a type is given which is incompatible with a boolean.
+        """
+        if isinstance(boolean, bool):
+            return boolean
+        raise TypeError("Encountered invalid type for XPath boolean parameter: %s"
+                        % type(boolean) )
+
+    def _floatParameter(self, number):
+        """
+        :return: The normalized float parameter from an XPath parameter.
+        :rtype: float
+
+        :Parameters:
+
+          number : float
+            Original XPath number parameter.
+
+        :Exceptions:
+
+          TypeError
+            When a type is given which is incompatible with an float.
+        """
+        if isinstance(number, float):
+            
+            return number
+        raise TypeError("Encountered invalid type for XPath float parameter: %s"
+                        % type(number) )
+
+    def _intParameter(self, number):
+        """
+        :return: The normalized int parameter from an XPath parameter.
+        :rtype: int
+
+        :Parameters:
+
+          number : int
+            Original XPath number parameter.
+
+        :Exceptions:
+
+          TypeError
+            When a type is given which is incompatible with an int.
+
+          ValueError
+            When the number has a non-trivial fractional part.
+        """
+        if isinstance(number, float):
+            ( fraction, integer ) = math.modf(number)
+            if fraction == 0:
+                return int(number)
+            raise ValueError("XPath number (%f) has non-trivial fractiona part"
+                             % ( number, ))
+        raise TypeError("Encountered invalid type for XPath int parameter: %s"
+                        % type(number) )
+
+###############################################################################
+
 class XsltParser(docutils.parsers.Parser):
     """
     Parses XML input by XSLT and stores the result in the attribute
     `xslt_result` of the document. Works together with `XsltWriter`.
     """
 
-    def __init__(self, xsltSource):
+    def __init__(self, xsltSource, extension=None):
         """
         :Parameters:
 
            xsltSource : file-like object
              The source containing the XSLT. This is an open file-like object.
+
+           extension : None | XPathExtension
+             The extensions to use or None for no extensions.
         """
         self.xslt = None
         """
@@ -70,7 +203,11 @@ class XsltParser(docutils.parsers.Parser):
         except Exception, e:
             raise Exception("Error parsing XSLT: %s" % ( e, ))
         xsltSource.close()
-        self.xslt = etree.XSLT(xsltDoc)
+        if extension is None:
+            exts = None
+        else:
+            exts = etree.Extension(extension, ns=extension.namespace)
+        self.xslt = etree.XSLT(xsltDoc, extensions=exts)
 
     def parse(self, inputstring, document):
         self.setup_parse(inputstring, document)
