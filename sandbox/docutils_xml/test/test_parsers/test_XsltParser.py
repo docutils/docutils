5abce65994755cf303af3384cf30b2c1b5ca1805
smerten <smerten@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2013-11-24 13:05:15 +0000

    `XsltParser` accepts XPath extensions.
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@7732 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/docutils_xml/test/test_parsers/test_XsltParser.py
+++ sandbox/docutils_xml/test/test_parsers/test_XsltParser.py
@@ -22,10 +22,11 @@ Test XmlParser.
 import unittest
 import docutils.utils, docutils.frontend
 import StringIO
+from lxml import etree
 
 from __init__ import DocutilsTestSupport
 
-from docutils_xml.parsers.xslt import XsltParser
+from docutils_xml.parsers.xslt import XsltParser, XPathExtension
 from docutils_xml.writers.xslt import XsltWriter
 
 ###############################################################################
@@ -35,7 +36,54 @@ class XsltParserTestCase(DocutilsTestSupport.ParserTestCase):
     Output checker for XsltParser.
     """
 
-    identityXslt = u"""\
+    xslt = None
+    """
+    :type: basestring
+
+    XSLT sheet to use for processing.
+
+    Override this in subclasses.
+    """
+
+    extension = None
+    """
+    :type: None | XPathExtension
+
+    XPath extension class to use.
+
+    Override this in subclasses if you need XPath extension functions.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.parser = XsltParser(StringIO.StringIO(self.xslt), self.extension)
+        """Input parser for this test case."""
+        self.option_parser = docutils.frontend.OptionParser(components=(
+                self.parser, ))
+        DocutilsTestSupport.ParserTestCase.__init__(self, *args, **kwargs)
+
+    def test_parser(self):
+        settings = self.settings.copy()
+        settings.__dict__.update(self.suite_settings)
+        document = docutils.utils.new_document('test data', settings)
+        if (isinstance(self.expected, type)
+            and issubclass(self.expected, Exception)):
+            with self.assertRaises(self.expected):
+                self.parser.parse(self.input, document)
+        else:
+            self.parser.parse(self.input, document)
+            writer = XsltWriter()
+            output = writer.write(document, docutils.io.StringOutput())
+            self.compare_output(self.input, output, self.expected)
+
+###############################################################################
+###############################################################################
+
+class XsltParserIdentityTestCase(XsltParserTestCase):
+    """
+    Output checker for XsltParser with identity transformation.
+    """
+
+    xslt = u"""\
 <?xml version="1.0"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
   <xsl:template match="/">
@@ -44,45 +92,473 @@ class XsltParserTestCase(DocutilsTestSupport.ParserTestCase):
 </xsl:stylesheet>
 """
 
-    parser = XsltParser(StringIO.StringIO(identityXslt))
-    """Parser shared by all XsltParserTestCases."""
+###############################################################################
 
-    option_parser = docutils.frontend.OptionParser(components=(
-            XsltParser, ))
+class XsltParserIdentityTestSuite(DocutilsTestSupport.ParserTestSuite):
 
-    def test_parser(self):
-        settings = self.settings.copy()
-        settings.__dict__.update(self.suite_settings)
-        document = docutils.utils.new_document('test data', settings)
-        self.parser.parse(self.input, document)
-        writer = XsltWriter()
-        output = writer.write(document, docutils.io.StringOutput())
-        self.compare_output(self.input, output, self.expected)
+    test_case_class = XsltParserIdentityTestCase
 
 ###############################################################################
 
-class XsltParserTestSuite(DocutilsTestSupport.ParserTestSuite):
+identity = { }
 
-    test_case_class = XsltParserTestCase
-
-###############################################################################
-
-totest = {}
-
-totest['simple'] = (
+identity['simple'] = (
     ( u"""<?xml version="1.0"?>
 <rootOnly/>
 """,
     u"""<?xml version="1.0"?>
 <rootOnly/>
 """ ),
+    ( u"""<?xml version="1.0"?>
+""",
+    etree.LxmlSyntaxError, ),
+    ( u"""<?xml version="1.0"?>
+<rootOnly>
+""",
+    etree.LxmlSyntaxError, ),
     )
 
 ###############################################################################
+###############################################################################
+
+class XPathExtensionTest(XPathExtension):
+    """
+    XPath extension functions for test purposes.
+    """
+
+    namespace = "http://www.merten-home.de/docutils_xml"
+
+    def parVoid(self, context):
+        """
+        Accept no parameters and do nothing.
+        """
+        pass
+
+    def parString(self, context, s):
+        """
+        Accept a string parameter and return its string representation.
+
+        :Parameters:
+
+          s : Smart string
+            The smart string.
+        """
+        return repr(self._stringParameter(s))
+
+    def parBoolean(self, context, b):
+        """
+        Accept a boolean parameter and return its string representation.
+
+        :Parameters:
+
+          b : boolean
+            The boolean.
+        """
+        return str(self._boolParameter(b))
+
+    def parFloat(self, context, f):
+        """
+        Accept a float parameter and return its string representation.
+
+        :Parameters:
+
+          f : float
+            The float.
+        """
+        return str(self._floatParameter(f))
+
+    def parInt(self, context, i):
+        """
+        Accept an int parameter and return its string representation.
+
+        :Parameters:
+
+          i : int
+            The int.
+        """
+        return str(self._intParameter(i))
+
+###############################################################################
+
+class XsltParserXPathExtensionTestCase(XsltParserTestCase):
+    """
+    Output checker for XsltParser using XPath extension functions.
+    """
+
+    xslt = u"""\
+<?xml version="1.0"?>
+<xsl:stylesheet
+    xmlns:ext="http://www.merten-home.de/docutils_xml"
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    version="1.0">
+
+  <xsl:output
+      method="text"
+      encoding="utf-8"/>
+
+  <xsl:template
+      name="output">
+    <xsl:param
+	name="value"/>
+    <xsl:value-of
+	select="$value"/>
+    <xsl:text>&#xA;</xsl:text>
+  </xsl:template>
+
+  <xsl:template
+      match="void">
+    <xsl:call-template
+	name="output">
+      <xsl:with-param
+	  name="value"
+	  select="ext:parVoid()"/>
+    </xsl:call-template>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <!-- Constant values. -->
+
+  <xsl:template
+      match="test[@const]">
+    <xsl:choose>
+      <xsl:when
+	  test="@type = 'string'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parString('Constant')"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'boolean'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parBoolean(true())"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'float'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parFloat(3.14)"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'int'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parInt(42)"/>
+	</xsl:call-template>
+      </xsl:when>
+    </xsl:choose>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <!-- Missing values. -->
+  
+  <xsl:template
+      match="test[@missing]">
+    <xsl:choose>
+      <xsl:when
+	  test="@type = 'string'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parString()"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'boolean'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parBoolean()"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'float'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parFloat()"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'int'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parInt()"/>
+	</xsl:call-template>
+      </xsl:when>
+    </xsl:choose>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <!-- Element as value. -->
+  
+  <xsl:template
+      match="test[@node]">
+    <xsl:choose>
+      <xsl:when
+	  test="@type = 'string'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parString(.)"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'boolean'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parBoolean(.)"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'float'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parFloat(.)"/>
+	</xsl:call-template>
+      </xsl:when>
+      <xsl:when
+	  test="@type = 'int'">
+	<xsl:call-template
+	    name="output">
+	  <xsl:with-param
+	      name="value"
+	      select="ext:parInt(.)"/>
+	</xsl:call-template>
+      </xsl:when>
+    </xsl:choose>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <!-- Test attribute values. -->
+
+  <xsl:template
+      match="@string">
+    <xsl:call-template
+	name="output">
+      <xsl:with-param
+	  name="value"
+	  select="ext:parString(.)"/>
+    </xsl:call-template>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <xsl:template
+      match="@boolean">
+    <xsl:call-template
+	name="output">
+      <xsl:with-param
+	  name="value"
+	  select="ext:parBoolean(boolean(string(.)))"/>
+    </xsl:call-template>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <xsl:template
+      match="@float">
+    <xsl:call-template
+	name="output">
+      <xsl:with-param
+	  name="value"
+	  select="ext:parFloat(number(.))"/>
+    </xsl:call-template>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <xsl:template
+      match="@int">
+    <xsl:call-template
+	name="output">
+      <xsl:with-param
+	  name="value"
+	  select="ext:parInt(number(.))"/>
+    </xsl:call-template>
+    <xsl:apply-templates/>
+  </xsl:template>
+
+  <!-- By default apply also to attributes. -->
+  <xsl:template
+      match="* | /">
+    <xsl:apply-templates
+	select="* | @* | text()"/>
+  </xsl:template>
+
+  <!-- Suppress normal output. -->
+  <xsl:template
+      match="text() | @*"/>
+
+</xsl:stylesheet>
+"""
+
+    extension = XPathExtensionTest()
+
+###############################################################################
+
+class XsltParserXPathExtensionTestSuite(DocutilsTestSupport.ParserTestSuite):
+
+    test_case_class = XsltParserXPathExtensionTestCase
+
+###############################################################################
+
+xPathExtension = { }
+
+xPathExtension['simple'] = (
+    ( u"""<?xml version="1.0"?>
+<root/>
+""",
+    u"" ),
+
+    ( u"""<?xml version="1.0"?>
+<void/>
+""",
+    u"""
+""" ),
+
+    # string
+    ( u"""<?xml version="1.0"?>
+<test type="string" const=""/>
+""",
+    u"""u'Constant'
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root string="Attribute content"/>
+""",
+    u"""u'Attribute content'
+""" ),
+    ( u"""<?xml version="1.0"?>
+<test type="string" missing=""/>
+""",
+    TypeError ),
+    ( u"""<?xml version="1.0"?>
+<test type="string" node="">Element content</test>
+""",
+    TypeError ),
+
+    # boolean
+    ( u"""<?xml version="1.0"?>
+<test type="boolean" const=""/>
+""",
+    u"""True
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root boolean="non-empty"/>
+""",
+    u"""True
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root boolean=""/>
+""",
+    u"""False
+""" ),
+    ( u"""<?xml version="1.0"?>
+<test type="boolean" missing=""/>
+""",
+    TypeError ),
+    ( u"""<?xml version="1.0"?>
+<test type="boolean" node="">non-empty</test>
+""",
+    TypeError ),
+
+    # float
+    ( u"""<?xml version="1.0"?>
+<test type="float" const=""/>
+""",
+    u"""3.14
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root float="0"/>
+""",
+    u"""0.0
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root float=" -3.7 "/>
+""",
+    u"""-3.7
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root float="1e-3"/>
+""",
+    u"""0.001
+""" ),
+    ( u"""<?xml version="1.0"?>
+<test type="float" missing=""/>
+""",
+    TypeError ),
+    ( u"""<?xml version="1.0"?>
+<test type="float" node="">10.7</test>
+""",
+    TypeError ),
+
+    # int
+    ( u"""<?xml version="1.0"?>
+<test type="int" const=""/>
+""",
+    u"""42
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root int="0"/>
+""",
+    u"""0
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root int=" -3 "/>
+""",
+    u"""-3
+""" ),
+    ( u"""<?xml version="1.0"?>
+<root int="1e3"/>
+""",
+    u"""1000
+""" ),
+    ( u"""<?xml version="1.0"?>
+<test type="int" missing=""/>
+""",
+    TypeError ),
+    ( u"""<?xml version="1.0"?>
+<test type="int" node="">17</test>
+""",
+    TypeError ),
+    ( u"""<?xml version="1.0"?>
+<root int="3.14"/>
+""",
+    ValueError ),
+    )
+
+###############################################################################
+###############################################################################
 
 def suite():
-    s = XsltParserTestSuite()
-    s.generateTests(totest)
+    s = unittest.TestSuite()
+
+    identitySuite = XsltParserIdentityTestSuite()
+    identitySuite.generateTests(identity, dictname='identity')
+    s.addTest(identitySuite)
+
+    xPathExtensionSuite = XsltParserXPathExtensionTestSuite()
+    xPathExtensionSuite.generateTests(xPathExtension, dictname='xPathExtension')
+    s.addTest(xPathExtensionSuite)
+
     return s
 
 ###############################################################################
