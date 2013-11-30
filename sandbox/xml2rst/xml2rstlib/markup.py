2603c3a1cda430bdf582035da1bc92240fe46da4
smerten <smerten@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2013-11-30 13:35:39 +0000

    Implementation uses package `docutils_xml` V0.4.
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@7735 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/xml2rst/xml2rstlib/markup.py
+++ sandbox/xml2rst/xml2rstlib/markup.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2011-2013 Stefan Merten
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation; either version 2 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+
 """
 Python based conversion.
 """
@@ -89,14 +106,16 @@ class Text():
         Return a plain text preventing further interpretation by
         reStructuredText. Text may contain linefeeds.
 
-        `text`: ``str``
-          The string to turn into output text.
+        :Parameters:
 
-        `indent`: ``str``
-          The string to use for indent in case of internal linefeeds.
+          text : str
+            The string to turn into output text.
 
-        `literal`: ``bool``
-          Output literal instead of quoting.
+          indent : str
+            The string to use for indent in case of internal linefeeds.
+
+          literal : bool
+            Output literally instead of quoting.
         """
         sep = "\n" + indent
         if literal:
