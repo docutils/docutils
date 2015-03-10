9b6cf5a03ebc31b7610bc1f146aaba604edc485b
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2015-03-10 13:47:26 +0000

    Use xhtml writer for MathML test: produce valid XHTML+MathML document.
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@7813 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/math_output_mathml.py
+++ docutils/test/functional/tests/math_output_mathml.py
@@ -5,10 +5,10 @@ test_destination = "math_output_mathml.xhtml"
 # Keyword parameters passed to publish_file.
 reader_name = "standalone"
 parser_name = "rst"
-writer_name = "html"
+writer_name = "xhtml"
 
 # Settings
 settings_overrides['math_output'] = 'MathML'
 # local copy of default stylesheet:
-settings_overrides['stylesheet_path'] = ( 
-    'functional/input/data/html4css1.css')
+settings_overrides['stylesheet_path'] = (
+    'functional/input/data/html4-base.css')
