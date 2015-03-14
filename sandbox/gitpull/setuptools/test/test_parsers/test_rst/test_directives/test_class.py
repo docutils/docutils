b65841455e27ecf8ed0edb388bd95d2142e6d504
gitpull <gitpull@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2015-03-14 18:00:20 +0000

    initial sandbox branch for setuptools
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7834 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/gitpull/setuptools/test/test_parsers/test_rst/test_directives/test_class.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+
+# $Id$
+# Author: Lea Wiemann <LeWiemann@gmail.com>
+# Copyright: This module has been placed in the public domain.
+
+"""
+Tests for the 'class' directive.
+"""
+
+from __init__ import DocutilsTestSupport
+
+def suite():
+    s = DocutilsTestSupport.ParserTestSuite()
+    s.generateTests(totest)
+    return s
+
+totest = {}
+
+totest['class'] = [
+["""\
+.. class:: class1  class2
+""",
+"""\
+<document source="test data">
+    <pending>
+        .. internal attributes:
+             .transform: docutils.transforms.misc.ClassAttribute
+             .details:
+               class: ['class1', 'class2']
+               directive: 'class'
+"""],
+["""\
+.. class:: class1  class2
+
+   The classes are applied to this paragraph.
+
+   And this one.
+""",
+"""\
+<document source="test data">
+    <paragraph classes="class1 class2">
+        The classes are applied to this paragraph.
+    <paragraph classes="class1 class2">
+        And this one.
+"""],
+]
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main(defaultTest='suite')
