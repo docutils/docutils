5b191ecd75ba09af7598a9a8bbb1e42eacb62415
timehorse <timehorse@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2015-06-03 22:02:46 +0000

    Fix for bug number 276 ODT Writer strips classes from inline character markup when more than one class.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7899 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/odf_odt/__init__.py
+++ docutils/docutils/writers/odf_odt/__init__.py
@@ -870,6 +870,8 @@ class ODFTranslator(nodes.GenericNodeVisitor):
         self.table_styles = None
         self.in_citation = False
 
+        # Keep track of nested styling classes
+        self.inline_style_count_stack = []
 
     def get_str_stylesheet(self):
         return self.str_stylesheet
@@ -2397,14 +2399,26 @@ class ODFTranslator(nodes.GenericNodeVisitor):
 
     def visit_inline(self, node):
         styles = node.attributes.get('classes', ())
-        if len(styles) > 0:
-            inline_style =  styles[0]
-        el = SubElement(self.current_element, 'text:span',
-            attrib={'text:style-name': self.rststyle(inline_style)})
+        if styles:
+            el = self.current_element
+            for inline_style in styles:
+                el = SubElement(el, 'text:span',
+                                attrib={'text:style-name':
+                                        self.rststyle(inline_style)})
+            count = len(styles)
+        else:
+            # No style was specified so use a default style (old code
+            # crashed if no style was given)
+            el = SubElement(self.current_element, 'text:span')
+            count = 1
+
         self.set_current_element(el)
+        self.inline_style_count_stack.append(count)
 
     def depart_inline(self, node):
-        self.set_to_parent()
+        count = self.inline_style_count_stack.pop()
+        for x in range(count):
+            self.set_to_parent()
 
     def _calculate_code_block_padding(self, line):
         count = 0
