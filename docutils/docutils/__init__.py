73f00fbd151199413198e1dc15d740b90d8c1e58
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2016-12-09 13:11:53 +0000

    Release 0.13.1: set version number to 0.13.2a
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@7991 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/__init__.py
+++ docutils/docutils/__init__.py
@@ -52,7 +52,7 @@ Subpackages:
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.13.1'
+__version__ = '0.13.2a'
 """``major.minor.micro`` version number.  The micro number is bumped for API
 changes, for new functionality, and for interim project releases.  The minor
 number is bumped whenever there is a significant project release.  The major
