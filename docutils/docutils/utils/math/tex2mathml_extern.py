429b378394410d7fbce08e9d9de1aaf0ee0e6e7b
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2018-01-04 07:03:42 +0000

    Fix error reporting in wrapper for external tex2mathml converters.
    Thanks to Alan Isaac for reporting.
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8208 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/utils/math/tex2mathml_extern.py
+++ docutils/docutils/utils/math/tex2mathml_extern.py
@@ -26,7 +26,7 @@ document_template = r"""\documentclass{article}
 
 def latexml(math_code, reporter=None):
     """Convert LaTeX math code to MathML with LaTeXML_
-    
+
     .. _LaTeXML: http://dlmf.nist.gov/LaTeXML/
     """
     p = subprocess.Popen(['latexml',
@@ -42,7 +42,7 @@ def latexml(math_code, reporter=None):
     p.stdin.close()
     latexml_code = p.stdout.read()
     latexml_err = p.stderr.read().decode('utf8')
-    if reporter and latexml_err.find('Error') >= 0 or not latexml_code:
+    if reporter and (latexml_err.find('Error') >= 0 or not latexml_code):
         reporter.error(latexml_err)
 
     post_p = subprocess.Popen(['latexmlpost',
@@ -60,9 +60,9 @@ def latexml(math_code, reporter=None):
     post_p.stdin.close()
     result = post_p.stdout.read().decode('utf8')
     post_p_err = post_p.stderr.read().decode('utf8')
-    if reporter and post_p_err.find('Error') >= 0 or not result:
+    if reporter and (post_p_err.find('Error') >= 0 or not result):
         reporter.error(post_p_err)
-    
+
     # extract MathML code:
     start,end = result.find('<math'), result.find('</math>')+7
     result = result[start:end]
@@ -72,7 +72,7 @@ def latexml(math_code, reporter=None):
 
 def ttm(math_code, reporter=None):
     """Convert LaTeX math code to MathML with TtM_
-    
+
     .. _TtM: http://hutchinson.belmont.ma.us/tth/mml/
     """
     p = subprocess.Popen(['ttm',
@@ -100,7 +100,7 @@ def ttm(math_code, reporter=None):
 
 def blahtexml(math_code, inline=True, reporter=None):
     """Convert LaTeX math code to MathML with blahtexml_
-    
+
     .. _blahtexml: http://gva.noekeon.org/blahtexml/
     """
     options = ['--mathml',
@@ -116,7 +116,7 @@ def blahtexml(math_code, inline=True, reporter=None):
     else:
         mathmode_arg = 'mode="display"'
         options.append('--displaymath')
-    
+
     p = subprocess.Popen(['blahtexml']+options,
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
@@ -126,8 +126,7 @@ def blahtexml(math_code, inline=True, reporter=None):
     p.stdin.close()
     result = p.stdout.read().decode('utf8')
     err = p.stderr.read().decode('utf8')
-    
-    print err
+
     if result.find('<error>') >= 0:
         raise SyntaxError('\nMessage from external converter blahtexml:\n'
                 +result[result.find('<message>')+9:result.find('</message>')])
