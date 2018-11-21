2f2a0cda4ec2ca1338fd3bf235696874f6651626
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2018-11-21 19:09:04 +0000

    Fix [ 351 ] The docstring of Reader class is wrong.
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8237 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/readers/__init__.py
+++ docutils/docutils/readers/__init__.py
@@ -23,8 +23,9 @@ class Reader(Component):
 
     Each reader module or package must export a subclass also called 'Reader'.
 
-    The two steps of a Reader's responsibility are `scan()` and
-    `parse()`.  Call `read()` to process a document.
+    The two steps of a Reader's responsibility are to read data from the
+    source Input object and parse the data with the Parser object.
+    Call `read()` to process a document.
     """
 
     component_type = 'reader'
