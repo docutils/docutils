2accb43d620d45f0d088b9b1864c5f8bfd2e3696
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2019-02-12 17:41:20 +0000

    Fix bug #358: Non-breaking space removed from fixed-width literal.
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8244 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/_html_base.py
+++ docutils/docutils/writers/_html_base.py
@@ -183,7 +183,7 @@ class HTMLTranslator(nodes.NodeVisitor):
 
     stylesheet_link = '<link rel="stylesheet" href="%s" type="text/css" />\n'
     embedded_stylesheet = '<style type="text/css">\n\n%s\n</style>\n'
-    words_and_spaces = re.compile(r'\S+| +|\n')
+    words_and_spaces = re.compile(r'[^ \n]+| +|\n')
     # wrap point inside word:
     in_word_wrap_point = re.compile(r'.+\W\W.+|[-?].+', re.U)
     lang_attribute = 'lang' # name changes to 'xml:lang' in XHTML 1.1
