b49f69093cc9939e81bc3f947f752cb158266cf6
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2019-07-21 07:17:16 +0000

    add classifier 'Programming Language :: Python :: 3.7'
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8271 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/setup.py
+++ docutils/setup.py
@@ -202,6 +202,7 @@ classifiers = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Documentation',
     'Topic :: Software Development :: Documentation',
     'Topic :: Text Processing',
