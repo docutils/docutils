e7b5292c7b786ea564ee249eb4db3c99e35f8c34
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2019-08-20 14:01:36 +0000

    Fix: ResourceWarning: unclosed file python3.8 (partial bug 377)
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8327 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/standalone_rst_s5_html_2.py
+++ docutils/test/functional/tests/standalone_rst_s5_html_2.py
@@ -1,5 +1,6 @@
 # initialize with the settings & definitions from test 1:
-exec(open('functional/tests/standalone_rst_s5_html_1.py').read())
+with open('functional/tests/standalone_rst_s5_html_1.py') as _f:
+    exec(_f.read())
 
 # overrides specific to this test:
 test_destination = 'standalone_rst_s5_html_2.html'
