0238af96f33c5400e111b67ea36a1ab29c47f2f9
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2019-09-06 10:19:11 +0000

    Fix typo in setup.py.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8384 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/setup.py
+++ docutils/setup.py
@@ -86,7 +86,7 @@ what-you-see-is-what-you-get plaintext markup syntax.""",  # wrap at col 60
     'maintainer_email': 'docutils-develop@lists.sourceforge.net',
     'license': 'public domain, Python, 2-Clause BSD, GPL 3 (see COPYING.txt)',
     'platforms': 'OS-independent',
-    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*', !=3.4.*',
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     'cmdclass': {
         'build_data': build_data,
         'install_data': smart_install_data,
