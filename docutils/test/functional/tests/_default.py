f6ec791c8bb54eca2fff5d5a85be066320b6510b
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2019-11-15 10:36:15 +0000

    tests/functional: Set "auto_id_prefix" to "%" (expands to tag-names).
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8414 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/_default.py
+++ docutils/test/functional/tests/_default.py
@@ -5,3 +5,4 @@ settings_overrides['halt_level'] = 5
 settings_overrides['warning_stream'] = ''
 settings_overrides['input_encoding'] = 'utf-8'
 settings_overrides['embed_stylesheet'] = False
+settings_overrides['auto_id_prefix'] = '%'
