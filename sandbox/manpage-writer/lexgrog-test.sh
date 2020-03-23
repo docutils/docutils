f7a4265ca883aa0a2bc61ee10bc56c2a32551074
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2020-03-23 13:56:19 +0000

    add list of failing pages
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8503 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/manpage-writer/lexgrog-test.sh
+++ sandbox/manpage-writer/lexgrog-test.sh
@@ -6,15 +6,18 @@ DIR=output
 
 CNT=0
 ERR=0
+FAILS=""
 for F in $DIR/*.man ; do 
     CNT=$(( CNT + 1 ))
     lexgrog $F
     if test $? -ne 0 ; then
         ERR=$(( ERR + 1 ))
+        FAILS="$FAILS $(basename $F)"
     fi
 done
 if test $ERR -eq 0 ; then
     echo "OK $CNT tests passed"
 else
     echo "ERROR $ERR of $CNT tests failed"
+    echo "-- $FAILS"
 fi 
