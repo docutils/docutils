0200812aa51fb77012553f368e0a79dd7517f97b
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2020-04-03 16:57:36 +0000

    html5 writer: wrap image elements in <p> unless inline or in figure.
    In HTML, <img> is by default an inline element.
    The rST "image" directive generates block-level elements
    (unless used in a "substitution" directive).
    
    Wrapping in a container enables more advanced CSS styling.
    A paragraph as wrapper fits well with Docutils wrapping
    inline content in paragraphs by default.
    
    No change for the "html4css1" writer (ensure backwards compatibility).
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8510 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/html4css1/__init__.py
+++ docutils/docutils/writers/html4css1/__init__.py
@@ -15,10 +15,14 @@ for proper viewing with a modern graphical browser.
 __docformat__ = 'reStructuredText'
 
 import os.path
+import re
+import sys
+
 import docutils
 from docutils import frontend, nodes, writers, io
 from docutils.transforms import writer_aux
 from docutils.writers import _html_base
+from docutils.writers._html_base import PIL, url2pathname
 
 class Writer(writers._html_base.Writer):
 
@@ -531,6 +535,75 @@ class HTMLTranslator(writers._html_base.HTMLTranslator):
     object_image_types = {'.svg': 'image/svg+xml',
                          '.swf': 'application/x-shockwave-flash'}
 
+    # do not wrap block-level images in <p></p> (for backwards compatibility)
+    def visit_image(self, node):
+        atts = {}
+        uri = node['uri']
+        ext = os.path.splitext(uri)[1].lower()
+        if ext in self.object_image_types:
+            atts['data'] = uri
+            atts['type'] = self.object_image_types[ext]
+        else:
+            atts['src'] = uri
+            atts['alt'] = node.get('alt', uri)
+        # image size
+        if 'width' in node:
+            atts['width'] = node['width']
+        if 'height' in node:
+            atts['height'] = node['height']
+        if 'scale' in node:
+            if (PIL and not ('width' in node and 'height' in node)
+                and self.settings.file_insertion_enabled):
+                imagepath = url2pathname(uri)
+                try:
+                    img = PIL.Image.open(
+                            imagepath.encode(sys.getfilesystemencoding()))
+                except (IOError, UnicodeEncodeError):
+                    pass # TODO: warn?
+                else:
+                    self.settings.record_dependencies.add(
+                        imagepath.replace('\\', '/'))
+                    if 'width' not in atts:
+                        atts['width'] = '%dpx' % img.size[0]
+                    if 'height' not in atts:
+                        atts['height'] = '%dpx' % img.size[1]
+                    del img
+            for att_name in 'width', 'height':
+                if att_name in atts:
+                    match = re.match(r'([0-9.]+)(\S*)$', atts[att_name])
+                    assert match
+                    atts[att_name] = '%s%s' % (
+                        float(match.group(1)) * (float(node['scale']) / 100),
+                        match.group(2))
+        style = []
+        for att_name in 'width', 'height':
+            if att_name in atts:
+                if re.match(r'^[0-9.]+$', atts[att_name]):
+                    # Interpret unitless values as pixels.
+                    atts[att_name] += 'px'
+                style.append('%s: %s;' % (att_name, atts[att_name]))
+                del atts[att_name]
+        if style:
+            atts['style'] = ' '.join(style)
+        if (isinstance(node.parent, nodes.TextElement) or
+            (isinstance(node.parent, nodes.reference) and
+             not isinstance(node.parent.parent, nodes.TextElement))):
+            # Inline context or surrounded by <a>...</a>.
+            suffix = ''
+        else:
+            suffix = '\n'
+        if 'align' in node:
+            atts['class'] = 'align-%s' % node['align']
+        if ext in self.object_image_types:
+            # do NOT use an empty tag: incorrect rendering in browsers
+            self.body.append(self.starttag(node, 'object', suffix, **atts) +
+                             node.get('alt', uri) + '</object>' + suffix)
+        else:
+            self.body.append(self.emptytag(node, 'img', suffix, **atts))
+
+    def depart_image(self, node):
+        pass
+
     # use table for footnote text,
     # context added in footnote_backrefs.
     def visit_label(self, node):
@@ -664,6 +737,32 @@ class HTMLTranslator(writers._html_base.HTMLTranslator):
 
     def depart_paragraph(self, node):
         self.body.append(self.context.pop())
+
+    # do not wrap images in paragraphs (for backwards compatibility)
+    def visit_reference(self, node):
+        atts = {'class': 'reference'}
+        if 'refuri' in node:
+            atts['href'] = node['refuri']
+            if ( self.settings.cloak_email_addresses
+                 and atts['href'].startswith('mailto:')):
+                atts['href'] = self.cloak_mailto(atts['href'])
+                self.in_mailto = True
+            atts['class'] += ' external'
+        else:
+            assert 'refid' in node, \
+                   'References must have "refuri" or "refid" attribute.'
+            atts['href'] = '#' + node['refid']
+            atts['class'] += ' internal'
+        if not isinstance(node.parent, nodes.TextElement):
+            assert len(node) == 1 and isinstance(node[0], nodes.image)
+            atts['class'] += ' image-reference'
+        self.body.append(self.starttag(node, 'a', '', **atts))
+
+    def depart_reference(self, node):
+        self.body.append('</a>')
+        if not isinstance(node.parent, nodes.TextElement):
+            self.body.append('\n')
+        self.in_mailto = False
 
     # ersatz for first/last pseudo-classes
     def visit_sidebar(self, node):
