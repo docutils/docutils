d541a865ce8b5364a325b0ddc81822ab4358c6ef
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2020-08-10 09:11:55 +0000

    disable pygments by default
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8532 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/_default.py
+++ docutils/test/functional/tests/_default.py
@@ -6,3 +6,5 @@ settings_overrides['warning_stream'] = ''
 settings_overrides['input_encoding'] = 'utf-8'
 settings_overrides['embed_stylesheet'] = False
 settings_overrides['auto_id_prefix'] = '%'
+# avoid "Pygments not found"
+settings_overrides['syntax_highlight'] = 'none'
