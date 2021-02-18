d4a194e0111be5997746bc2c2f644bc52c3efcbf
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-02-18 23:09:44 +0000

    latex-writer: add \\leavevmode after term or field name if required.
    Based on patch #176 by John Thorvald Wodder II
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8626 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/functional/tests/latex_leavevmode.py
@@ -0,0 +1,10 @@
+# Source and destination file names.
+test_source = "latex_leavevmode.txt"
+test_destination = "latex_leavevmode.tex"
+
+# Keyword parameters passed to publish_file.
+writer_name = "latex"
+
+# Settings
+# use "smartquotes" transition:
+settings_overrides['smart_quotes'] = True
