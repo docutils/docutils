00e56b3bc17ef9a962883d36319957c1fec40165
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-04-17 10:19:10 +0000

    clear build directory then more wheels differ
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8698 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/infrastructure/crosstest/buildwheel.sh
+++ sandbox/infrastructure/crosstest/buildwheel.sh
@@ -15,7 +15,8 @@ for VDIR in ./py3.* ; do
     export PYTHONPATH= ; . bin/activate
     cd $PATH2SRC
     find . -type f -name "*.pyc" -delete
-    mkdir -p dist
+    rm -r dist
+    rm -r build
     python setup.py bdist_wheel --universal
     deactivate
     cd $TESTDIR
