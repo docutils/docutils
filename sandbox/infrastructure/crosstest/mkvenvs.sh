54e9e3548eb3617d1314c71eab456fa9535986d4
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-04-17 09:54:18 +0000

    install wheel into each venv
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8695 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/infrastructure/crosstest/mkvenvs.sh
+++ sandbox/infrastructure/crosstest/mkvenvs.sh
@@ -4,6 +4,12 @@
 
 # python2.7 -m virtualenv py2.7
 for I in 5 6 7 8 9 10 ; do
-    python3.$I -m venv py3.$I
+    VDIR=py3.$I
+    python3.$I -m venv $VDIR
+    cd $VDIR
+    . bin/activate
+    python -m pip install setuptools wheel
+    deactivate
+    cd ..
 done
 
