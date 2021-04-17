04c0fda3e2047d3610f22412a57228ee7a36a660
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-04-17 10:02:51 +0000

    build with each venv
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8696 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/infrastructure/crosstest/buildwheel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# build wheel in each virtual environment
+
+PATH2SRC=$(pwd)/../../../docutils
+
+TESTDIR=$(pwd)
+
+# python2.7 -m virtualenv py2.7
+for VDIR in ./py3.* ; do
+    echo "$VDIR"
+    echo "=========="
+
+    cd $VDIR
+    export PYTHONPATH= ; . bin/activate
+    cd $PATH2SRC
+    find . -type f -name "*.pyc" -delete
+    mkdir -p dist
+    python setup.py bdist_wheel --universal
+    deactivate
+    cd $TESTDIR
+    mv $PATH2SRC/dist $VDIR
+done
+
