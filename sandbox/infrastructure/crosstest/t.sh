ff42c88702f1d9f60da310c55de385696d2c88f2
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-04-17 14:41:18 +0000

    test script: before install remove from pip cache, after show what is installed
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8709 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/infrastructure/crosstest/t.sh
+++ sandbox/infrastructure/crosstest/t.sh
@@ -16,14 +16,17 @@ for VDIR in ./py3.* ; do
     if [ -e $WHL ] ; then
         python -m pip install $WHL
     elif [ $WHL = "TESTPYPI" ] ; then
+        # cache command available in newer pips
+        python -m pip cache remove docutils
         python -m pip install --no-cache-dir --index-url https://test.pypi.org/simple/ --no-deps docutils
-        python -m pip install --upgrade --index-url https://test.pypi.org/simple/ --no-deps docutils
     else
         python -m pip install $WHL
     fi
+    python -m pip show docutils
+    read -p "pause" X
     cp -Lr ~/projects/docutils-code/docutils/test . 
     python test/alltests.py
-    read X
+    read -p "pause" X
     deactivate
     cd ..
 done
