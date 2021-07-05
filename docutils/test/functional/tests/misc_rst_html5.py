fcbcf53254f4f81b602e7903472576e1c49d8cf6
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-07-05 19:25:20 +0000

    HTML5: small tweaks
    Move space character between section number and heading
    into "sectnum" span. Simplify code in _html_base.py.
    
    responsive.css: highlight target of current URL,
    show an arrow when hovering over section heading
    to hint at the target of toc-backlinks.
    
    Test setting `--toc_backlinks=top` and the "responsive.css" stylesheet.
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8787 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/misc_rst_html5.py
+++ docutils/test/functional/tests/misc_rst_html5.py
@@ -8,6 +8,11 @@ parser_name = "rst"
 writer_name = "html5"
 
 # Settings
+# local copy of stylesheets:
+# (Test runs in ``docutils/test/``, we need relative path from there.)
+settings_overrides['stylesheet_dirs'] = ('.', 'functional/input/data')
+settings_overrides['stylesheet_path'] = 'minimal.css,responsive.css'
 settings_overrides['smart_quotes'] = 'yes'
 settings_overrides['embed_images'] = 'yes'
-settings_overrides['embed_stylesheet'] = False
+settings_overrides['toc_backlinks'] = 'top'
+
