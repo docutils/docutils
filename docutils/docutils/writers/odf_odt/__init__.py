3bebfc5a462b606cbb522db669a87ca05991ed66
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-11-15 19:40:31 +0000

    Small documentation updates. Mark ODT writer as provisional.
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8889 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/writers/odf_odt/__init__.py
+++ docutils/docutils/writers/odf_odt/__init__.py
@@ -5,6 +5,8 @@
 """
 Open Document Format (ODF) Writer.
 
+This module is provisional:
+the API is not settled and may change with any minor Docutils version.
 """
 from __future__ import absolute_import
 
