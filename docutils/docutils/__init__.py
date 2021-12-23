22db16eb78425b9f1b0422b0f6ea75cad87d3777
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2021-12-23 14:54:51 +0000

    Set version to 0.19b.dev.
    Leaving the 0.18 maintenance mode.
    
    Note: We may skip 0.19 if the next release is "1.0-ready"
    (depending on consensus on backwards compatibility policy and
    definition/documentation of the public part of the API).
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@8914 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/__init__.py
+++ docutils/docutils/__init__.py
@@ -56,7 +56,7 @@ from collections import namedtuple
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.18.2b.dev'
+__version__ = '0.19b.dev'
 """Docutils version identifier (complies with PEP 440)::
 
     major.minor[.micro][releaselevel[serial]][.dev]
@@ -111,8 +111,8 @@ class VersionInfo(namedtuple('VersionInfo',
 
 __version_info__ = VersionInfo(
     major=0,
-    minor=18,
-    micro=2,
+    minor=19,
+    micro=0,
     releaselevel='beta', # one of 'alpha', 'beta', 'candidate', 'final'
     # pre-release serial number (0 for final releases and active development):
     serial=0,
