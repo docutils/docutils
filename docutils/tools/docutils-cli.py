8253cb4eac29a1f8b0c0b8637b9d3a2c3cd4cee7
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2022-01-25 16:39:07 +0000

    docutils-cli: Use "SettingsSpec" interface for config file parsing.
    Use the high-level interface provided by docutils.core
    instead of the low-level classes/functions in docutils.frontend
    to get reader/parser/writer names from standard configuration files.
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@8965 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/tools/docutils-cli.py
+++ docutils/tools/docutils-cli.py
@@ -25,71 +25,66 @@ except:
 import argparse
 import sys
 
-from docutils.core import publish_cmdline, default_description
-from docutils.frontend import ConfigParser, OptionParser
+import docutils
+from docutils.core import Publisher, publish_cmdline
 
-config_section = 'docutils-cli application'
+usage = '%(prog)s [options] [<source> [<destination>]]'
 
-def config_settings_from_files():
-    """Return dict with default settings for the docutils-cli front end.
+description = ('Convert plaintext documentation into useful formats.\n'
+               'Reads from <source> (default is stdin) '
+               'and writes to <destination> (default is stdout).')
 
-    Read default values for the three docutils-cli options from the
-    [docutils-cli application] section in standard configuration files.
+epilog = ('Further optional arguments depend on the selected components. '
+          'See <https://docutils.sourceforge.io/docs/user/config.html> '
+          'for the full reference. The list below adapts to your selection.')
 
-    Command-line options are defined separately, using the "argparse" module
-    to allow two-stage parsing of the command line.
-    """
-    settings = {'reader': 'standalone',
-                'parser': 'rst',
-                'writer': 'html5'
-               }
-    option_parser = OptionParser()
-    config_parser = ConfigParser()
-    config_files = option_parser.get_standard_config_files()
-    config_parser.read(config_files, option_parser)
-    settings.update(config_parser.get_section(config_section))
-    return settings
 
-default_settings = config_settings_from_files()
+class CliSettingsSpec(docutils.SettingsSpec):
+    """Additional runtime settings & command-line options for the CLI."""
 
-description = u'Generate documents from reStructuredText or Markdown sources.'
+    settings_spec = (
+        'Docutils CLI Options',
+        None,
+        (# 'help text', [<option strings>], {<keyword arguments>}
+         ('', ['--reader'], {'default': 'standalone'}),
+         ('', ['--parser'], {'default': 'restructuredtext'}),
+         ('', ['--writer'], {'default': 'html'}),
+        )
+    )
+    config_section = 'docutils-cli application'
+    config_section_dependencies = ('applications',)
 
-epilog = (u'Further optional arguments are added by the selected '
-          u'components, the list below adapts to your selection.'
-         )
+# Get default components from configuration files 
+# default to "html5" writer for backwards compatibility
+default_settings = Publisher().get_settings(settings_spec=CliSettingsSpec,
+                        writer='html5')
 
-parser = argparse.ArgumentParser(add_help=False,
-                description=description, epilog=epilog,
-                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('source', nargs='?')
-parser.add_argument('destination', nargs='?')
-parser.add_argument('--reader', help=u'reader name',
-                    default=default_settings['reader'])
-parser.add_argument('--parser', help=u'parser name',
-                    default=default_settings['parser'])
-parser.add_argument('--writer', help=u'writer name',
-                    default=default_settings['writer'])
+argparser = argparse.ArgumentParser(
+                usage=usage, description=description, epilog=epilog,
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                add_help=False,)
 
-(args, remainder) = parser.parse_known_args()
+argparser.add_argument('--reader', help='reader name',
+                       default=default_settings.reader)
+argparser.add_argument('--parser', help='parser name',
+                       default=default_settings.parser)
+argparser.add_argument('--writer', help='writer name',
+                       default=default_settings.writer)
 
-# push back positional arguments
-if args.destination:
-    remainder.insert(0, args.destination)
-if args.source:
-    remainder.insert(0, args.source)
+(args, remainder) = argparser.parse_known_args()
 
 if '-h' in sys.argv or '--help' in sys.argv:
-    print(parser.format_help())
-    print('')
+    print(argparser.format_help())
 
 try:
     publish_cmdline(reader_name=args.reader,
                     parser_name=args.parser,
                     writer_name=args.writer,
-                    config_section=config_section,
-                    description=default_description,
-                    argv=remainder)
+                    config_section=CliSettingsSpec.config_section,
+                    argv=remainder,
+                    usage='',
+                    description='')
 except ImportError as error:
     print('%s.' % error)
     if '--traceback' in remainder:
