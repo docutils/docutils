fbc87d80a480efe7df98df5dd9851950b939b151
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2022-06-19 20:23:12 +0000

    Add wrapper around groups of footnotes to facilitate styling as a list.
    Adapt style sheets and tests.
    
    Cf. bug #450.
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9081 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_writers/test_html5_polyglot_parts.py
+++ docutils/test/test_writers/test_html5_polyglot_parts.py
@@ -675,6 +675,7 @@ The latter are referenced a second time [#f2]_ [twice]_.
 {'fragment': '''\
 <p>Two footnotes <a class="footnote-reference brackets" href="#f1" id="footnote-reference-1" role="doc-noteref"><span class="fn-bracket">[</span>1<span class="fn-bracket">]</span></a> <a class="footnote-reference brackets" href="#f2" id="footnote-reference-2" role="doc-noteref"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></a> and two citations <a class="citation-reference" href="#once" id="citation-reference-1" role="doc-biblioref">[once]</a> <a class="citation-reference" href="#twice" id="citation-reference-2" role="doc-biblioref">[twice]</a>.</p>
 <p>The latter are referenced a second time <a class="footnote-reference brackets" href="#f2" id="footnote-reference-3" role="doc-noteref"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></a> <a class="citation-reference" href="#twice" id="citation-reference-3" role="doc-biblioref">[twice]</a>.</p>
+<aside class="footnote-list brackets">
 <aside class="footnote brackets" id="f1" role="note">
 <span class="label"><span class="fn-bracket">[</span>1<span class="fn-bracket">]</span></span>
 <p>referenced once</p>
@@ -682,6 +683,7 @@ The latter are referenced a second time [#f2]_ [twice]_.
 <aside class="footnote brackets" id="f2" role="note">
 <span class="label"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></span>
 <p>referenced twice</p>
+</aside>
 </aside>
 <div role="list" class="citation-list">
 <div class="citation" id="once" role="doc-biblioentry">
@@ -697,6 +699,7 @@ The latter are referenced a second time [#f2]_ [twice]_.
 <main>
 <p>Two footnotes <a class="footnote-reference brackets" href="#f1" id="footnote-reference-1" role="doc-noteref"><span class="fn-bracket">[</span>1<span class="fn-bracket">]</span></a> <a class="footnote-reference brackets" href="#f2" id="footnote-reference-2" role="doc-noteref"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></a> and two citations <a class="citation-reference" href="#once" id="citation-reference-1" role="doc-biblioref">[once]</a> <a class="citation-reference" href="#twice" id="citation-reference-2" role="doc-biblioref">[twice]</a>.</p>
 <p>The latter are referenced a second time <a class="footnote-reference brackets" href="#f2" id="footnote-reference-3" role="doc-noteref"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></a> <a class="citation-reference" href="#twice" id="citation-reference-3" role="doc-biblioref">[twice]</a>.</p>
+<aside class="footnote-list brackets">
 <aside class="footnote brackets" id="f1" role="note">
 <span class="label"><span class="fn-bracket">[</span>1<span class="fn-bracket">]</span></span>
 <p>referenced once</p>
@@ -704,6 +707,7 @@ The latter are referenced a second time [#f2]_ [twice]_.
 <aside class="footnote brackets" id="f2" role="note">
 <span class="label"><span class="fn-bracket">[</span>2<span class="fn-bracket">]</span></span>
 <p>referenced twice</p>
+</aside>
 </aside>
 <div role="list" class="citation-list">
 <div class="citation" id="once" role="doc-biblioentry">
