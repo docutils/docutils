cb95bf19dba30901a275dee0c552e63f7568e218
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2022-07-05 20:43:08 +0000

    version 0.19.1b.dev
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@9105 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/docutils/__init__.py
+++ docutils/docutils/__init__.py
@@ -54,7 +54,7 @@ from collections import namedtuple
 
 __docformat__ = 'reStructuredText'
 
-__version__ = '0.19'
+__version__ = '0.19.1b.dev'
 """Docutils version identifier (complies with PEP 440)::
 
     major.minor[.micro][releaselevel[serial]][.dev]
@@ -117,10 +117,10 @@ class VersionInfo(namedtuple('VersionInfo',
 __version_info__ = VersionInfo(
     major=0,
     minor=19,
-    micro=0,
-    releaselevel='final',  # one of 'alpha', 'beta', 'candidate', 'final'
+    micro=1,
+    releaselevel='beta',  # one of 'alpha', 'beta', 'candidate', 'final'
     serial=0,  # pre-release number (0 for final releases and snapshots)
-    release=True  # True for official releases and pre-releases
+    release=False  # True for official releases and pre-releases
     )
 """Comprehensive version information tuple.
 
