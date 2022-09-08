4aa9f112a842ce1ea61a2e256c0bf39005764892
aa-turner <aa-turner@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2022-09-08 14:10:32 +0000

    Add draft reposurgeon conversion procedure
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9119 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ sandbox/aa-turner/git_conversion/post-convert.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+cd $1
+
+# Ignore filemode changes
+git config core.filemode false
+
+# Run a garbage-collect on the generated git repository.  Import doesn't.
+# This repack call is the active part of gc --aggressive.  This call is
+# tuned for very large repositories.
+git -c pack.threads=1 repack -AdF --window=1250 --depth=250
+
+# Clean repo
+git prune
+git clean -fxd
+git reset HEAD --hard
+
+# Check integrity
+git fsck --full --dangling --unreachable --strict
+
+# Enable commit graph
+git config core.commitGraph true
+git show-ref -s | git commit-graph write --stdin-commits
