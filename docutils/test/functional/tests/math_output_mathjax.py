7037219c96cb335d6b561e2b28eb63324c6fe644
aa-turner <aa-turner@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2022-10-27 17:40:23 +0000

    Inline ``_default`` in functional tests
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9180 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/math_output_mathjax.py
+++ docutils/test/functional/tests/math_output_mathjax.py
@@ -1,3 +1,17 @@
+# Default settings for all tests.
+
+settings_overrides = {
+    '_disable_config': True,
+    'report_level': 2,
+    'halt_level': 5,
+    'warning_stream': '',
+    'input_encoding': 'utf-8',
+    'embed_stylesheet': False,
+    'auto_id_prefix': '%',
+    # avoid "Pygments not found"
+    'syntax_highlight': 'none'
+}
+
 # Source and destination file names.
 test_source = "data/math.txt"
 test_destination = "math_output_mathjax.html"
