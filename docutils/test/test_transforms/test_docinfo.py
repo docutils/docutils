docutils/docutils

commit d0ec1b367c100d727b8977fb231acc61971ee0e1

Fix test for spurious enumerated lists in bibliographic fields.
The test for "single line paragraphs" failed for bibliographic fields followed
by an empty line.

git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@9348 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

docutils/docutils/transforms/frontmatter.py
@@ -466,7 +466,7 @@ class DocInfo(Transform):
         # enumerated list
         # https://docutils.sourceforge.io/docs/ref/rst/restructuredtext.html#enumerated-lists
         if (isinstance(f_body[0], nodes.enumerated_list)
-            and '\n' not in f_body.rawsource):
+            and '\n' not in f_body.rawsource.strip()):
             # parse into a dummy document and use created nodes
             _document = utils.new_document('*DocInfo transform*',
                                            field.document.settings)
@@ -517,7 +517,8 @@ class DocInfo(Transform):
                   'it must contain either a single paragraph (with authors '
                   'separated by one of "%s"), multiple paragraphs (one per '
                   'author), or a bullet list with one paragraph (one author) '
-                  'per item.'
+                  'per item. Remember that leading initials can cause '
+                  '(mis)recognizing names as enumerated lists.'
                   % (name, ''.join(self.language.author_separators)),
                   base_node=field)
             raise

docutils/test/test_transforms/test_docinfo.py
@@ -75,8 +75,11 @@ totest['bibliographic_field_lists'] = ((DocInfo,), [
     It is automatically moved to the end of the other bibliographic elements.
 
 :Author: E. *Xample*
+
 :Version: 1
+
 :Date: 2001-08-11
+
 :Parameter i: integer
 """,
 """\

commit 16a031dbd0a1d2d0bb3b98976c46f76b4d2ea00c

Fix test failure introduced in [r9348].
Reword and wrap warning message for incompatible content
of the bibliographic field "author".

Adapt "frontmatter" unit test cases.

git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9351 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

docutils/docutils/transforms/frontmatter.py
@@ -513,14 +513,15 @@ class DocInfo(Transform):
                 raise TransformError
         except TransformError:
             field[-1] += self.document.reporter.warning(
-                  'Bibliographic field "%s" incompatible with extraction: '
-                  'it must contain either a single paragraph (with authors '
-                  'separated by one of "%s"), multiple paragraphs (one per '
-                  'author), or a bullet list with one paragraph (one author) '
-                  'per item. Remember that leading initials can cause '
-                  '(mis)recognizing names as enumerated lists.'
-                  % (name, ''.join(self.language.author_separators)),
-                  base_node=field)
+                f'Cannot extract "{name}" from bibliographic field:\n'
+                f'Bibliographic field "{name}" must contain either\n'
+                ' a single paragraph (with author names separated by one of '
+                f'"{"".join(self.language.author_separators)}"),\n'
+                ' multiple paragraphs (one per author),\n'
+                ' or a bullet list with one author name per item.\n'
+                'Note: Leading initials can cause (mis)recognizing names '
+                'as enumerated list.',
+                base_node=field)
             raise
 
     def authors_from_one_paragraph(self, field):

docutils/test/test_transforms/test_docinfo.py
@@ -358,7 +358,12 @@ totest['bibliographic_field_lists'] = ((DocInfo,), [
                             Shaw
                 <system_message level="2" line="3" source="test data" type="WARNING">
                     <paragraph>
-                        Bibliographic field "Authors" incompatible with extraction: it must contain either a single paragraph (with authors separated by one of ";,"), multiple paragraphs (one per author), or a bullet list with one paragraph (one author) per item. Remember that leading initials can cause (mis)recognizing names as enumerated lists.
+                        Cannot extract "Authors" from bibliographic field:
+                        Bibliographic field "Authors" must contain either
+                         a single paragraph (with author names separated by one of ";,"),
+                         multiple paragraphs (one per author),
+                         or a bullet list with one author name per item.
+                        Note: Leading initials can cause (mis)recognizing names as enumerated list.
         <field classes="authors">
             <field_name>
                 Authors
@@ -368,7 +373,12 @@ totest['bibliographic_field_lists'] = ((DocInfo,), [
                     <list_item>
                 <system_message level="2" line="6" source="test data" type="WARNING">
                     <paragraph>
-                        Bibliographic field "Authors" incompatible with extraction: it must contain either a single paragraph (with authors separated by one of ";,"), multiple paragraphs (one per author), or a bullet list with one paragraph (one author) per item. Remember that leading initials can cause (mis)recognizing names as enumerated lists.
+                        Cannot extract "Authors" from bibliographic field:
+                        Bibliographic field "Authors" must contain either
+                         a single paragraph (with author names separated by one of ";,"),
+                         multiple paragraphs (one per author),
+                         or a bullet list with one author name per item.
+                        Note: Leading initials can cause (mis)recognizing names as enumerated list.
         <field classes="authors">
             <field_name>
                 Authors
@@ -381,7 +391,12 @@ totest['bibliographic_field_lists'] = ((DocInfo,), [
                     Two
                 <system_message level="2" line="10" source="test data" type="WARNING">
                     <paragraph>
-                        Bibliographic field "Authors" incompatible with extraction: it must contain either a single paragraph (with authors separated by one of ";,"), multiple paragraphs (one per author), or a bullet list with one paragraph (one author) per item. Remember that leading initials can cause (mis)recognizing names as enumerated lists.
+                        Cannot extract "Authors" from bibliographic field:
+                        Bibliographic field "Authors" must contain either
+                         a single paragraph (with author names separated by one of ";,"),
+                         multiple paragraphs (one per author),
+                         or a bullet list with one author name per item.
+                        Note: Leading initials can cause (mis)recognizing names as enumerated list.
         <field classes="authors">
             <field_name>
                 Authors
@@ -394,7 +409,12 @@ totest['bibliographic_field_lists'] = ((DocInfo,), [
                             Two
                 <system_message level="2" line="15" source="test data" type="WARNING">
                     <paragraph>
-                        Bibliographic field "Authors" incompatible with extraction: it must contain either a single paragraph (with authors separated by one of ";,"), multiple paragraphs (one per author), or a bullet list with one paragraph (one author) per item. Remember that leading initials can cause (mis)recognizing names as enumerated lists.
+                        Cannot extract "Authors" from bibliographic field:
+                        Bibliographic field "Authors" must contain either
+                         a single paragraph (with author names separated by one of ";,"),
+                         multiple paragraphs (one per author),
+                         or a bullet list with one author name per item.
+                        Note: Leading initials can cause (mis)recognizing names as enumerated list.
 """],
 ["""\
 .. RCS keyword extraction.