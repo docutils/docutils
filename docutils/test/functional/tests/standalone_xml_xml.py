62beaddd254bdc0a4299dbfdc75160058a781939
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2024-06-06 14:02:27 +0000

    xml-parser: Add functional test (round-trip)
    Test the XML parser with the output of the XML writer.
    Goal is a round-trip without changes (under reasonable conditions).
    
    Work in progress, still fails.
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9739 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

---
+++ docutils/test/functional/tests/standalone_xml_xml.py
@@ -0,0 +1,13 @@
+# Test the Docutils XML parser
+#
+# Source is the output sample of the XML writer test.
+# We expect a round-trip without changes.
+
+# Source and destination file names
+test_source = '../expected/standalone_rst_docutils_xml.xml'
+test_destination = 'standalone_rst_docutils_xml.xml'
+
+# Keyword parameters passed to publish_file()
+parser_name = 'xml'
+writer_name = 'xml'
+settings_overrides = {'indents': True}
