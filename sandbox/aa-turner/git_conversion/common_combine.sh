9d9d36896b921496a81bfcfec8b054ec44dbdcb7
aa-turner <aa-turner@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2024-08-07 01:41:55 +0000

    Major improvement to the reposurgeon conversion procedure
    - Update to reposurgeon 4.38
    - Incorporate more history (pre-CVS release archives)
    - Preserve SVN branches and convert to git merges
    - Add attribution for contributed patches
    
    
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9856 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- sandbox/aa-turner/git_conversion/common_combine.sh
+++ sandbox/aa-turner/git_conversion/common_combine.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
+# exit on error or undefined variable; echo commands
+set -eux
+
 # These reposurgeon scripts are common to all repositories.
 # They are run in order:
 # 1. common_load.lift
-# 2. common_merge.sh
-# 3. common_clean.lift
+# 2. common_cvs_docstring.lift
+# 3. common_cvs_structuredtext.lift
+# 4. common_combine.sh
+# 5. common_clean.lift
 
-mkdir -p tmp_combined
+mkdir tmp_combined
 cd tmp_combined
 
 git init --quiet
 git config core.filemode false
+git config user.email "goodger@python.org"
+git config user.name "David Goodger"
 
 git remote add tmp_early ../docutils_tmp_early
 git remote add tmp_docstring ../cvs_tmp_docstring
@@ -18,16 +25,21 @@ git remote add tmp_structuredtext ../cvs_tmp_structuredtext
 git fetch --all
 
 git switch -c docutils tmp_early/master
-git switch -c docstring tmp_docstring/docstring
-git switch -c structuredtext tmp_structuredtext/structuredtext
-git switch docutils
+git switch -c docstring tmp_docstring/master-docstring
+git switch -c structuredtext tmp_structuredtext/master-structuredtext
 
-git merge --allow-unrelated-histories -m "~merge histories~" docutils structuredtext docstring
+# This will claim to fail, hence using read-tree
+# https://stackoverflow.com/a/31186732
+git switch docutils
+git merge --allow-unrelated-histories -m "~merge histories~" docutils structuredtext docstring || true
 git read-tree docutils structuredtext docstring
+# Remove the old web directories
+git rm -r web_docstring web_structuredtext
+# Commit the index and reset the working tree
 git commit -m "~merge histories~"
 git reset --hard
 
-git rm -r web_docstring web_structuredtext
-git commit --amend --all -m "~merge histories~"
-#git rm -rf -- *
-#git checkout docutils -- .
+git branch --delete docstring structuredtext
+git remote remove tmp_early
+git remote remove tmp_docstring
+git remote remove tmp_structuredtext
--- sandbox/aa-turner/git_conversion/post-convert.sh
+++ sandbox/aa-turner/git_conversion/post-convert.sh
@@ -3,8 +3,9 @@ set -e
 
 cd "$1"
 
-# Ignore filemode changes
-git config core.filemode false
+# Reinitialise repository. We do this for sanity, as git might expect
+# things that reposurgeon doesn't provide.
+git init
 
 # Run a garbage-collect on the generated git repository.  Import doesn't.
 # This repack call is the active part of gc --aggressive.  This call is
@@ -13,12 +14,18 @@ git -c pack.threads=1 repack -AdF --window=1250 --depth=250
 
 # Clean repo
 git prune
+git rm -rf .
 git clean -fxd
 git reset HEAD --hard
 
 # Check integrity
-git fsck --full --dangling --unreachable --strict
+git fsck --full --strict --unreachable --dangling --lost-found
 
 # Enable commit graph
 git config core.commitGraph true
-git show-ref -s | git commit-graph write --stdin-commits
+git config fetch.writeCommitGraph true
+git commit-graph write --append --reachable --changed-paths
+git commit-graph verify
+
+# End by re-reinitialising repository. Why not!
+git init
