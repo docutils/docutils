c2a14e8d9994065e27d527a70274e02414226c75
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2024-10-22 21:41:16 +0000

    Skip functional test that requires PIL if PIL is absent.
    For Docutils, PIL is a recommendation, no requirement.
    
    It is, however, required for the extraction of the image size
    from an image file and a warning is written if it it missing.
    This makes the output differ from the "expected" sample on
    a functional test where the "scale" option of the "image"
    directive is used.
    
    The "length_units_html5" functional test is intended to provide
    an example of the length unit support for visual inspection with
    a HTML browser.  There are additional unit tests for the
    "scale" option with and without PIL, so we can safely skip
    the functional test if PIL is missing on a system.
    
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@9965 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/functional/tests/length_units_html5.py
+++ docutils/test/functional/tests/length_units_html5.py
@@ -1,3 +1,11 @@
+import unittest
+# PIL is optional but required with the "image" directive's "scale" option
+from docutils.parsers.rst.directives.images import PIL
+if PIL is None:
+    raise unittest.SkipTest('requires PIL')
+else:
+    del unittest, PIL  # clean up namespace
+
 # Source and destination file names
 test_source = "length_units.rst"
 test_destination = "length_units_html5.html"
