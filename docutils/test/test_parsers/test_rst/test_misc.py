758a515bbd74b26d9f684d420f3ecac462e39c06
milde <milde@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2025-08-19 15:07:47 +0000

    Test nested parsing in a directive.
    Define/use sample directives to test nested parsing with section support.
    Advantages:
    
    * The tests are simpler and easier to comprehend.
    * Emulate possible use cases. Provide template for extension developers.
    * More comprehensive testing.
    * Allows test/comparision with the legacy section parsing algorithm
      (used up to 0.22).
    
    Tests revealed problems with section styles matching existing styles:
    
    legacy section parsing:
      DATA LOSS:
      Sections with a level outside the nested parsing are silently dropped!
      This happens also with a base node attached to the document.
    
    new section parsing:
      After nested parsing, the "insertion point" is restored to what it was
      before. If sibling sections or parent sections are attached according to
      their level, the insertion point after parsing is before these sections
      (the order of the nodes is mixed up).
    
      In a block-quote or other body element the current node is not attached to
      the document because the content is parsed with nested_parse into node that
      is appended after parsing.
      If the current node is used as base node, the section style hierarchy may be
      document-wide or local, depending on the placement of the directive.
    
    git-svn-id: svn://svn.code.sf.net/p/docutils/code/trunk@10205 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_parsers/test_rst/test_misc.py
+++ docutils/test/test_parsers/test_rst/test_misc.py
@@ -9,16 +9,14 @@ Tests for basic functionality of parser classes.
 
 from pathlib import Path
 import sys
-import types
 import unittest
 
 if __name__ == '__main__':
     # prepend the local "docutils root" to the Python library path
     sys.path.insert(0, Path(__file__).resolve().parents[2].as_posix())
 
-from docutils import frontend, nodes, statemachine, utils
+from docutils import frontend, utils
 import docutils.parsers.rst
-from docutils.parsers.rst import states
 
 
 class RstParserTests(unittest.TestCase):
@@ -30,173 +28,6 @@ class RstParserTests(unittest.TestCase):
                                       frontend.get_default_settings(parser))
         with self.assertRaises(TypeError):
             parser.parse(b'hol', document)
-
-
-class RSTStateTests(unittest.TestCase):
-
-    # state machine
-    machine = states.RSTStateMachine(state_classes=states.state_classes,
-                                     initial_state='Body')
-    # "generic" state
-    state = states.RSTState(machine)
-
-    def title_markup(self, text, adornment='-'):
-        underline = adornment * len(text)
-        return statemachine.StringList([text, underline], 'section block')
-
-    def setUp(self):
-        # state machine runtime initialization (cf. RSTStateMachine.run())
-        # Only for test:
-        #    don't use this low-level approach in production code!
-
-        # empty <document> and settings:
-        settings = frontend.get_default_settings(docutils.parsers.rst.Parser)
-        settings.halt_level = 2
-        settings.warning_stream = ''
-        document = self.document = utils.new_document('test data', settings)
-        # language module (localized directive and role names)
-        # self.machine.language = languages.get_language(
-        #                             document.settings.language_code)
-        # self.machine.match_titles = True  # support sections
-        # "memo": Container for document-wide auxiliary data
-        inliner = states.Inliner()  # rST parser for inline markup
-        inliner.init_customizations(document.settings)
-        self.machine.memo = types.SimpleNamespace(document=document,
-                                                  reporter=document.reporter,
-                                                  language='en',
-                                                  title_styles=[],
-                                                  inliner=inliner)
-        # self.machine.document = document
-        # self.machine.reporter = document.reporter
-        self.machine.node = document
-        # initialize `state` object
-        self.state.runtime_init()
-
-    def test_nested_parse(self):
-        # parse a text block in a nested parser
-        text = statemachine.StringList(['test input'], 'nested block')
-        tip = nodes.tip('')  # base `node`
-
-        # plain text -> attach <paragraph> to `node`:
-        self.state.nested_parse(text, input_offset=0, node=tip)
-        self.assertEqual('<tip><paragraph>test input</paragraph></tip>',
-                         str(tip))
-
-        # by default, section titles are not supported:
-        title = self.title_markup('top', '-')
-        with self.assertRaisesRegex(utils.SystemMessage,
-                                    'Unexpected section title.'):
-            self.state.nested_parse(title, input_offset=0, node=tip)
-
-    # Nested parsing with section markup is supported with
-    # ``match_titles=True`` but not used in Docutils.
-    # (However, Sphinx "autodoc" and some contributed extensions use it.)
-    def test_nested_parse_with_sections_base_attached(self):
-        # If the base `node` is attached to the `document`,
-        # the document-wide section style hierarchy is used.
-
-        # base`node` is a <section>
-        section = nodes.section('')
-        self.document += section  # attach to document
-        self.machine.memo.title_styles.append('-')  # register title style
-        # parse level-2 section title
-        title = self.title_markup('sub', '~')
-        # -> append new <section> to the parsers base `node`
-        self.state.nested_parse(title, 0, node=section, match_titles=True)
-        self.assertEqual('<document source="test data">\n'
-                         '    <section>\n'
-                         '        <section ids="sub" names="sub">\n'
-                         '            <title>\n'
-                         '                sub\n',
-                         str(self.document.pformat()))
-        self.assertEqual(['-', '~'], self.machine.memo.title_styles)
-
-        # parse top-level section title
-        title = self.title_markup('top', '-')
-        # -> move 1 level up and attach <section> to document
-        self.state.nested_parse(title, 0, node=section, match_titles=True)
-        self.assertEqual('<document source="test data">\n'
-                         '    <section>\n'
-                         '        <section ids="sub" names="sub">\n'
-                         '            <title>\n'
-                         '                sub\n'
-                         '    <section ids="top" names="top">\n'
-                         '        <title>\n'
-                         '            top\n',
-                         str(self.document.pformat()))
-
-        # base`node` is a <paragraph>
-        paragraph = nodes.paragraph('', 'base node')
-        section += paragraph  # attach (indirectly)
-        # parse top-level section title
-        title = self.title_markup('top 2', '-')
-        # -> move 1 level up and attach <section> to document
-        self.state.nested_parse(title, 0, node=paragraph, match_titles=True)
-        self.assertEqual('<section ids="top-2" names="top\\ 2">\n'
-                         '    <title>\n'
-                         '        top 2\n',
-                         self.document[-1].pformat())
-
-        # new (2nd-level) section title
-        title = self.title_markup('sub 2', '~')
-        self.state.nested_parse(title, 0, node=paragraph, match_titles=True)
-        self.assertEqual('<section>\n'
-                         '    <section ids="sub" names="sub">\n'
-                         '        <title>\n'
-                         '            sub\n'
-                         '    <paragraph>\n'
-                         '        base node\n'
-                         '    <section ids="sub-2" names="sub\\ 2">\n'
-                         '        <title>\n'
-                         '            sub 2\n',
-                         section.pformat())
-
-    def test_nested_parse_with_sections_detached(self):
-        # The base `node` does not need to be attached to the document.
-        # "global" title style hierarchy (ignored with detached base node)
-        self.machine.memo.title_styles = ['-', '~']
-
-        # base `node` is a <paragraph> without parents
-        base = nodes.paragraph('')
-        base.document = self.document  # this is not "attaching"
-        # level-2 title style
-        title = self.title_markup('sub', '~')
-        # new hierarchy -> attach <section> to base `node`
-        self.state.nested_parse(title, 0, node=base, match_titles=True)
-        self.assertEqual('<paragraph>\n'
-                         '    <section ids="sub" names="sub">\n'
-                         '        <title>\n'
-                         '            sub\n',
-                         base.pformat())
-        # It is the users responsibility to ensure that the base node
-        # may contain a <section> (or move the section after parsing).
-        # You may check with `validate()`:
-        with self.assertRaises(nodes.ValidationError):
-            base.validate()
-
-        # a new hierarchy is used in every call of nested_parse()
-        # parse 2 section titles
-        title = self.title_markup('new', '*') + self.title_markup('top', '-')
-        # new hierarchy -> attach section and sub-section to base node
-        self.state.nested_parse(title, 0, node=base, match_titles=True)
-        self.assertEqual(
-                         '<paragraph>\n'
-                         '    <section ids="sub" names="sub">\n'
-                         '        <title>\n'
-                         '            sub\n'
-                         '    <section ids="new" names="new">\n'
-                         '        <title>\n'
-                         '            new\n'
-                         '        <section ids="top" names="top">\n'
-                         '            <title>\n'
-                         '                top\n',
-                         base.pformat())
-
-        # document-wide style hierarchy unchanged:
-        self.assertEqual(['-', '~'], self.machine.memo.title_styles)
-
-        # print(self.document.pformat())
-        # print(base.pformat())
 
 
 if __name__ == '__main__':
