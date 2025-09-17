c65b453450185d61f39f68e557bd6010cbac7c26
grubert <grubert@929543f6-e4f2-0310-98a6-ba3bd3dd1d04>
2025-09-17 18:37:21 +0000

    version 0.22.2b1.dev
    git-svn-id: http://svn.code.sf.net/p/docutils/code/trunk@10241 929543f6-e4f2-0310-98a6-ba3bd3dd1d04

--- docutils/test/test_writers/test_manpage.py
+++ docutils/test/test_writers/test_manpage.py
@@ -96,7 +96,7 @@ class WriterPublishTestCase(unittest.TestCase):
 
 
 document_start = r""".\" Man page generated from reStructuredText
-.\" by the Docutils 0.22.1 manpage writer.
+.\" by the Docutils 0.22.2b1.dev manpage writer.
 .
 """
 
